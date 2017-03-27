a07182c665f3fc0c63bb5e840c5302a25dc5a64c
SN\kavics <gyeby5@gmail.com>
2017-03-27 16:58:23 +0200

    Add storage test class stub.

---
+++ src/SenseNet.Core.Tests/PackagingStorageTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SenseNet.Core.Tests
+{
+    [TestClass]
+    public class PackagingStorageTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
