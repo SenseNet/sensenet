36948cb85eb1b039fa380670c192ae304a0fdf28
SN\kavics <gyeby5@gmail.com>
2017-03-28 05:25:38 +0200

    Add implementation classes.

---
+++ src/SenseNet.Core.Tests/Implementations/TestDataProcedure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenseNet.Core.Tests.Implementations
+{
+    class TestDataProcedure
+    {
+    }
+}
