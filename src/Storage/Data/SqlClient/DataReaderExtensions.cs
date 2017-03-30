bbdc9a03e2b9d6e46d4ce660bdb44024e72ae0e8
Zoltan Gyebrovszki <gyeby5@gmail.com>
2017-03-30 09:27:54 +0200

    Rename SqlDataReaderExtensions to DataReaderExtensions.

---
+++ src/Storage/Data/SqlClient/DataReaderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenseNet.ContentRepository.Storage.Data.SqlClient
+{
+    //UNDONE: rename to DataReaderExtensions
+    public static class DataReaderExtensions
+    {
+        public static DateTime GetDateTimeUtc(this IDataReader reader, string name)
+        {
+            int ordinal = reader.GetOrdinal(name);
+            return reader.GetDateTimeUtc(ordinal);
+        }
+
+        public static DateTime GetDateTimeUtc(this IDataReader reader, int ordinal)
+        {
+            DateTime unspecified = reader.GetDateTime(ordinal);
+            return DateTime.SpecifyKind(unspecified, DateTimeKind.Utc);
+        }
+    }  
+}
