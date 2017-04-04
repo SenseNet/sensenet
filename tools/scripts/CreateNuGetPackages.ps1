1925b6ba16856983f882e74c85085a3c3b72395a
tusmester <tusmester@gmail.com>
2017-04-04 11:37:03 +0200

    Move import structure to a different folder and create SnAdmin install package as a zip file.

--- tools/scripts/CreateNuGetPackages.ps1
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -2,4 +2,15 @@
 nuget pack ..\..\src\BlobStorage\BlobStorage.csproj -properties Configuration=Release
 nuget pack ..\..\src\Preview\Preview.csproj -properties Configuration=Release
 nuget pack ..\..\src\Services\Services.nuspec -properties Configuration=Release
+
+New-Item ..\..\src\nuget\snadmin\install-services\scripts -ItemType directory -Force
+
+Copy-Item ..\..\src\Storage\Data\SqlClient\Scripts\Create_SenseNet_Database.sql ..\..\src\nuget\snadmin\install-services\scripts -Force
+Copy-Item ..\..\src\Storage\Data\SqlClient\Scripts\Install_Security.sql ..\..\src\nuget\snadmin\install-services\scripts -Force
+Copy-Item ..\..\src\Storage\Data\SqlClient\Scripts\Install_01_Schema.sql ..\..\src\nuget\snadmin\install-services\scripts -Force
+Copy-Item ..\..\src\Storage\Data\SqlClient\Scripts\Install_02_Procs.sql ..\..\src\nuget\snadmin\install-services\scripts -Force
+Copy-Item ..\..\src\Storage\Data\SqlClient\Scripts\Install_03_Data_Phase1.sql ..\..\src\nuget\snadmin\install-services\scripts -Force
+Copy-Item ..\..\src\Storage\Data\SqlClient\Scripts\Install_04_Data_Phase2.sql ..\..\src\nuget\snadmin\install-services\scripts -Force
+
+Compress-Archive -Path "..\..\src\nuget\snadmin\install-services\*" -Force -CompressionLevel Optimal -DestinationPath "..\..\src\nuget\content\Admin\tools\install-services.zip"
 nuget pack ..\..\src\Services\Services.Install.nuspec -properties Configuration=Release
