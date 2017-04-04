9e3e0be2a4f2789fa423dd5bfa59aff064c66761
tusmester <tusmester@gmail.com>
2017-04-04 11:07:58 +0200

    Separate NuGet packages for the Common, BlobStorage and Preview libraries. PS script added for batch package creation.

---
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -0,0 +1,5 @@
+﻿nuget pack ..\..\src\Common\Common.csproj -properties Configuration=Release
+nuget pack ..\..\src\BlobStorage\BlobStorage.csproj -properties Configuration=Release
+nuget pack ..\..\src\Preview\Preview.csproj -properties Configuration=Release
+nuget pack ..\..\src\Services\Services.nuspec -properties Configuration=Release
+nuget pack ..\..\src\Services\Services.Install.nuspec -properties Configuration=Release
