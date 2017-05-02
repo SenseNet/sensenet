703529d0f7581636663ae0e874389be8c9b53ada
tusmester <tusmester@gmail.com>
2017-05-02 20:17:54 +0200

    Raise version to 7.0.0-beta2.

--- src/Preview/Properties/AssemblyInfo.cs
+++ src/Preview/Properties/AssemblyInfo.cs
@@ -23,4 +23,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("7.0.0.0")]
 [assembly: AssemblyFileVersion("7.0.0.0")]
-[assembly: AssemblyInformationalVersion("7.0.0-beta1")]
+[assembly: AssemblyInformationalVersion("7.0.0-beta2")]
