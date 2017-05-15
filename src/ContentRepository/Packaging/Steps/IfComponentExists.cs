b8de8383c31d1361926046d7fee094029b5d6776
tusmester <tusmester@gmail.com>
2017-05-15 12:50:02 +0200

    Add new step for checking component existence during package execution. (#89)
    * Add new step for checking component existence during package execution.
    * IfComponentExists step can handle multiple component dependencies.

---
+++ src/ContentRepository/Packaging/Steps/IfComponentExists.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using SenseNet.ContentRepository;
+
+namespace SenseNet.Packaging.Steps
+{
+    public class IfComponentExists : ConditionalStep
+    {
+        public IEnumerable<XmlElement> Components { get; set; }
+
+        protected override bool EvaluateCondition(ExecutionContext context)
+        {
+            if (Components == null)
+                return true;
+
+            foreach (var dependency in Components.Select(Dependency.Parse))
+            {
+                try
+                {
+                    context.Manifest.CheckDependency(dependency, RepositoryVersionInfo.Instance, false);
+                }
+                catch (PackagePreconditionException)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
