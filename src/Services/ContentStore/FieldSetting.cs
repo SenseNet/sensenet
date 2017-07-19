e653de8f5549448ba25f28ecab8247289843de64
SN\kavics <gyeby5@gmail.com>
2017-07-19 03:50:46 +0200

    Delete ContentStoreService classes.

--- src/Services/ContentStore/FieldSetting.cs
+++ src/Services/ContentStore/FieldSetting.cs
@@ -1,48 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using sn = SenseNet.ContentRepository.Schema;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using sn = SenseNet.ContentRepository.Schema;
 
-namespace SenseNet.Services.ContentStore
-{
-    public class FieldSetting
-    {
-        private sn.FieldSetting sourceData;
+//namespace SenseNet.Services.ContentStore
+//{
+//    public class FieldSetting
+//    {
+//        private sn.FieldSetting sourceData;
 
-        public string DisplayName
-        {
-            get { return sourceData.DisplayName; }
-        }
+//        public string DisplayName
+//        {
+//            get { return sourceData.DisplayName; }
+//        }
 
-        public string FullName
-        {
-            get { return sourceData.FullName; }
-        }
+//        public string FullName
+//        {
+//            get { return sourceData.FullName; }
+//        }
 
-        public string BindingName
-        {
-            get { return sourceData.BindingName; }
-        }
+//        public string BindingName
+//        {
+//            get { return sourceData.BindingName; }
+//        }
 
-        public string TypeName
-        {
-            get { return sourceData.ShortName; }
-        }
+//        public string TypeName
+//        {
+//            get { return sourceData.ShortName; }
+//        }
 
-        public string OwnerName
-        {
-            get { return sourceData.Owner.Name; }
-        }
+//        public string OwnerName
+//        {
+//            get { return sourceData.Owner.Name; }
+//        }
 
-        public string OwnerTitle
-        {
-            get { return sourceData.Owner.DisplayName; }
-        }
+//        public string OwnerTitle
+//        {
+//            get { return sourceData.Owner.DisplayName; }
+//        }
 
-        public FieldSetting(sn.FieldSetting value)
-        {
-            sourceData = value;
-        }
-    }
-}
+//        public FieldSetting(sn.FieldSetting value)
+//        {
+//            sourceData = value;
+//        }
+//    }
+//}
