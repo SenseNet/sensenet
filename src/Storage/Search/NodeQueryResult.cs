1a13ca700f323eb91603e0d8178b6c72bfc66faf
SN\kavics <gyeby5@gmail.com>
2017-07-20 04:35:39 +0200

    Comment out unused enums and classes. Move files.

--- src/Storage/Search/NodeQueryResult.cs
+++ src/Storage/Search/NodeQueryResult.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
 
-namespace SenseNet.ContentRepository.Storage.Search
-{
-    [Obsolete("Use SenseNet.Search.QueryResult instead.", true)]
-    public class NodeQueryResult
-    {
-        private NodeList<Node> _result;
+//namespace SenseNet.ContentRepository.Storage.Search
+//{
+//    [Obsolete("Use SenseNet.Search.QueryResult instead.", true)]
+//    public class NodeQueryResult
+//    {
+//        private NodeList<Node> _result;
 
-        public IEnumerable<Node> Nodes { get { return _result; } }
-        public IEnumerable<int> Identifiers { get { return _result.GetIdentifiers(); } }
-        public int Count { get { return _result.Count; } }
+//        public IEnumerable<Node> Nodes { get { return _result; } }
+//        public IEnumerable<int> Identifiers { get { return _result.GetIdentifiers(); } }
+//        public int Count { get { return _result.Count; } }
 
-        internal NodeQueryResult(NodeList<Node> nodes)
-        {
-            _result = nodes;
-        }
+//        internal NodeQueryResult(NodeList<Node> nodes)
+//        {
+//            _result = nodes;
+//        }
 
-        // public constructor for serving in-memory nodequery result creation
-        public NodeQueryResult(IEnumerable<int> ids) : this(new NodeList<Node>(ids))
-        {
-        }
-    }
-}
+//        // public constructor for serving in-memory nodequery result creation
+//        public NodeQueryResult(IEnumerable<int> ids) : this(new NodeList<Node>(ids))
+//        {
+//        }
+//    }
+//}
