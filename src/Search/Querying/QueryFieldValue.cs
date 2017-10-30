cb42c03e38d713062aa1014cc6befa02aec7acb1
Zoltán Gyebrovszki <kavics@sensenet.com>
2017-10-30 05:57:28 +0100

    [KFI] Organize namespaces.

---
+++ src/Search/Querying/QueryFieldValue.cs
@@ -0,0 +1,21 @@
+﻿namespace SenseNet.Search.Querying
+{
+    public class QueryFieldValue
+    {
+        public string StringValue { get; }
+        internal bool IsPhrase { get; private set; }
+        internal double? FuzzyValue { get; set; }
+
+        internal QueryFieldValue(string stringValue, bool isPhrase)
+        {
+            StringValue = stringValue;
+            IsPhrase = isPhrase;
+        }
+
+        public override string ToString()
+        {
+            return string.Concat(StringValue, FuzzyValue == null ? "" : ":" + FuzzyValue);
+        }
+
+    }
+}
