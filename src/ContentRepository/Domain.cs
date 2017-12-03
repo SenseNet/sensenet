8a3f35d8157f0219f433f42a6afff2296ea00fd1
Zoltán Gyebrovszki <kavics@sensenet.com>
2017-12-03 03:39:02 +0100

    [KFI] Doc: Domain.

--- src/ContentRepository/Domain.cs
+++ src/ContentRepository/Domain.cs
@@ -5,18 +5,42 @@ using SenseNet.ContentRepository.Security.ADSync;
 
 namespace SenseNet.ContentRepository
 {
+    /// <summary>
+    /// Defines an Content handler that represents a domain that is the top level element of the identity management.
+    /// </summary>
     [ContentHandler]
     public class Domain : Folder, IADSyncable
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Domain"/> class.
+        /// </summary>
+        /// <param name="parent">The parent.</param>
         public Domain(Node parent) : this(parent, null) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Domain"/> class.
+        /// </summary>
+        /// <param name="parent">The parent.</param>
+        /// <param name="nodeTypeName">Name of the node type.</param>
         public Domain(Node parent, string nodeTypeName) : base(parent, nodeTypeName) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Domain"/> class during the loading process.
+        /// Do not use this constructor directly from your code.
+        /// </summary>
         protected Domain(NodeToken token) : base(token) { }
 
         //////////////////////////////////////// Public Properties ////////////////////////////////////////
 
+        /// <summary>
+        /// Returns true if this instance name is "BuiltIn".
+        /// </summary>
         public bool IsBuiltInDomain => Name == IdentityManagement.BuiltInDomainName;
 
         // =================================================================================== IADSyncable Members
+
+        /// <summary>
+        /// Writes back the given sync-id to the database.
+        /// </summary>
+        /// <param name="guid"></param>
         public void UpdateLastSync(System.Guid? guid)
         {
             if (guid.HasValue)
