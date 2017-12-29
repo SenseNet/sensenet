4f54a1f3dd87bef87a71b564f24cb7153dc1f5f5
tusmester <tusmester@gmail.com>
2017-12-29 13:52:47 +0100

    Update xml docs in the ContentHandlerAttribute class.

--- src/ContentRepository/Schema/ContentHandlerAttribute.cs
+++ src/ContentRepository/Schema/ContentHandlerAttribute.cs
@@ -5,14 +5,14 @@ using System.Text;
 namespace  SenseNet.ContentRepository.Schema
 {
     /// <summary>
-    /// Indicates that a class handles a specific type of content in the sensenet content repository.
+    /// Indicates that a class handles a specific type of content in the Content Repository.
     /// This class cannot be inherited.
     /// </summary>
 	[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
 	public sealed class ContentHandlerAttribute : Attribute
 	{
         /// <summary>
-        /// Initializes a new instance of the <see cref="ContentHandlerAttribute"/>.
+        /// Initializes a new instance of the <see cref="ContentHandlerAttribute"/> class.
         /// </summary>
 		public ContentHandlerAttribute() { }
 	}
