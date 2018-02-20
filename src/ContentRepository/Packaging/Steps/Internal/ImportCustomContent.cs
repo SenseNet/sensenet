1d07a46bf98de2b3d0b03b03ee976552799d8077
tusmester <tusmester@gmail.com>
2018-02-20 18:16:07 +0100

    [KFI] Add new steps for importing custom content and copying custom binaries during patching. (#286)

---
+++ src/ContentRepository/Packaging/Steps/Internal/ImportCustomContent.cs
@@ -0,0 +1,28 @@
+﻿using SenseNet.ContentRepository;
+using System.IO;
+using System.Linq;
+
+namespace SenseNet.Packaging.Steps.Internal
+{
+    public class ImportCustomContent : Import
+    {
+        public ImportCustomContent()
+        {
+            Source = "Customization\\import";
+            SourceIsRelativeTo = PathRelativeTo.Package;
+            Target = Repository.RootPath;
+        }
+
+        public override void Execute(ExecutionContext context)
+        {
+            var source = ResolvePackagePath(Source, context);
+            if (!Directory.Exists(source) || !Directory.EnumerateFileSystemEntries(source).Any())
+            {
+                Logger.LogMessage("No custom content to import.");
+                return;
+            }
+
+            base.Execute(context);
+        }
+    }
+}
