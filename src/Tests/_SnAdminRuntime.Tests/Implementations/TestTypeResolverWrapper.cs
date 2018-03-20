ee18a0dc1e5e5f879ec45915b46334925c556bbd
Zoltán Gyebrovszki <kavics@sensenet.com>
2018-03-20 04:31:24 +0100

    Rebuild SnAdminRuntime.Tests

---
+++ src/Tests/_SnAdminRuntime.Tests/Implementations/TestTypeResolverWrapper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+using SenseNet.Tools.SnAdmin.Testability;
+
+namespace SnAdminRuntime.Tests.Implementations
+{
+    public class TestTypeResolverWrapper : ITypeResolverWrapper
+    {
+        public StringBuilder Log { get; } = new StringBuilder();
+
+        public object CreateInstance(string typeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object CreateInstance(string typeName, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T CreateInstance<T>(string typeName) where T : new()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T CreateInstance<T>(string typeName, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type FindTypeInAppDomain(string typeName, bool throwOnError = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Assembly[] GetAssemblies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type GetType(string typeName, bool throwOnError = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type[] GetTypesByBaseType(Type baseType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type[] GetTypesByInterface(Type interfaceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] LoadAssembliesFrom(string path)
+        {
+            Log.AppendLine($"CALL: LoadAssembliesFrom({path})");
+            return new string[0];
+        }
+    }
+}
---
+++ src/Tests/_SnAdminRuntime.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SnAdminRuntime.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SnAdminRuntime.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eca9a043-5b86-445c-b70e-843573aaf72e")]
+
+[assembly: AssemblyVersion("7.0.0.0")]
+[assembly: AssemblyFileVersion("7.0.0.0")]
+[assembly: AssemblyInformationalVersion("7.0.0")]
