0e0c7667c40ba2a8a0e3b77835cf246636dc38e2
tusmester <tusmester@gmail.com>
2018-03-26 15:21:26 +0200

    Update indexing activities table with table lock to avoid loading activities multiple times.

--- src/Search/Indexing/IndexDocument.cs
+++ src/Search/Indexing/IndexDocument.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -57,28 +56,7 @@ namespace SenseNet.Search.Indexing
         [NonSerialized]
         public static List<string> ForbiddenFields = new List<string>(new[] { "Password", "PasswordHash" });
 
-        private readonly IDictionary<string, IndexField> _fields = new ConcurrentDictionary<string, IndexField>();
-
-        /// <summary>
-        /// Initializes a new instance of the IndexDocument class.
-        /// </summary>
-        public IndexDocument() { }
-        /// <summary>
-        /// Initializes a new instance of the IndexDocument class.
-        /// </summary>
-        /// <param name="fields">Initial field collection.</param>
-        public IndexDocument(IDictionary<string, IndexField> fields)
-        {
-            _fields =  new ConcurrentDictionary<string, IndexField>(fields);
-        }
-
-        /// <summary>
-        /// Converts the field collection in this index document to a dictionary for serialization scenarios.
-        /// </summary>
-        public Dictionary<string, IndexField> ToDictionary()
-        {
-            return new Dictionary<string, IndexField>(_fields);
-        }
+        private readonly IDictionary<string, IndexField> _fields = new Dictionary<string, IndexField>();
 
         /// <summary>
         /// Returns with VersionId. Shortcut of the following call: GetIntegerValue(IndexFieldName.VersionId);
