817bc6821de99fa89d16ed555f937920fe0da321
tusmester <tusmester@gmail.com>
2018-05-03 18:43:02 +0200

    Add ClientMetadata provider with tests.

---
+++ src/Services/Metadata/IMetadataProvider.cs
@@ -0,0 +1,17 @@
+﻿using SenseNet.ContentRepository.Schema.Metadata;
+
+namespace SenseNet.Services.Metadata
+{
+    /// <summary>
+    /// Defines methods for a metadata provider that converts and possibly stores schema
+    /// items in a format appropriate for the client.
+    /// </summary>
+    public interface IClientMetadataProvider
+    {
+        /// <summary>
+        /// Gets a - possibly cached - object that represents a schema class 
+        /// and can be serialized and sent to the client.
+        /// </summary>
+        object GetClientMetaClass(Class schemaClass);
+    }
+}
---
+++ src/Tests/SenseNet.ContentRepository.Tests/ClientMetadataProviderTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using SenseNet.ContentRepository.Schema;
+using SenseNet.ContentRepository.Schema.Metadata;
+using SenseNet.Services.Metadata;
+using SenseNet.Tests;
+
+namespace SenseNet.ContentRepository.Tests
+{
+    [TestClass]
+    public class ClientMetadataProviderTests : TestBase
+    {
+        [TestMethod]
+        [TestCategory("Metadata")]
+        public void ClientMetadataProvider_ExistingType()
+        {
+            Test(() =>
+            {
+                var fileClass = new Class(ContentType.GetByName("File"));
+                var fileTypeObject = ClientMetadataProvider.Instance.GetClientMetaClass(fileClass) as JObject;
+
+                Assert.IsNotNull(fileTypeObject);
+                Assert.AreEqual("File", fileTypeObject["ContentTypeName"].Value<string>());
+                Assert.IsNotNull(fileTypeObject["DisplayName"]);
+                Assert.IsNotNull(fileTypeObject["Description"]);
+                Assert.IsNotNull(fileTypeObject["Icon"]);
+                Assert.IsNotNull(fileTypeObject["ParentTypeName"]);
+                Assert.IsNotNull(fileTypeObject["AllowIndexing"]);
+                Assert.IsNotNull(fileTypeObject["AllowIncrementalNaming"]);
+                Assert.IsNotNull(fileTypeObject["AllowedChildTypes"]);
+                Assert.IsNotNull(fileTypeObject["FieldSettings"]);
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("Metadata")]
+        public void ClientMetadataProvider_NewType()
+        {
+            var contentTypeName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+            var fieldName1 = "Field123456";
+
+            Test(() =>
+            {
+                var myType = ContentType.GetByName(contentTypeName);
+
+                Assert.IsNull(myType);
+
+                // register a new content type
+                ContentTypeInstaller.InstallContentType($@"<?xml version='1.0' encoding='utf-8'?>
+                <ContentType name='{contentTypeName}' parentType='GenericContent'
+                         handler='{typeof(GenericContent).FullName}' xmlns='http://schemas.sensenet.com/SenseNet/ContentRepository/ContentTypeDefinition'>
+                    <Fields>
+                        <Field name='{fieldName1}' type='ShortText'></Field>
+                    </Fields>
+                </ContentType>
+                ");
+
+                // this should return the new type now
+                myType = ContentType.GetByName(contentTypeName);
+
+                var myClass = new Class(myType);
+                var myTypeObject = ClientMetadataProvider.Instance.GetClientMetaClass(myClass) as JObject;
+
+                Assert.IsNotNull(myTypeObject);
+                Assert.AreEqual(contentTypeName, myTypeObject["ContentTypeName"].Value<string>());
+
+                var field = myTypeObject["FieldSettings"].Values<JToken>().First(f => f["Name"].Value<string>() == fieldName1);
+
+                Assert.AreEqual("ShortTextFieldSetting", field["Type"].Value<string>());
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("Metadata")]
+        public void ClientMetadataProvider_GetSchema_AllTypes()
+        {
+            Test(() =>
+            {
+                var allTypeObjects = ClientMetadataProvider.GetSchema(null) as object[];
+
+                Assert.IsNotNull(allTypeObjects);
+                Assert.IsTrue(allTypeObjects.Length > 1);
+
+                // check a few unrelated types
+                Assert.IsTrue(allTypeObjects.Cast<JObject>().Any(jo => jo["ContentTypeName"].Value<string>() == "File"));
+                Assert.IsTrue(allTypeObjects.Cast<JObject>().Any(jo => jo["ContentTypeName"].Value<string>() == "Folder"));
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("Metadata")]
+        public void ClientMetadataProvider_GetSchema_SingleType()
+        {
+            Test(() =>
+            {
+                var typeObjects = ClientMetadataProvider.GetSchema(null, "File") as object[];
+
+                Assert.IsNotNull(typeObjects);
+                Assert.IsTrue(typeObjects.Length == 1);
+
+                var tpyeJObject = typeObjects[0] as JObject;
+
+                Assert.IsNotNull(tpyeJObject);
+                Assert.AreEqual("File", tpyeJObject["ContentTypeName"].Value<string>());
+            });
+        }
+    }
+}
