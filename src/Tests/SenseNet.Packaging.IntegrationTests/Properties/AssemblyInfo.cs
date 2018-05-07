35619cdab59fab5cbca3d06eff547cc5a9b99b47
Zoltán Gyebrovszki <kavics@sensenet.com>
2018-05-07 03:06:01 +0200

    Prepare release.

--- src/Tests/SenseNet.Packaging.IntegrationTests/Properties/AssemblyInfo.cs
+++ src/Tests/SenseNet.Packaging.IntegrationTests/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("sensenet")]
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("7.1.1.0")]
-[assembly: AssemblyFileVersion("7.1.1.0")]
-[assembly: AssemblyInformationalVersion("7.1.1")]
+[assembly: AssemblyVersion("7.1.2.0")]
+[assembly: AssemblyFileVersion("7.1.2.0")]
+[assembly: AssemblyInformationalVersion("7.1.2")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("cf049086-0872-4f55-af6d-6177245cc59e")]
--- src/Tests/SnAdminRuntime.Tests/Properties/AssemblyInfo.cs
+++ src/Tests/SnAdminRuntime.Tests/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("sensenet")]
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("7.1.1.0")]
-[assembly: AssemblyFileVersion("7.1.1.0")]
-[assembly: AssemblyInformationalVersion("7.1.1")]
+[assembly: AssemblyVersion("7.1.2.0")]
+[assembly: AssemblyFileVersion("7.1.2.0")]
+[assembly: AssemblyInformationalVersion("7.1.2")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("eca9a043-5b86-445c-b70e-843573aaf72e")]
