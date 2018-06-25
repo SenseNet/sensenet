e44f04a85bbcdb3770ea97b183367583629f3759
tusmester <tusmester@gmail.com>
2018-06-25 15:20:16 +0200

    Convert the Common project to a netstandard project.

--- src/Common/Properties/AssemblyInfo.cs
+++ src/Common/Properties/AssemblyInfo.cs
@@ -6,19 +6,4 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("SenseNet.Packaging.Tests")]
 [assembly: InternalsVisibleTo("SenseNet.Packaging.IntegrationTests")]
 
-#if DEBUG
-[assembly: AssemblyTitle("SenseNet.Common (Debug)")]
-#else
-[assembly: AssemblyTitle("SenseNet.Common (Release)")]
-#endif
-
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Sense/Net Inc.")]
-[assembly: AssemblyCopyright("Copyright © Sense/Net Inc.")]
-[assembly: AssemblyProduct("sensenet")]
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("7.1.1.0")]
-[assembly: AssemblyFileVersion("7.1.1.0")]
-[assembly: AssemblyInformationalVersion("7.1.1")]
