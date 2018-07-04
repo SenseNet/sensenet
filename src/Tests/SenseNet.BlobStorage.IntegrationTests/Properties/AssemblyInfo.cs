79bae0ce8e0bd901115e958501e74aea631d6040
Zoltán Gyebrovszki <kavics@sensenet.com>
2018-07-04 07:15:04 +0200

    Add integration test project for the blobs.

---
+++ src/Tests/SenseNet.BlobStorage.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SenseNet.BlobStorage.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SenseNet.BlobStorage.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f7752ca-3178-4a8a-81ae-8ff50571de8c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Tests/SenseNet.BlobStorage.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SenseNet.BlobStorage.IntegrationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static string _connectionString = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=sn7tests;Data Source=.\SQL2016";
+
+        private enum TestMode{BuiltIn, BuiltInFs, Legacy, LegacyFs};
+        private static readonly Dictionary<TestMode, string> ConnectionStrings = new Dictionary<TestMode, string>
+        {
+            {TestMode.BuiltIn, @"Initial Catalog=sn7tests;Data Source=.\SQL2016;Integrated Security=SSPI;Persist Security Info=False"},
+            {TestMode.BuiltInFs, ""},
+            {TestMode.Legacy, ""},
+            {TestMode.LegacyFs, ""},
+        };
+
+        [TestMethod]
+        public void Blob_BuiltIn_()
+        {
+        }
+        [TestMethod]
+        public void Blob_BuiltInFS_()
+        {
+        }
+        [TestMethod]
+        public void Blob_Legacy_()
+        {
+        }
+        [TestMethod]
+        public void Blob_LegacyFS_()
+        {
+        }
+    }
+}
