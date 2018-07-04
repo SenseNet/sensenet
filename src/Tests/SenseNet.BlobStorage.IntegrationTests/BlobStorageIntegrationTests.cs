457f3e97a186278f039918b5d97c16ad640d375c
Zoltán Gyebrovszki <kavics@sensenet.com>
2018-07-04 10:25:14 +0200

    Writing test infrastructure.

---
+++ src/Tests/SenseNet.BlobStorage.IntegrationTests/BlobStorageIntegrationTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.ContentRepository.Storage.Data;
+
+namespace SenseNet.BlobStorage.IntegrationTests
+{
+    [TestClass]
+    public abstract class BlobStorageIntegrationTests
+    {
+        private static readonly string ConnetionStringBase = @"Data Source=.\SQL2016;Integrated Security=SSPI;Persist Security Info=False";
+
+        private static string GetConnectionString(string databaseName = null)
+        {
+            return $"Initial Catalog={databaseName};{ConnetionStringBase}";
+        }
+
+
+        protected static void EnsureDatabase(string databaseName)
+        {
+            var dbid = ExecuteSqlScalar<int?>($"SELECT database_id FROM sys.databases WHERE Name = '{databaseName}'", "master");
+            if (dbid == null)
+            {
+                // CREATE DATABASE
+                // load sql file
+                var sqlDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                    @"..\..\..\..\Storage\Data\SqlClient\Scripts"));
+                var sqlPath = Path.Combine(sqlDirectory, "Create_SenseNet_Database_Templated.sql");
+
+throw new NotImplementedException();
+            }
+        }
+        private static void ExecuteSqlCommand(string sql, string databaseName)
+        {
+            var backup = Configuration.ConnectionStrings.ConnectionString;
+            Configuration.ConnectionStrings.ConnectionString = GetConnectionString(databaseName);
+            try
+            {
+                var proc = DataProvider.CreateDataProcedure(sql);
+                proc.CommandType = CommandType.Text;
+                proc.ExecuteNonQuery();
+            }
+            finally
+            {
+                Configuration.ConnectionStrings.ConnectionString = backup;
+            }
+        }
+        private static T ExecuteSqlScalar<T>(string sql, string databaseName)
+        {
+            var backup = Configuration.ConnectionStrings.ConnectionString;
+            Configuration.ConnectionStrings.ConnectionString = GetConnectionString(databaseName);
+            try
+            {
+                var proc = DataProvider.CreateDataProcedure(sql);
+                proc.CommandType = CommandType.Text;
+                return (T)proc.ExecuteScalar();
+            }
+            finally
+            {
+                Configuration.ConnectionStrings.ConnectionString = backup;
+            }
+        }
+
+        [TestMethod]
+        public void Blob_CreateFile()
+        {
+            Assert.Inconclusive();
+        }
+
+    }
+}
--- src/Tests/SenseNet.BlobStorage.IntegrationTests/UnitTest1.cs
+++ src/Tests/SenseNet.BlobStorage.IntegrationTests/UnitTest1.cs
@@ -1,38 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SenseNet.BlobStorage.IntegrationTests
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest1 : BlobStorageIntegrationTests
     {
-        private static string _connectionString = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=sn7tests;Data Source=.\SQL2016";
+        // private enum TestMode{BuiltIn, BuiltInFs, Legacy, LegacyFs};
 
-        private enum TestMode{BuiltIn, BuiltInFs, Legacy, LegacyFs};
-        private static readonly Dictionary<TestMode, string> ConnectionStrings = new Dictionary<TestMode, string>
-        {
-            {TestMode.BuiltIn, @"Initial Catalog=sn7tests;Data Source=.\SQL2016;Integrated Security=SSPI;Persist Security Info=False"},
-            {TestMode.BuiltInFs, ""},
-            {TestMode.Legacy, ""},
-            {TestMode.LegacyFs, ""},
-        };
+        protected static string DatabaseName => "sn7blobtests";
 
-        [TestMethod]
-        public void Blob_BuiltIn_()
+        [ClassInitialize]
+        public static void InitializeClass(TestContext testContext)
         {
+            EnsureDatabase(DatabaseName);
         }
-        [TestMethod]
-        public void Blob_BuiltInFS_()
-        {
-        }
-        [TestMethod]
-        public void Blob_Legacy_()
-        {
-        }
-        [TestMethod]
-        public void Blob_LegacyFS_()
-        {
-        }
+
     }
 }
