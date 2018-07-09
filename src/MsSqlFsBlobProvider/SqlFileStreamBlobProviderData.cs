e72cad08a845309bf331e4ae5ad84e5783117eb8
Zoltán Gyebrovszki <kavics@sensenet.com>
2018-07-09 04:05:25 +0200

    Add files to MsSqlFs project.

---
+++ src/MsSqlFsBlobProvider/SqlFileStreamBlobProviderData.cs
@@ -0,0 +1,24 @@
+﻿namespace SenseNet.MsSqlFsBlobProvider
+{
+    /// <summary>
+    /// Blob provider data for the built-in blob provider.
+    /// </summary>
+    internal class SqlFileStreamBlobProviderData
+    {
+        /// <summary>
+        /// Custom data for the Filestream column.
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("FileStreamData")]
+        public SqlFileStreamData FileStreamData { get; set; }
+    }
+}
+
+namespace SenseNet.ContentRepository.Storage.Data.SqlClient
+{
+    /// <summary>
+    /// This class is used in legacy databases.
+    /// </summary>
+    internal class BuiltinBlobProviderData : MsSqlFsBlobProvider.SqlFileStreamBlobProviderData
+    {
+    }
+}
---
+++ src/MsSqlFsBlobProvider/SqlFileStreamData.cs
@@ -0,0 +1,21 @@
+﻿namespace SenseNet.MsSqlFsBlobProvider
+{
+    /// <summary>
+    /// Holds context information for SqlFileStream operations.
+    /// </summary>
+    internal class SqlFileStreamData
+    {
+        public string Path { get; set; }
+        public byte[] TransactionContext { get; set; }
+    }
+}
+
+namespace SenseNet.ContentRepository.Storage.Data
+{
+    /// <summary>
+    /// This class is used in legacy databases.
+    /// </summary>
+    internal class FileStreamData : MsSqlFsBlobProvider.SqlFileStreamData
+    {
+    }
+}
