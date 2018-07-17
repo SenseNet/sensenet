7e16ca3d44ed124e3cb7f3ff31367ceea5198061
Zoltán Gyebrovszki <kavics@sensenet.com>
2018-07-17 04:44:00 +0200

    Cleanup and set assemblyinfo.cs

--- src/MsSqlFsBlobProvider/Properties/AssemblyInfo.cs
+++ src/MsSqlFsBlobProvider/Properties/AssemblyInfo.cs
@@ -4,35 +4,22 @@ using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("SenseNet.BlobStorage.IntegrationTests")]
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("MsSqlFsBlobProvider")]
+#if DEBUG
+[assembly: AssemblyTitle("SenseNet.MsSqlFsBlobProvider (Debug)")]
+#else
+[assembly: AssemblyTitle("SenseNet.MsSqlFsBlobProvider (Release)")]
+#endif
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MsSqlFsBlobProvider")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Sense/Net Inc.")]
+[assembly: AssemblyCopyright("Copyright © Sense/Net Inc.")]
+[assembly: AssemblyProduct("sensenet")]
+[assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7ec0d2c0-9e97-4c60-bf23-fe4e3fa5bfc8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("7ec0d2c0-9e97-4c60-bf23-fe4e3fa5bfc8")]
