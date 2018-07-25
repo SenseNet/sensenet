7f8c9d2d6920b55793b19015a71af831b797c2e4
Zoltán Gyebrovszki <kavics@sensenet.com>
2018-07-25 02:55:43 +0200

    Move logger adapter implementation and formatters to the temp project.

---
+++ src/SenseNet.EntlibLogger/SnXmlFormatter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Logging.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Logging.Formatters;
+using System.Collections.Specialized;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+using System.Web;
+using System.Xml;
+
+namespace SenseNet.ContentRepository.Storage.Diagnostics
+{
+    [ConfigurationElementType(typeof(CustomFormatterData))]
+    public class SnXmlFormatter : ILogFormatter
+    {
+        private NameValueCollection _x;
+
+        public SnXmlFormatter(NameValueCollection x)
+        {
+            _x = x;
+        }
+
+        public string Format(LogEntry log)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendFormat("<LogEntry>").AppendLine();
+            sb.AppendFormat("  <Timestamp>{0}</Timestamp>", log.TimeStampString).AppendLine();
+            sb.AppendFormat("  <Message>{0}</Message>", log.Message).AppendLine();
+            sb.AppendFormat("  <Category>{0}</Category>", log.CategoriesStrings).AppendLine();
+            sb.AppendFormat("  <Priority>{0}</Priority>", log.Priority).AppendLine();
+            sb.AppendFormat("  <EventId>{0}</EventId>", log.EventId).AppendLine();
+            sb.AppendFormat("  <Severity>{0}</Severity>", log.Severity).AppendLine();
+            sb.AppendFormat("  <Title>{0}</Title>", log.Title).AppendLine();
+            sb.AppendFormat("  <Machine>{0}</Machine>", log.MachineName).AppendLine();
+            sb.AppendFormat("  <ApplicationDomain>{0}</ApplicationDomain>", log.AppDomainName).AppendLine();
+            sb.AppendFormat("  <ProcessId>{0}</ProcessId>", log.ProcessId).AppendLine();
+            sb.AppendFormat("  <ProcessName>{0}</ProcessName>", log.ProcessName).AppendLine();
+            sb.AppendFormat("  <Win32ThreadId>{0}</Win32ThreadId>", log.Win32ThreadId).AppendLine();
+            sb.AppendFormat("  <ThreadName>{0}</ThreadName>", log.ManagedThreadName).AppendLine();
+            sb.AppendFormat("  <ExtendedProperties>").AppendLine();
+            foreach (var prop in log.ExtendedProperties)
+                sb.AppendFormat("    <{0}>{1}</{0}>", prop.Key, FormatValue(prop.Value)).AppendLine();
+            sb.AppendFormat("  </ExtendedProperties>").AppendLine();
+            sb.AppendFormat("</LogEntry>").AppendLine();
+
+            return sb.ToString();
+        }
+
+        private string FormatValue(object obj)
+        {
+            if (obj == null)
+                return string.Empty;
+            if (obj is DateTime)
+                return XmlConvert.ToString((DateTime)obj, XmlDateTimeSerializationMode.Utc);
+
+            var s = obj as string ?? obj.ToString();
+            var mustEscape = s.Any(c => c == '<' || c == '>' || c == '&' || c == '"' || c == '\'');
+
+            return mustEscape ? HttpUtility.HtmlEncode(s) : s;
+        }
+    }
+}
+
--- src/Storage/Diagnostics/EntlibLoggerAdapter.cs
+++ src/Storage/Diagnostics/EntlibLoggerAdapter.cs
@@ -1,84 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Diagnostics;
-using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
-using Microsoft.Practices.EnterpriseLibrary.Logging;
-using Microsoft.Practices.EnterpriseLibrary.Logging.Configuration;
-using Microsoft.Practices.EnterpriseLibrary.Logging.TraceListeners;
+﻿//UNDONE: DELETE this file
 
-namespace SenseNet.Diagnostics
-{
-    public class EntLibLoggerAdapter : IEventLogger
-    {
-        public void Write(object message, ICollection<string> categories, int priority, int eventId, TraceEventType severity, string title, IDictionary<string, object> properties)
-        {
-            var props = Utility.CollectAutoProperties(properties);
-            var eventTypeName = severity.ToString().ToUpper();
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Diagnostics;
+//using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+//using Microsoft.Practices.EnterpriseLibrary.Logging;
+//using Microsoft.Practices.EnterpriseLibrary.Logging.Configuration;
+//using Microsoft.Practices.EnterpriseLibrary.Logging.TraceListeners;
 
-            if (SnTrace.Event.Enabled)
-            {
-                if (severity <= TraceEventType.Information) // Critical = 1, Error = 2, Warning = 4, Information = 8
-                {
-                    var id = "#" + Guid.NewGuid().ToString();
-                    props["SnTrace"] = id;
-                    SnTrace.Event.Write("{0} {1}: {2}", eventTypeName, id, message);
-                }
-                else
-                {
-                    object id = "-";
-                    object path = "-";
-                    if (properties != null)
-                    {
-                        properties.TryGetValue("Id", out id);
-                        properties.TryGetValue("Path", out path);
-                    }
+//namespace SenseNet.Diagnostics
+//{
+//    public class EntLibLoggerAdapter : IEventLogger
+//    {
+//        public void Write(object message, ICollection<string> categories, int priority, int eventId, TraceEventType severity, string title, IDictionary<string, object> properties)
+//        {
+//            var props = Utility.CollectAutoProperties(properties);
+//            var eventTypeName = severity.ToString().ToUpper();
 
-                    if (categories.Count == 1 && categories.First() == "Audit")
-                        eventTypeName = "Audit";
-                    SnTrace.Event.Write("{0}: {1}, Id:{2}, Path:{3}", eventTypeName, message, id, path);
-                }
-            }
+//            if (SnTrace.Event.Enabled)
+//            {
+//                if (severity <= TraceEventType.Information) // Critical = 1, Error = 2, Warning = 4, Information = 8
+//                {
+//                    var id = "#" + Guid.NewGuid().ToString();
+//                    props["SnTrace"] = id;
+//                    SnTrace.Event.Write("{0} {1}: {2}", eventTypeName, id, message);
+//                }
+//                else
+//                {
+//                    object id = "-";
+//                    object path = "-";
+//                    if (properties != null)
+//                    {
+//                        properties.TryGetValue("Id", out id);
+//                        properties.TryGetValue("Path", out path);
+//                    }
 
-            Microsoft.Practices.EnterpriseLibrary.Logging.Logger.Write(
-                message ?? String.Empty, categories ?? null, 
-                priority, eventId, severity, title ?? string.Empty, props);
-        }
+//                    if (categories.Count == 1 && categories.First() == "Audit")
+//                        eventTypeName = "Audit";
+//                    SnTrace.Event.Write("{0}: {1}, Id:{2}, Path:{3}", eventTypeName, message, id, path);
+//                }
+//            }
 
-        public void Write<T>(object message, ICollection<string> categories, int priority, int eventId,
-            TraceEventType severity, string title, Func<T, IDictionary<string, object>> getPropertiesCallback, T callbackArg)
-        {
-            var properties = getPropertiesCallback != null
-                                 ? getPropertiesCallback(callbackArg)
-                                 : callbackArg as IDictionary<string, object>;
+//            Microsoft.Practices.EnterpriseLibrary.Logging.Logger.Write(
+//                message ?? String.Empty, categories ?? null, 
+//                priority, eventId, severity, title ?? string.Empty, props);
+//        }
 
-            Write(message, categories, priority, eventId, severity, title, properties);
-        }
-    }
+//        public void Write<T>(object message, ICollection<string> categories, int priority, int eventId,
+//            TraceEventType severity, string title, Func<T, IDictionary<string, object>> getPropertiesCallback, T callbackArg)
+//        {
+//            var properties = getPropertiesCallback != null
+//                                 ? getPropertiesCallback(callbackArg)
+//                                 : callbackArg as IDictionary<string, object>;
 
-    [ConfigurationElementType(typeof(CustomTraceListenerData))]
-    public class OneLineTraceListener : CustomTraceListener
-    {
-        public override void TraceData(TraceEventCache eventCache, string source, TraceEventType eventType, int id, object data)
-        {
-            if (data is LogEntry && this.Formatter != null)
-            {
-                this.WriteLine(this.Formatter.Format(data as LogEntry));
-            }
-            else
-            {
-                this.WriteLine(data.ToString());
-            }
-        }
-        public override void Write(string message)
-        {
-            Debug.Write(message);
-        }
-        public override void WriteLine(string message)
-        {
-            Debug.WriteLine(message);
-        }
-    }
+//            Write(message, categories, priority, eventId, severity, title, properties);
+//        }
+//    }
 
-}
+//    [ConfigurationElementType(typeof(CustomTraceListenerData))]
+//    public class OneLineTraceListener : CustomTraceListener
+//    {
+//        public override void TraceData(TraceEventCache eventCache, string source, TraceEventType eventType, int id, object data)
+//        {
+//            if (data is LogEntry && this.Formatter != null)
+//            {
+//                this.WriteLine(this.Formatter.Format(data as LogEntry));
+//            }
+//            else
+//            {
+//                this.WriteLine(data.ToString());
+//            }
+//        }
+//        public override void Write(string message)
+//        {
+//            Debug.Write(message);
+//        }
+//        public override void WriteLine(string message)
+//        {
+//            Debug.WriteLine(message);
+//        }
+//    }
+
+//}
