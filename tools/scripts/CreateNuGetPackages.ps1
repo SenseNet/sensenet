d48d07730bf4e310c9a70fe1f3cd509fb96ff91f
Zoltán Gyebrovszki <kavics@sensenet.com>
2018-07-27 00:27:17 +0200

    Add/Modify nuspecs with temporary versions.

--- tools/scripts/CreateNuGetPackages.ps1
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -7,6 +7,7 @@ Remove-Item $PSScriptRoot\*.nupkg
 
 nuget pack $srcPath\Services\SenseNet.Services.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
 nuget pack $srcPath\Tests\SenseNet.Tests\SenseNet.Tests.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
+nuget pack $srcPath\SenseNet.EntlibLogger\SenseNet.EntlibLogger.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
 
 New-Item $srcPath\nuget\snadmin\install-services\scripts -ItemType directory -Force
 
