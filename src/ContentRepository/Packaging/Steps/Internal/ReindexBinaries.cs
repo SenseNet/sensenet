f991a70a8f8636c59e2300d11b47a768fd3f29be
Zoltán Gyebrovszki <kavics@sensenet.com>
2018-08-29 01:48:42 +0200

    BinaryReindex step initial.

---
+++ src/ContentRepository/Packaging/Steps/Internal/ReindexBinaries.cs
@@ -0,0 +1,76 @@
+﻿using System.Threading.Tasks;
+using SenseNet.ContentRepository.Search;
+using SenseNet.ContentRepository.Storage;
+using SenseNet.ContentRepository.Storage.Security;
+
+// ReSharper disable CheckNamespace
+namespace SenseNet.Packaging.Steps.Internal
+{
+    public partial class ReindexBinaries : Step
+    {
+        public override void Execute(ExecutionContext context)
+        {
+            DataHandler.InstallTables();
+            ReindexMetadata();
+            DataHandler.StartBackgroundTasks();
+        }
+        private void ReindexMetadata()
+        {
+            using (new SystemAccount())
+            {
+                Parallel.ForEach(new NodeList<Node>(DataHandler.GetAllNodeIds(0)), n =>
+                {
+                    foreach (var node in n.LoadVersions())
+                        ReindexNode(node);
+                });
+            }
+        }
+        private void ReindexNode(Node node)
+        {
+            var indx = DataBackingStore.SaveIndexDocument(node, true, false, out var hasBinary);
+            if (hasBinary)
+                CreateBinaryReindexTask(node.VersionId,
+                    indx.IsLastPublic ? 1 : indx.IsLastDraft ? 2 : 3);
+        }
+        private void CreateBinaryReindexTask(int versionId, int rank)
+        {
+            DataHandler.CreateTempTask(versionId, rank);
+        }
+
+        /* =============================================================== */
+        private static bool _featureIsRunning;
+        private static bool _featureIsRequested;
+        /// <summary>
+        /// Gets some background task from the database and executes them.
+        /// Called by the SnMaintenance.
+        /// </summary>
+        public static void GetBackgroundTasksAndExecute()
+        {
+            if (_featureIsRunning)
+            {
+                _featureIsRequested = true;
+                return;
+            }
+            _featureIsRunning = true;
+            _featureIsRequested = false;
+            do
+            {
+                foreach (var versionId in DataHandler.AssignTasks(100, 1))
+                {
+                    ReindexBinaryProperties(versionId);
+                    DataHandler.FinishTask(versionId);
+                }
+            } while (_featureIsRequested);
+            _featureIsRunning = false;
+        }
+        private static void ReindexBinaryProperties(int versionId)
+        {
+            using (new SystemAccount())
+            {
+                var node = Node.LoadNodeByVersionId(versionId);
+                var indx = SearchManager.LoadIndexDocumentByVersionId(versionId);
+                DataBackingStore.SaveIndexDocument(node, indx);
+            }
+        }
+    }
+}
---
+++ src/ContentRepository/Packaging/Steps/Internal/ReindexBinariesDataHandler.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using SenseNet.ContentRepository.Storage.Data;
+
+// ReSharper disable once CheckNamespace
+namespace SenseNet.Packaging.Steps.Internal
+{
+    public partial class ReindexBinaries
+    {
+        private static class DataHandler
+        {
+            internal static void InstallTables()
+            {
+                // proc CreateTable
+                using (var cmd = DataProvider.CreateDataProcedure(SqlScripts.CreateTables))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            internal static void StartBackgroundTasks()
+            {
+                using (var cmd = DataProvider.CreateDataProcedure(SqlScripts.CreateTasks))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            internal static int[] AssignTasks(int taskCount, int timeoutInMinutes)
+            {
+                var result = new List<int>();
+
+                // proc AssignTasks(@AssignedTaskCount int, @TimeOutInMinutes int)
+                using (var cmd = DataProvider.CreateDataProcedure(SqlScripts.AssignTasks))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(CreateParameter("@AssignedTaskCount", taskCount, DbType.Int32));
+                    cmd.Parameters.Add(CreateParameter("@TimeOutInMinutes", timeoutInMinutes, DbType.Int32));
+
+                    using (var reader = cmd.ExecuteReader())
+                        while (reader.Read())
+                            result.Add(reader.GetInt32(0));
+                }
+
+                return result.ToArray();
+            }
+
+            internal static void FinishTask(int versionId)
+            {
+                // proc FinishTask(@VersionId int)
+                using (var cmd = DataProvider.CreateDataProcedure(SqlScripts.FinishTask))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(CreateParameter("@VersionId", versionId, DbType.Int32));
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            /* ========================================================================================= */
+
+            private static IDbDataParameter CreateParameter(string name, object value, DbType dbType)
+            {
+                var prm = DataProvider.CreateParameter();
+                prm.ParameterName = name;
+                prm.DbType = dbType;
+                prm.Value = value;
+                return prm;
+            }
+
+            /* ========================================================================================= */
+
+            public static void CreateTempTask(int versionId, int rank)
+            {
+                // proc CreateTempTask(@VersionId int, @Rank int)
+                using (var cmd = DataProvider.CreateDataProcedure(SqlScripts.CreateTempTask))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(CreateParameter("@VersionId", versionId, DbType.Int32));
+                    cmd.Parameters.Add(CreateParameter("@Rank", rank, DbType.Int32));
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            public static IEnumerable<int> GetAllNodeIds(int from)
+            {
+                var result = new List<int>();
+
+                // proc GetAllNodeIds(@From int)
+                using (var cmd = DataProvider.CreateDataProcedure(SqlScripts.GetAllNodeIds))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(CreateParameter("@From", from, DbType.Int32));
+
+                    using (var reader = cmd.ExecuteReader())
+                        while (reader.Read())
+                            result.Add(reader.GetInt32(0));
+                }
+
+                return result;
+            }
+        }
+
+        private static class SqlScripts
+        {
+            private const string TempTableName = "Maintenance.BinaryReindexingTemp";
+            private const string TaskTableName = "Maintenance.BinaryReindexingTasks";
+
+            #region CreateTables
+            internal static readonly string CreateTables =
+                $@"
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[{TempTableName}]') AND type in (N'U'))
+	DROP TABLE [dbo].[{TempTableName}]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[{TempTableName}](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[VersionId] [int] NOT NULL,
+	[Rank] [int] NOT NULL,
+ CONSTRAINT [PK_{TempTableName}] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_MFRTemp_Rank] ON [dbo].[{TempTableName}]
+(
+	[Rank] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
+/*------------------------------------*/
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[{TaskTableName}]') AND type in (N'U'))
+	DROP TABLE [dbo].[{TaskTableName}]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[{TaskTableName}](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[VersionId] [int] NOT NULL,
+	[StartDate] [datetime] NULL,
+	[EndDate] [datetime] NULL,
+ CONSTRAINT [PK_{TaskTableName}] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_MFRTask_EndDate] ON [dbo].[{TaskTableName}]
+(
+	[EndDate] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_MFRTask_StartDate] ON [dbo].[{TaskTableName}]
+(
+	[StartDate] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IX_MFRTask_VersionId] ON [dbo].[{TaskTableName}]
+(
+	[VersionId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+";
+            #endregion
+
+            #region GetAllNodeIds(@From int)
+            internal static readonly string GetAllNodeIds =
+                @"SELECT NodeId FROM Nodes WHERE NodeId >= @From ORDER BY NodeId
+";
+            #endregion
+
+            #region CreateTempTask(@VersionId int, @Rank int)
+            internal static readonly string CreateTempTask = $@"NSERT INTO {TempTableName} (VersionId, Rank) VALUES (@VersionId, @Rank)
+";
+            #endregion
+
+            #region CreateTasks
+            internal static readonly string CreateTasks = $@"
+INSERT INTO {TaskTableName}
+	SELECT VersionId, NULL, NULL FROM {TempTableName} ORDER BY [Rank]
+
+TRUNCATE TABLE {TempTableName}
+";
+            #endregion
+
+            #region AssignTasks(@AssignedTaskCount int, @TimeOutInMinutes int)
+            internal static readonly string AssignTasks =
+                $@"UPDATE {TaskTableName} WITH (TABLOCK) SET StartDate = GETUTCDATE()
+OUTPUT INSERTED.VersionId, INSERTED.StartDate
+WHERE VersionId IN (SELECT TOP (@AssignedTaskCount) VersionId FROM {TaskTableName}
+	WHERE EndDate IS NULL AND (StartDate IS NULL OR StartDate < DATEADD(MINUTES, -@TimeOutInMinutes, GETUTCDATE())))
+";
+            #endregion
+
+            #region FinishTask(@VersionId int)
+            internal static readonly string FinishTask =
+                $@"UPDATE {TaskTableName} WITH (TABLOCK) SET EndDate = GETUTCDATE()
+WHERE VersionId = @VersionId
+";
+            #endregion
+
+        }
+    }
+}
