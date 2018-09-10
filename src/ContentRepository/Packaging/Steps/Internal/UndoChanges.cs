da59e0bd2f7b14922339450c94f710a66d2b783a
tusmester <tusmester@gmail.com>
2018-09-10 10:42:44 +0200

    Add undo changes step and test. (#458)
    * Add undo changes step and test.
    * Add Path parameter to Undo step and add more tests.

---
+++ src/ContentRepository/Packaging/Steps/Internal/UndoChanges.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using SenseNet.ContentRepository;
+using SenseNet.ContentRepository.Storage.Security;
+using SenseNet.Search;
+
+namespace SenseNet.Packaging.Steps.Internal
+{
+    public class UndoChanges : Step
+    {
+        /// <summary>Content types to check for locked status and undo changes. Empty means all types.</summary>
+        [DefaultProperty]
+        [Annotation("Content types to check for locked status and undo changes. Empty means all types.")]
+        public string ContentTypes { get; set; }
+        /// <summary>Path of the subtree where changes should be reverted. Default is the root.</summary>
+        [Annotation("Path of the subtree where changes should be reverted. Default is the root.")]
+        public string Path { get; set; }
+
+        public override void Execute(ExecutionContext context)
+        {
+            context.AssertRepositoryStarted();
+
+            var typeNames = context.ResolveVariable(ContentTypes) as string;
+            if (string.IsNullOrEmpty(typeNames))
+                typeNames = typeof(GenericContent).Name;
+
+            var typeNameArray = typeNames.Split(new[] {',', ';'}, StringSplitOptions.RemoveEmptyEntries);
+            var path = context.ResolveVariable(Path) as string;
+
+            UndoContentChanges(path, typeNameArray);
+        }
+
+        internal static void UndoContentChanges(string path, params string[] typeNameArray)
+        {
+            if (typeNameArray == null || typeNameArray.Length == 0)
+                typeNameArray = new[] {typeof(GenericContent).Name};
+            if (string.IsNullOrEmpty(path))
+                path = "/Root";
+
+            using (new SystemAccount())
+            {
+                Parallel.ForEach(ContentQuery
+                        .Query(SafeQueries.LockedContentByPath, QuerySettings.AdminSettings, typeNameArray, path).Nodes
+                        .Where(n => n is GenericContent).Cast<GenericContent>(),
+                    new ParallelOptions {MaxDegreeOfParallelism = 10},
+                    gc =>
+                    {
+                        Logger.LogMessage($"UNDO changes: {gc.Path}");
+                        gc.UndoCheckOut();
+                    });
+            }
+        }
+    }
+}
---
+++ src/Tests/SenseNet.Packaging.Tests/StepTests/UndoChangesTests.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.ContentRepository;
+using SenseNet.ContentRepository.Storage;
+using SenseNet.Packaging.Steps.Internal;
+using SenseNet.Packaging.Tests.Implementations;
+using SenseNet.Tests;
+
+namespace SenseNet.Packaging.Tests.StepTests
+{
+    [TestClass]
+    public class UndoChangesTests : TestBase
+    {
+        private static StringBuilder _log;
+
+        [TestInitialize]
+        public void PrepareTest()
+        {
+            // preparing logger
+            _log = new StringBuilder();
+            var loggers = new[] {new PackagingTestLogger(_log)};
+            var loggerAcc = new PrivateType(typeof(Logger));
+            loggerAcc.SetStaticField("_loggers", loggers);
+        }
+        [TestCleanup]
+        public void AfterTest()
+        {
+        }
+
+        [TestMethod]
+        public void Step_UndoChanges_All() => Test(() =>
+        {
+            var parent = CretateFolder(Repository.Root, true);
+            var file = CreateFileAndCheckout(parent);
+
+            Assert.IsTrue(GetLockedCount() > 0);
+            Assert.AreEqual(VersionStatus.Locked, file.Version.Status);
+
+            // undo all changes in the repo
+            UndoChanges.UndoContentChanges(null);
+
+            file = Node.Load<File>(file.Id);
+
+            Assert.IsTrue(GetLockedCount() == 0);
+            Assert.AreNotEqual(VersionStatus.Locked, file.Version.Status);
+        });
+        [TestMethod]
+        public void Step_UndoChanges_ByPath() => Test(() =>
+        {
+            var parent = CretateFolder(Repository.Root);
+
+            var file1 = CreateFileAndCheckout(parent);
+            var file2 = CreateFileAndCheckout(parent);
+
+            Assert.AreEqual(VersionStatus.Locked, file1.Version.Status);
+            Assert.AreEqual(VersionStatus.Locked, file2.Version.Status);
+
+            // undo all changes under a path
+            UndoChanges.UndoContentChanges(file1.Path);
+
+            file1 = Node.Load<File>(file1.Id);
+            file2 = Node.Load<File>(file2.Id);
+
+            Assert.AreNotEqual(VersionStatus.Locked, file1.Version.Status);
+            Assert.AreEqual(VersionStatus.Locked, file2.Version.Status);
+        });
+        [TestMethod]
+        public void Step_UndoChanges_ByType() => Test(() =>
+        {
+            var parent = CretateFolder(Repository.Root, true);
+            var file1 = CreateFileAndCheckout(parent);
+
+            parent = Node.Load<SystemFolder>(parent.Id);
+
+            Assert.AreEqual(VersionStatus.Locked, parent.Version.Status);
+            Assert.AreEqual(VersionStatus.Locked, file1.Version.Status);
+
+            // undo all changes by type
+            UndoChanges.UndoContentChanges(null, "File");
+
+            file1 = Node.Load<File>(file1.Id);
+            parent = Node.Load<SystemFolder>(parent.Id);
+
+            Assert.AreEqual(VersionStatus.Locked, parent.Version.Status);
+            Assert.AreNotEqual(VersionStatus.Locked, file1.Version.Status);
+        });
+        [TestMethod]
+        public void Step_UndoChanges_ByTypeAndPath() => Test(() =>
+        {
+            var parent1 = CretateFolder(Repository.Root, true);
+            var file1 = CreateFileAndCheckout(parent1);
+
+            var parent2 = CretateFolder(parent1, true);
+            var file2 = CreateFileAndCheckout(parent2);
+
+            Assert.AreEqual(VersionStatus.Locked, parent1.Version.Status);
+            Assert.AreEqual(VersionStatus.Locked, parent2.Version.Status);
+            Assert.AreEqual(VersionStatus.Locked, file1.Version.Status);
+            Assert.AreEqual(VersionStatus.Locked, file2.Version.Status);
+
+            // undo changes under a path by type
+            UndoChanges.UndoContentChanges(parent2.Path, "File", "SystemFolder");
+
+            parent1 = Node.Load<SystemFolder>(parent1.Id);
+            parent2 = Node.Load<SystemFolder>(parent2.Id);
+            file1 = Node.Load<File>(file1.Id);
+            file2 = Node.Load<File>(file2.Id);
+
+            Assert.AreEqual(VersionStatus.Locked, parent1.Version.Status);
+            Assert.AreEqual(VersionStatus.Locked, file1.Version.Status);
+            Assert.AreNotEqual(VersionStatus.Locked, parent2.Version.Status);            
+            Assert.AreNotEqual(VersionStatus.Locked, file2.Version.Status);
+        });
+
+        private static int GetLockedCount()
+        {
+            return CreateSafeContentQuery("+Locked:true").Execute().Count;
+        }
+        private static File CreateFileAndCheckout(Node parent)
+        {
+            var file = new File(parent) { Name = Guid.NewGuid().ToString() };
+            file.Save();
+
+            //TODO: workaround for pinned node.Content issue
+            file = Node.Load<File>(file.Id);
+            file.CheckOut();
+
+            return file;
+        }
+        private static SystemFolder CretateFolder(Node parent, bool checkout = false)
+        {
+            var folder = new SystemFolder(parent) { Name = Guid.NewGuid().ToString() };
+            folder.Save();
+
+            if (checkout)
+            {
+                folder = Node.Load<SystemFolder>(folder.Id);
+                folder.CheckOut();
+            }
+
+            return folder;
+        }
+    }
+}
