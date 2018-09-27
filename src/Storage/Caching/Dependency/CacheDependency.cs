1c6945385474a7479bb385276548680ca9eec96c
Zoltán Gyebrovszki <kavics@sensenet.com>
2018-09-27 06:46:17 +0200

    Remove az "undone" line.

--- src/Storage/Caching/Dependency/CacheDependency.cs
+++ src/Storage/Caching/Dependency/CacheDependency.cs
@@ -1,6 +1,5 @@
 ﻿namespace SenseNet.ContentRepository.Storage.Caching.Dependency
 {
-    //UNDONE: Ensure that the CacheDependency shared members are not static and they are stored in an object in the Providers instance.
     //UNDONE: Ensure that the CacheDependency is extensible (EventHolder["NodeType"].TheEvent += CallbackMethod;... EventHolder is in the Providers instance
     /// <summary>
     /// Base class of the object that declares various, implementation-independent cache dependency value.
