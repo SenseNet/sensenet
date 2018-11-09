2ada05f6efdd36d9cab87fe140969d3837106678
tusmester <tusmester@gmail.com>
2018-11-09 13:33:18 +0100

    Add Custom OData content to aid response formatting of arrays.

---
+++ src/ContentRepository/OData/ODataCustomContent.cs
@@ -0,0 +1,17 @@
+﻿namespace SenseNet.ContentRepository.OData
+{
+    public class ODataCustomContent
+    {
+        public object Data { get; private set; }
+
+        private ODataCustomContent() { }
+
+        public static ODataCustomContent Create(object data)
+        {
+            return new ODataCustomContent
+            {
+                Data = data
+            };
+        }
+    }
+}
