c718569ab3feeb3580b31ddd9af1a58321d21ec4
Zoltán Gyebrovszki <kavics@sensenet.com>
2018-12-06 00:56:58 +0100

    Extend test http request with header support.

--- src/Tests/SenseNet.Tests/SimulatedHttpRequest.cs
+++ src/Tests/SenseNet.Tests/SimulatedHttpRequest.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Specialized;
 using System.IO;
+using System.Linq;
 using System.Web.Hosting;
+using System.Web.UI;
 
 namespace SenseNet.Tests
 {
@@ -10,17 +13,18 @@ namespace SenseNet.Tests
 	public class SimulatedHttpRequest : SimpleWorkerRequest
 	{
 	    private string _host;
+	    private string[][] _headers;
 
-		/// <summary>
-		/// Creates a new <see cref="SimulatedHttpRequest"/> instance.
-		/// </summary>
-		/// <param name="appVirtualDir">App virtual dir.</param>
-		/// <param name="appPhysicalDir">App physical dir.</param>
-		/// <param name="page">Page.</param>
-		/// <param name="query">Query.</param>
-		/// <param name="output">Output.</param>
-		/// <param name="host">Host.</param>
-		public SimulatedHttpRequest(string appVirtualDir, string appPhysicalDir, string page, string query, TextWriter output, string host)
+        /// <summary>
+        /// Creates a new <see cref="SimulatedHttpRequest"/> instance.
+        /// </summary>
+        /// <param name="appVirtualDir">App virtual dir.</param>
+        /// <param name="appPhysicalDir">App physical dir.</param>
+        /// <param name="page">Page.</param>
+        /// <param name="query">Query.</param>
+        /// <param name="output">Output.</param>
+        /// <param name="host">Host.</param>
+        public SimulatedHttpRequest(string appVirtualDir, string appPhysicalDir, string page, string query, TextWriter output, string host)
 			: base(appVirtualDir, appPhysicalDir, page, query, output)
 		{
 			if (host == null)
@@ -29,12 +33,17 @@ namespace SenseNet.Tests
 				throw new ArgumentException("Host cannot be empty.");
 			_host = host;
 		}
+	    public SimulatedHttpRequest(string appVirtualDir, string appPhysicalDir, string page, string query, TextWriter output, string host, string[][] headers)
+	        : this(appVirtualDir, appPhysicalDir, page, query, output, host)
+	    {
+	        _headers = headers;
+	    }
 
-		/// <summary>
-		/// Gets the name of the server.
-		/// </summary>
-		/// <returns></returns>
-		public override string GetServerName()
+        /// <summary>
+        /// Gets the name of the server.
+        /// </summary>
+        /// <returns></returns>
+        public override string GetServerName()
 		{
 			return _host;
 		}
@@ -48,5 +57,17 @@ namespace SenseNet.Tests
 		{
 			return Path.Combine(this.GetAppPath(), virtualPath);
 		}
+
+
+	    public override string GetUnknownRequestHeader(string name)
+	    {
+	        var item = _headers.FirstOrDefault(x => name.Equals(x[0], StringComparison.OrdinalIgnoreCase));
+	        return item?[1];
+	    }
+
+	    public override string[][] GetUnknownRequestHeaders()
+	    {
+	        return _headers;
+	    }
 	}
 }
