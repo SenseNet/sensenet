9378f3e6011004cea5221b2a1a5b8368e5a6b9cd
tusmester <tusmester@gmail.com>
2019-01-24 16:20:16 +0100

    Move resource parser method from SN resource manager to Services to remove System.Web.Compilation dependency.

---
+++ src/Services/ServiceExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web;
+using System.Web.Compilation;
+using SenseNet.ContentRepository.i18n;
+
+namespace SenseNet.Services
+{
+    public static class ServiceExtensions
+    {
+        public static string GetStringByExpression(this SenseNetResourceManager resourceManager, string expression)
+        {
+            if (string.IsNullOrEmpty(expression))
+                return expression;
+
+            if (!expression.StartsWith(SenseNetResourceManager.ResourceStartKey) ||
+                !expression.EndsWith(SenseNetResourceManager.ResourceEndKey))
+                return null;
+            
+            expression = expression.Replace(" ", "");
+            expression = expression.Replace(SenseNetResourceManager.ResourceStartKey, "");
+            expression = expression.Replace(SenseNetResourceManager.ResourceEndKey, "");
+
+            if (expression.Contains("Resources:"))
+                expression = expression.Remove(expression.IndexOf("Resources:", StringComparison.Ordinal), 10);
+
+            var expressionFields = ResourceExpressionBuilder.ParseExpression(expression);
+            if (expressionFields == null)
+            {
+                var context = HttpContext.Current;
+                var msg = $"{expression} is not a valid string resource format.";
+                if (context == null)
+                    throw new ApplicationException(msg);
+                return string.Format(msg);
+            }
+
+            return resourceManager.GetString(expressionFields.ClassKey, expressionFields.ResourceKey);
+        }
+    }
+}
