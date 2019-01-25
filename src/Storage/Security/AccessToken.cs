415208d4251ea957a66ff40df82e6910bc04c7f6
Zoltán Gyebrovszki <kavics@sensenet.com>
2019-01-25 04:26:27 +0100

    Code doc.

--- src/Storage/Security/AccessToken.cs
+++ src/Storage/Security/AccessToken.cs
@@ -2,19 +2,52 @@
 
 namespace SenseNet.ContentRepository.Storage.Security
 {
+    /// <summary>
+    /// Represents an access token for various authentication or authorization purposes.
+    /// </summary>
     public class AccessToken
     {
+        /// <summary>
+        /// Gets the general database identifier of the object.
+        /// </summary>
         public int Id { get; internal set; }
 
+        /// <summary>
+        /// Gets the value of the token.
+        /// </summary>
         public string Value { get; internal set; }
 
+        /// <summary>
+        /// Gets the Id of the owner User.
+        /// </summary>
         public int UserId { get; internal set; }
+
+        /// <summary>
+        /// Gets the associated Id of the content.
+        /// The value is 0 if there is no association.
+        /// </summary>
         public int ContentId { get; internal set; }
+
+        /// <summary>
+        /// Gets the associated feature name if there is.
+        /// Default value is null.
+        /// </summary>
         public string Feature { get; internal set; }
 
+        /// <summary>
+        /// Gets the cretion date.
+        /// </summary>
         public DateTime CreationDate { get; internal set; }
+
+        /// <summary>
+        /// Gets or sets the expiration date.
+        /// </summary>
         public DateTime ExpirationDate { get; set; }
 
+        /// <summary>
+        /// Returns the string representation of the token.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return Value;
