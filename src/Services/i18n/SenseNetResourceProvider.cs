89f080148980e7bb84d99be83b7895f5a080b078
tusmester <tusmester@gmail.com>
2019-02-01 10:40:29 +0100

    Move resource provider implementation to the Services project.

---
+++ src/Services/i18n/SenseNetResourceProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Resources;
+using System.Web.Compilation;
+
+namespace SenseNet.ContentRepository.i18n
+{
+    public class SenseNetResourceProvider : IResourceProvider
+    {
+        private readonly string _className;
+
+        public SenseNetResourceProvider(string className)
+        {
+            _className = className;
+        }
+        
+        public object GetObject(string resourceKey, CultureInfo culture)
+        {
+            if (culture == null)
+                culture = CultureInfo.CurrentUICulture;
+
+            return SenseNetResourceManager.Current.GetObject(_className, resourceKey, culture);
+        }
+
+        public IResourceReader ResourceReader => throw new SnNotSupportedException();
+    }
+}
