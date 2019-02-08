2e3668cb448c29a5e81c6be055b1d8668fd86247
tusmester <tusmester@gmail.com>
2019-02-08 10:48:05 +0100

    Refactor Evaluator to use the central Providers API and add tests.

---
+++ src/Tests/SenseNet.ContentRepository.Tests/EvaluatorTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.Configuration;
+using SenseNet.ContentRepository.Storage.Scripting;
+using SenseNet.Tests;
+
+namespace SenseNet.ContentRepository.Tests
+{
+    [ScriptTagName("testTag")]
+    internal class TestEvaluator : IEvaluator
+    {
+        public string Evaluate(string source)
+        {
+            return "success";
+        }
+    }
+    [ScriptTagName("testTag")]
+    internal class CustomTestEvaluator : IEvaluator
+    {
+        public string Evaluate(string source)
+        {
+            return "custom";
+        }
+    }
+
+    [TestClass]
+    public class EvaluatorTests : TestBase
+    {
+        [TestMethod]
+        public void Evaluator_Eval_Existing()
+        {
+            const string script = "[Script:testTag]...myscript...[/Script]";
+
+            Assert.AreEqual("success", Evaluator.Evaluate(script));
+        }
+        [TestMethod]
+        public void Evaluator_Eval_NonExisting()
+        {
+            const string script = "[Script:wrongTag]...myscript...[/Script]";
+
+            Assert.AreEqual("...myscript...", Evaluator.Evaluate(script));
+        }
+        [TestMethod]
+        public void Evaluator_Eval_Custom()
+        {
+            const string script = "[Script:testTag]...myscript...[/Script]";
+
+            var original = Providers.Instance.GetProvider<IEvaluator>(Evaluator.GetFullTagName("testTag"));
+            var repo = new RepositoryBuilder();
+            repo.UseEvaluator(new CustomTestEvaluator());
+
+            // the custom evaluator should be in control
+            Assert.AreEqual("custom", Evaluator.Evaluate(script));
+
+            // back to the original
+            repo.UseEvaluator(original);
+            
+            Assert.AreEqual("success", Evaluator.Evaluate(script));
+        }
+    }
+}
