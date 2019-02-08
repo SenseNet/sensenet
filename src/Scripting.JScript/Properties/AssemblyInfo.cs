e5d20f5bd2aaf1c5f7d6c9199fbcc5d5df13984d
tusmester <tusmester@gmail.com>
2019-02-08 16:10:06 +0100

    Netstandard/evaluator (#567)
    * Refactor Evaluator to use the central Providers API and add tests.
    * Remove JScriptEvaluator from the ContentRepository project.
    * Remove jScript default value from the Memo ctd.
    * Add legacy project for JScript evaluator.

---
+++ src/Scripting.JScript/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+[assembly: AssemblyTitle("SenseNet.Scripting.JScript (Debug)")]
+#else
+[assembly: AssemblyTitle("SenseNet.Scripting.JScript (Release)")]
+#endif
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sense/Net Inc.")]
+[assembly: AssemblyCopyright("Copyright © Sense/Net Inc.")]
+[assembly: AssemblyProduct("sensenet JScript")]
+[assembly: AssemblyTrademark("Sense/Net Inc.")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("7.0.0.0")]
+[assembly: AssemblyFileVersion("7.0.0.0")]
+[assembly: AssemblyInformationalVersion("7.0.0.0")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("f934da68-baf1-4da4-b09b-b018ef44e8ac")]
+
+
