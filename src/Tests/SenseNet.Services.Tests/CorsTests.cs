50825f23cf9cde43a82d5587747726124363580c
tusmester <tusmester@gmail.com>
2019-02-25 11:21:32 +0100

    Allow a wildcard domain definition for CORS.

---
+++ src/Tests/SenseNet.Services.Tests/CorsTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.Portal.Virtualization;
+using SenseNet.Tests;
+
+namespace SenseNet.Services.Tests
+{
+    [TestClass]
+    public class CorsTests : TestBase
+    {
+        [TestMethod]
+        public void Cors_AllowedDomain()
+        {
+            // empty
+            Assert.AreEqual(null, HttpHeaderTools.GetAllowedDomain("abc", new string[0]));
+
+            // regular domains
+            Assert.AreEqual("abc", HttpHeaderTools.GetAllowedDomain("abc", new[] { "abc", "def" }));
+            Assert.AreEqual("abc", HttpHeaderTools.GetAllowedDomain("abc", new[] { "def", "abc" }));
+
+            // wildcard (all)
+            Assert.AreEqual("*", HttpHeaderTools.GetAllowedDomain("abc", new []{"*"}));
+            Assert.AreEqual("*", HttpHeaderTools.GetAllowedDomain("abc", new []{"*", "abc"}));
+            Assert.AreEqual("abc", HttpHeaderTools.GetAllowedDomain("abc", new []{"abc", "*" }));
+            Assert.AreEqual("*", HttpHeaderTools.GetAllowedDomain("abc", new []{"abcd", "*" }));
+
+            // wildcard (subdomain)
+            Assert.AreEqual(null, HttpHeaderTools.GetAllowedDomain("abc", new[] { "*.abc" }));
+            Assert.AreEqual("abc", HttpHeaderTools.GetAllowedDomain("abc", new[] { "*.abc", "abc" }));
+            Assert.AreEqual("abc", HttpHeaderTools.GetAllowedDomain("abc", new[] { "abc", "*.abc" }));
+            Assert.AreEqual("*.abc", HttpHeaderTools.GetAllowedDomain("sub1.abc", new[] { "*.abc" }));
+            Assert.AreEqual("*.abc", HttpHeaderTools.GetAllowedDomain("sub1.abc", new[] { "abcd", "sub1abc", "sub1abccom", "*.abc" }));
+        }
+    }
+}
