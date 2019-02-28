3e84ecd5d5c1055435f0dc6963a07ff47bceef49
tusmester <tusmester@gmail.com>
2019-02-28 17:06:57 +0100

    Raise CR and Services version to 7.6.0.1.

--- src/Services/Properties/AssemblyInfo.cs
+++ src/Services/Properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("7.6")]
-[assembly: AssemblyFileVersion("7.6")]
-[assembly: AssemblyInformationalVersion("7.6")]
+[assembly: AssemblyVersion("7.6.0.1")]
+[assembly: AssemblyFileVersion("7.6.0.1")]
+[assembly: AssemblyInformationalVersion("7.6.0.1")]
