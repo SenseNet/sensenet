6677bda9acf8ff22d41c89ec8785c4c50e90cba3
Zoltán Gyebrovszki <kavics@sensenet.com>
2019-03-13 06:03:51 +0100

    INodeOperationValidator code comments.

--- src/Storage/INodeOperationValidator.cs
+++ src/Storage/INodeOperationValidator.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using SenseNet.ContentRepository.Storage;
-
+﻿
+ // ReSharper disable once CheckNamespace
 namespace SenseNet.ContentRepository.Storage
 {
     /// <summary>
@@ -11,8 +8,31 @@ namespace SenseNet.ContentRepository.Storage
     /// </summary>
     public interface  INodeOperationValidator
     {
+        /// <summary>
+        /// Checks the saving operation executability by the target Node instance and the currently changed data.
+        /// Returns true if the operation is allowed.
+        /// Returns false and provides an error message in the out parameter if the operation is disabled.
+        /// Disallowed operation causes an InvalidOperationException with the provided message.
+        /// Warning! Do not modify any property of the given objects.
+        /// </summary>
         bool CheckSaving(Node node, ChangedData[] changeData, out string errorMessage);
+
+        /// <summary>
+        /// Checks the Move operation executability by the source and target Node instances.
+        /// Returns true if the operation is allowed.
+        /// Returns false and provides an error message in the out parameter if the operation is disabled.
+        /// Disallowed operation causes an InvalidOperationException with the provided message.
+        /// Warning! Do not modify any property of the given object.
+        /// </summary>
         bool CheckMoving(Node source, Node target, out string errorMessage);
+
+        /// <summary>
+        /// Checks the Delete operation executability by the target Node instance.
+        /// Returns true if the operation is allowed.
+        /// Returns false and provides an error message in the out parameter if the operation is disabled.
+        /// Disallowed operation causes an InvalidOperationException with the provided message.
+        /// Warning! Do not modify any property of the given object.
+        /// </summary>
         bool CheckDeletion(Node node, out string errorMessage);
     }
 }
