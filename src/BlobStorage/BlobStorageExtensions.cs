70e2fd60eb3cb2f764608bf9479ebc1032c24409
tusmester <tusmester@gmail.com>
2019-03-22 18:27:31 +0100

    Add repositorybuilder method for configuring the external blob provider.

---
+++ src/BlobStorage/BlobStorageExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using SenseNet.ContentRepository.Storage.Data;
+using SenseNet.ContentRepository.Storage.Data.SqlClient;
+using SenseNet.Diagnostics;
+using SenseNet.Tools;
+
+namespace SenseNet.ContentRepository.Storage
+{
+    public static class BlobStorageExtensions
+    {
+        public static IRepositoryBuilder UseExternalBlobProvider(this IRepositoryBuilder builder, IBlobProvider provider)
+        {
+            if (provider == null)
+                return builder;
+
+            BuiltInBlobProviderSelector.ExternalBlobProvider = provider;
+
+            // we have to add this manually configured provider to the automatically collected list
+            if (BlobStorageBase.Providers == null)
+                BlobStorageBase.Providers = new Dictionary<string, IBlobProvider>();
+
+            // ReSharper disable once AssignNullToNotNullAttribute
+            BlobStorageBase.Providers[provider.GetType().FullName] = provider;
+
+            SnTrace.System.Write($"External blob provider configured: {provider.GetType().FullName}.");
+
+            return builder;
+        }
+    }
+}
