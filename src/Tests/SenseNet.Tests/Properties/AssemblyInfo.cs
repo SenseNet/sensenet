faa8518571d7701b00f4aff79eba6cdefa68f008
tusmester <tusmester@gmail.com>
2019-03-26 14:37:13 +0100

    Raise version to 7.6.0.2.

--- src/Tests/SenseNet.Tests/Properties/AssemblyInfo.cs
+++ src/Tests/SenseNet.Tests/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.5")]
-[assembly: AssemblyFileVersion("1.5")]
-[assembly: AssemblyInformationalVersion("1.5")]
+[assembly: AssemblyVersion("1.5.0.1")]
+[assembly: AssemblyFileVersion("1.5.0.1")]
+[assembly: AssemblyInformationalVersion("1.5.0.1")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("4674b8b8-6567-4eb5-b5a8-c838fecce302")]
