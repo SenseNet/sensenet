ee2c046397ff3b3ad2ac5762d64a56b772bbacb4
Zoltán Gyebrovszki <kavics@sensenet.com>
2019-04-08 01:28:00 +0200

    DataPackage class

---
+++ src/Storage/DataModel/DataPackage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SenseNet.ContentRepository.Storage.DataModel;
+
+namespace SenseNet.Storage.DataModel
+{
+    public class DataPackage
+    {
+        /// <summary>
+        /// Gets or sets the new or modified schema items.
+        /// </summary>
+        public RepositorySchemaData Schema { get; set; }
+
+        /// <summary>
+        /// Gets or sets the new or modified NodeHead items.
+        /// </summary>
+        public IEnumerable<NodeHeadData> Nodes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the new or modified Version items.
+        /// </summary>
+        public IEnumerable<VersionData> Versions { get; set; }
+
+        /// <summary>
+        /// Gets or sets the new or modified dynamic property values by VersionId
+        /// </summary>
+        public IDictionary<int, DynamicData> DynamicPropertyes { get; set; }
+    }
+}
