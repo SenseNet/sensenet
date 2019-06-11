7b329b038935ecfbabaf359efd0d0173ebdee2ea
tusmester <tusmester@gmail.com>
2019-06-11 13:46:16 +0200

    Refactor Access Token storage to use the new data provider.

--- src/Storage/Data/SqlClient/SqlAccessTokenDataProvider.cs
+++ src/Storage/Data/SqlClient/SqlAccessTokenDataProvider.cs
@@ -1,36 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Threading.Tasks;
+using SenseNet.Common.Storage.Data;
 using SenseNet.ContentRepository.Storage.Security;
 
 namespace SenseNet.ContentRepository.Storage.Data.SqlClient
 {
+    //UNDONE: AccessToken: async API + CancellationToken
     public class SqlAccessTokenDataProvider : IAccessTokenDataProviderExtension
     {
-        private DataProvider _mainProvider; //DB:ok but rewrite in the SqlAccessTokenDataProvider2
-        public DataProvider MainProvider => _mainProvider ?? (_mainProvider = DataProvider.Instance); //DB:ok but rewrite in the SqlAccessTokenDataProvider2
+        private RelationalDataProviderBase _dataProvider;
+        private RelationalDataProviderBase MainProvider => _dataProvider ?? (_dataProvider = (RelationalDataProviderBase)DataStore.DataProvider);
 
         private string _accessTokenValueCollationName;
-        private string AccessTokenValueCollationName
+        private string GetAccessTokenValueCollationName()
         {
-            get
-            {
-                if (_accessTokenValueCollationName == null)
-                {
-                    using (var proc = MainProvider.CreateDataProcedure("SELECT c.collation_name, c.* " +
-                                                                           "FROM sys.columns c " +
-                                                                           "  JOIN sys.tables t ON t.object_id = c.object_id " +
-                                                                           "WHERE t.name = 'AccessTokens' AND c.name = N'Value'"))
-                    {
-                        proc.CommandType = CommandType.Text;
+            var sql = "SELECT c.collation_name, c.* " +
+                      "FROM sys.columns c " +
+                      "  JOIN sys.tables t ON t.object_id = c.object_id " +
+                      "WHERE t.name = 'AccessTokens' AND c.name = N'Value'";
 
-                        var result = proc.ExecuteScalar();
-                        var originalCollation = Convert.ToString(result);
-                        _accessTokenValueCollationName = originalCollation.Replace("_CI_", "_CS_");
-                    }
+            if (_accessTokenValueCollationName == null)
+            {
+                using (var ctx = new SnDataContext(MainProvider))
+                {
+                    var result = ctx.ExecuteScalarAsync(sql).Result;
+                    var originalCollation = Convert.ToString(result);
+                    _accessTokenValueCollationName = originalCollation.Replace("_CI_", "_CS_");
                 }
-                return _accessTokenValueCollationName;
             }
+
+            return _accessTokenValueCollationName;
         }
 
         private AccessToken GetAccessTokenFromReader(IDataReader reader)
@@ -49,137 +50,169 @@ namespace SenseNet.ContentRepository.Storage.Data.SqlClient
 
         public void DeleteAllAccessTokens()
         {
-            using (var proc = MainProvider.CreateDataProcedure("TRUNCATE TABLE AccessTokens"))
+            using (var ctx = new SnDataContext(MainProvider))
             {
-                proc.CommandType = CommandType.Text;
-                proc.ExecuteNonQuery();
+                ctx.ExecuteNonQueryAsync("TRUNCATE TABLE AccessTokens").Wait(ctx.CancellationToken);
             }
         }
 
         public void SaveAccessToken(AccessToken token)
         {
-            using (var proc = MainProvider.CreateDataProcedure(
-                "INSERT INTO [dbo].[AccessTokens] " +
-                "([Value],[UserId],[ContentId],[Feature],[CreationDate],[ExpirationDate]) VALUES " +
-                "(@Value, @UserId, @ContentId, @Feature, @CreationDate, @ExpirationDate)" +
-                "SELECT @@IDENTITY")
-                .AddParameter("@Value", token.Value, DbType.String, 1000)
-                .AddParameter("@UserId", token.UserId)
-                .AddParameter("@ContentId", token.ContentId != 0 ? (object)token.ContentId : DBNull.Value, DbType.Int32)
-                .AddParameter("@Feature", token.Feature != null ? (object)token.Feature : DBNull.Value, DbType.String, 1000)
-                .AddParameter("@CreationDate", token.CreationDate)
-                .AddParameter("@ExpirationDate", token.ExpirationDate))
-            {
-                proc.CommandType = CommandType.Text;
+            const string sql = "INSERT INTO [dbo].[AccessTokens] " +
+                               "([Value],[UserId],[ContentId],[Feature],[CreationDate],[ExpirationDate]) VALUES " +
+                               "(@Value, @UserId, @ContentId, @Feature, @CreationDate, @ExpirationDate)" +
+                               "SELECT @@IDENTITY";
 
-                var result = proc.ExecuteScalar();
+            using (var ctx = new SnDataContext(MainProvider))
+            {
+                var result = ctx.ExecuteScalarAsync(sql, cmd =>
+                {
+                    cmd.Parameters.AddRange(new[]
+                    {
+                        ctx.CreateParameter("@Value", DbType.String, token.Value),
+                        ctx.CreateParameter("@UserId", DbType.Int32, token.UserId),
+                        ctx.CreateParameter("@ContentId", DbType.Int32, token.ContentId != 0 ? (object)token.ContentId : DBNull.Value),
+                        ctx.CreateParameter("@Feature", DbType.String, token.Feature != null ? (object)token.Feature : DBNull.Value),
+                        ctx.CreateParameter("@CreationDate", DbType.DateTime2, token.CreationDate),
+                        ctx.CreateParameter("@ExpirationDate", DbType.DateTime2, token.ExpirationDate)
+                    });
+                }).Result;
+
                 token.Id = Convert.ToInt32(result);
             }
         }
 
         public AccessToken LoadAccessTokenById(int accessTokenId)
         {
-            using (var proc = MainProvider.CreateDataProcedure("SELECT TOP 1 * FROM [dbo].[AccessTokens] WHERE [AccessTokenId] = @Id")
-                .AddParameter("@Id", accessTokenId))
+            using (var ctx = new SnDataContext(MainProvider))
             {
-                proc.CommandType = CommandType.Text;
+                return ctx.ExecuteReaderAsync("SELECT TOP 1 * FROM AccessTokens WHERE [AccessTokenId] = @Id", cmd =>
+                    {
+                        cmd.Parameters.Add(ctx.CreateParameter("@Id", DbType.Int32, accessTokenId));
+                    },
+                    reader =>
+                    {
+                        var token = reader.Read() ? GetAccessTokenFromReader(reader) : null;
 
-                using (var reader = proc.ExecuteReader())
-                    return reader.Read() ? GetAccessTokenFromReader(reader) : null;
+                        return Task.FromResult(token);
+                    }
+                ).Result;
             }
         }
 
         public AccessToken LoadAccessToken(string tokenValue, int contentId, string feature)
         {
             var sql = "SELECT TOP 1 * FROM [dbo].[AccessTokens] " +
-                      $"WHERE [Value] = @Value COLLATE {AccessTokenValueCollationName} AND [ExpirationDate] > GETUTCDATE() AND " +
+                      $"WHERE [Value] = @Value COLLATE {GetAccessTokenValueCollationName()} AND [ExpirationDate] > GETUTCDATE() AND " +
                       (contentId != 0 ? $"ContentId = {contentId} AND " : "ContentId IS NULL AND ") +
                       (feature != null ? $"Feature = '{feature}'" : "Feature IS NULL");
 
-            using (var proc = MainProvider.CreateDataProcedure(sql)
-                .AddParameter("@Value", tokenValue))
+            using (var ctx = new SnDataContext(MainProvider))
             {
-                proc.CommandType = CommandType.Text;
+                return ctx.ExecuteReaderAsync(sql, cmd =>
+                    {
+                        cmd.Parameters.Add(ctx.CreateParameter("@Value", DbType.String, tokenValue));
+                    },
+                    reader =>
+                    {
+                        var token = reader.Read() ? GetAccessTokenFromReader(reader) : null;
 
-                using (var reader = proc.ExecuteReader())
-                    return reader.Read() ? GetAccessTokenFromReader(reader) : null;
+                        return Task.FromResult(token);
+                    }
+                ).Result;
             }
         }
 
         public AccessToken[] LoadAccessTokens(int userId)
         {
-            using (var proc = MainProvider.CreateDataProcedure("SELECT * FROM [dbo].[AccessTokens] " +
-                                                                   "WHERE [UserId] = @UserId AND [ExpirationDate] > GETUTCDATE()")
-                .AddParameter("@UserId", userId))
+            const string sql = "SELECT * FROM [dbo].[AccessTokens] " +
+                               "WHERE [UserId] = @UserId AND [ExpirationDate] > GETUTCDATE()";
+
+            using (var ctx = new SnDataContext(MainProvider))
             {
-                proc.CommandType = CommandType.Text;
+                return ctx.ExecuteReaderAsync(sql, cmd =>
+                    {
+                        cmd.Parameters.Add(ctx.CreateParameter("@UserId", DbType.Int32, userId));
+                    },
+                    reader =>
+                    {
+                        var tokens = new List<AccessToken>();
+                        while (reader.Read())
+                            tokens.Add(GetAccessTokenFromReader(reader));
 
-                var tokens = new List<AccessToken>();
-                using (var reader = proc.ExecuteReader())
-                    while (reader.Read())
-                        tokens.Add(GetAccessTokenFromReader(reader));
-
-                return tokens.ToArray();
+                        return Task.FromResult(tokens.ToArray());
+                    }
+                ).Result;
             }
         }
 
         public void UpdateAccessToken(string tokenValue, DateTime newExpirationDate)
         {
-            using (var proc = MainProvider.CreateDataProcedure("UPDATE [AccessTokens] " +
-                                                                   "SET [ExpirationDate] = @NewExpirationDate " +
-                                                                   "   OUTPUT INSERTED.AccessTokenId" +
-                                                                   " WHERE [Value] = @Value " +
-                                                                   $" COLLATE {AccessTokenValueCollationName} AND [ExpirationDate] > GETUTCDATE()")
-                .AddParameter("@Value", tokenValue)
-                .AddParameter("@NewExpirationDate", newExpirationDate))
-            {
-                proc.CommandType = CommandType.Text;
+            var sql = "UPDATE [AccessTokens] " +
+                      "SET [ExpirationDate] = @NewExpirationDate " +
+                      "   OUTPUT INSERTED.AccessTokenId" +
+                      " WHERE [Value] = @Value " +
+                      $" COLLATE {GetAccessTokenValueCollationName()} AND [ExpirationDate] > GETUTCDATE()";
 
-                var result = proc.ExecuteScalar();
+            using (var ctx = new SnDataContext(MainProvider))
+            {
+                var result = ctx.ExecuteScalarAsync(sql, cmd =>
+                {
+                    cmd.Parameters.AddRange(new[]
+                    {
+                        ctx.CreateParameter("@Value", DbType.String, tokenValue),
+                        ctx.CreateParameter("@NewExpirationDate", DbType.DateTime2, newExpirationDate)
+                    });
+                }).Result;
+
                 if (result == null || result == DBNull.Value)
                     throw new InvalidAccessTokenException("Token not found or it is expired.");
             }
-
         }
 
         public void DeleteAccessToken(string tokenValue)
         {
-            using (var proc = MainProvider.CreateDataProcedure("DELETE FROM [dbo].[AccessTokens] " +
-                                                                   $"WHERE [Value] = @Value COLLATE {AccessTokenValueCollationName}")
-                .AddParameter("@Value", tokenValue))
+            var sql = "DELETE FROM [dbo].[AccessTokens] " +
+                      $"WHERE [Value] = @Value COLLATE {GetAccessTokenValueCollationName()}";
+
+            using (var ctx = new SnDataContext(MainProvider))
             {
-                proc.CommandType = CommandType.Text;
-                proc.ExecuteNonQuery();
+                ctx.ExecuteNonQueryAsync(sql, cmd =>
+                    {
+                        cmd.Parameters.Add(ctx.CreateParameter("@Value", DbType.String, tokenValue));
+                    }).Wait();
             }
         }
 
         public void DeleteAccessTokensByUser(int userId)
         {
-            using (var proc = MainProvider.CreateDataProcedure("DELETE FROM [dbo].[AccessTokens] WHERE [UserId] = @UserId")
-                .AddParameter("@UserId", userId))
+            using (var ctx = new SnDataContext(MainProvider))
             {
-                proc.CommandType = CommandType.Text;
-                proc.ExecuteNonQuery();
+                ctx.ExecuteNonQueryAsync("DELETE FROM [dbo].[AccessTokens] WHERE [UserId] = @UserId", cmd =>
+                {
+                    cmd.Parameters.Add(ctx.CreateParameter("@UserId", DbType.Int32, userId));
+                }).Wait();
             }
         }
 
         public void DeleteAccessTokensByContent(int contentId)
         {
-            using (var proc = MainProvider.CreateDataProcedure("DELETE FROM [dbo].[AccessTokens] WHERE [ContentId] = @ContentId")
-                .AddParameter("@ContentId", contentId))
+            using (var ctx = new SnDataContext(MainProvider))
             {
-                proc.CommandType = CommandType.Text;
-                proc.ExecuteNonQuery();
+                ctx.ExecuteNonQueryAsync("DELETE FROM [dbo].[AccessTokens] WHERE [ContentId] = @ContentId", cmd =>
+                {
+                    cmd.Parameters.Add(ctx.CreateParameter("@ContentId", DbType.Int32, contentId));
+                }).Wait();
             }
         }
 
         public void CleanupAccessTokens()
         {
-            var sql = "DELETE FROM [dbo].[AccessTokens] WHERE [ExpirationDate] < DATEADD(MINUTE, -30, GETUTCDATE())";
-            using (var proc = MainProvider.CreateDataProcedure(sql))
+            const string sql = "DELETE FROM [dbo].[AccessTokens] WHERE [ExpirationDate] < DATEADD(MINUTE, -30, GETUTCDATE())";
+
+            using (var ctx = new SnDataContext(MainProvider))
             {
-                proc.CommandType = CommandType.Text;
-                proc.ExecuteNonQuery();
+                ctx.ExecuteNonQueryAsync(sql).Wait();
             }
         }
     }
