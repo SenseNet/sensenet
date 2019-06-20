a80aa34436e2f8341f1f772cfd4a89f91eb0362d
Zoltán Gyebrovszki <kavics@sensenet.com>
2019-06-20 03:04:36 +0200

    Show internals for SenseNet.IntegrationTests.Common

--- src/Common/Properties/AssemblyInfo.cs
+++ src/Common/Properties/AssemblyInfo.cs
@@ -4,6 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("SenseNet.Tests")]
 [assembly: InternalsVisibleTo("SenseNet.BlobStorage.IntegrationTests")]
+[assembly: InternalsVisibleTo("SenseNet.IntegrationTests.Common")]
 [assembly: InternalsVisibleTo("SenseNet.ContentRepository.IntegrationTests")]
 [assembly: InternalsVisibleTo("SenseNet.Storage.IntegrationTests")]
 [assembly: InternalsVisibleTo("SenseNet.Packaging.Tests")]
