ef08bedc8eb4477d8ddddb6709d6de11018004d4
Zoltán Gyebrovszki <kavics@sensenet.com>
2019-07-08 05:27:09 +0200

    Split classes into independent files.

--- src/Common/Storage/Data/SnDataContext.cs
+++ src/Common/Storage/Data/SnDataContext.cs
@@ -9,37 +9,6 @@ using IsolationLevel = System.Data.IsolationLevel;
 // ReSharper disable once CheckNamespace
 namespace SenseNet.ContentRepository.Storage.Data
 {
-    //UNDONE:DB: REFACTOR: separate to independent files.
-    public class TransactionWrapper : IDbTransaction
-    {
-        public DbTransaction Transaction { get; }
-
-        public IDbConnection Connection => Transaction.Connection;
-        public IsolationLevel IsolationLevel => Transaction.IsolationLevel;
-        public TransactionStatus Status { get; private set; }
-
-        public TransactionWrapper(DbTransaction transaction)
-        {
-            Status = TransactionStatus.Active;
-            Transaction = transaction;
-        }
-
-        public virtual void Dispose()
-        {
-            Transaction.Dispose();
-        }
-        public virtual void Commit()
-        {
-            Transaction.Commit();
-            Status = TransactionStatus.Committed;
-        }
-        public virtual void Rollback()
-        {
-            Transaction.Rollback();
-            Status = TransactionStatus.Aborted;
-        }
-    }
-
     //UNDONE:DB: ASYNC API: CancellationToken is not used in this class.
     public class SnDataContext : IDisposable
     {
---
+++ src/Common/Storage/Data/TransactionWrapper.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Transactions;
+using IsolationLevel = System.Data.IsolationLevel;
+
+// ReSharper disable once CheckNamespace
+namespace SenseNet.ContentRepository.Storage.Data
+{
+    public class TransactionWrapper : IDbTransaction
+    {
+        public DbTransaction Transaction { get; }
+
+        public IDbConnection Connection => Transaction.Connection;
+        public IsolationLevel IsolationLevel => Transaction.IsolationLevel;
+        public TransactionStatus Status { get; private set; }
+
+        public TransactionWrapper(DbTransaction transaction)
+        {
+            Status = TransactionStatus.Active;
+            Transaction = transaction;
+        }
+
+        public virtual void Dispose()
+        {
+            Transaction.Dispose();
+        }
+        public virtual void Commit()
+        {
+            Transaction.Commit();
+            Status = TransactionStatus.Committed;
+        }
+        public virtual void Rollback()
+        {
+            Transaction.Rollback();
+            Status = TransactionStatus.Aborted;
+        }
+    }
+}
