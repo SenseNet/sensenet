cb97cd0f91c4f8b4e12ac5288e9259f17f671526
tusmester <tusmester@gmail.com>
2019-07-15 14:42:14 +0200

    Raise version to 7.6.2.

--- src/Tests/SenseNet.Services.OData.Tests/Properties/AssemblyInfo.cs
+++ src/Tests/SenseNet.Services.OData.Tests/Properties/AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("7.6.1")]
-[assembly: AssemblyFileVersion("7.6.1")]
-[assembly: AssemblyInformationalVersion("7.6.1")]
+[assembly: AssemblyVersion("7.6.2")]
+[assembly: AssemblyFileVersion("7.6.2")]
+[assembly: AssemblyInformationalVersion("7.6.2")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("d398f652-f091-4e52-b32b-6e73e214f946")]
