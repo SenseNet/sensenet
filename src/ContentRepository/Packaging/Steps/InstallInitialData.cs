93e4cc16073dbb6d09523c91f449bf3d2290cf82
Zoltán Gyebrovszki <kavics@sensenet.com>
2019-07-29 08:25:08 +0200

    New step InstallInitialData.

---
+++ src/ContentRepository/Packaging/Steps/InstallInitialData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SenseNet.ContentRepository.Storage.Data;
+using SenseNet.ContentRepository.Storage.Data.MsSqlClient;
+
+namespace SenseNet.Packaging.Steps
+{
+    public class InstallInitialData : Step
+    {
+        public string ConnectionName { get; set; }
+        public string DataSource { get; set; }
+        public string InitialCatalogName { get; set; }
+        public InitialCatalog InitialCatalog { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public override void Execute(ExecutionContext context)
+        {
+            var connectionInfo = new ConnectionInfo
+            {
+                ConnectionName = (string) context.ResolveVariable(ConnectionName),
+                DataSource = (string) context.ResolveVariable(DataSource),
+                InitialCatalog = InitialCatalog,
+                InitialCatalogName = (string) context.ResolveVariable(InitialCatalogName),
+                UserName = (string) context.ResolveVariable(UserName),
+                Password = (string) context.ResolveVariable(Password)
+            };
+            using (var ctx = new MsSqlDataContext(connectionInfo))
+            {
+
+            }
+        }
+    }
+}
