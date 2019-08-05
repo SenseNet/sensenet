4597f596ec41b0813558f2d200b13af7fd7b1d4a
tusmester <tusmester@gmail.com>
2019-08-05 17:34:18 +0200

    Raise version to 7.6.3.

--- src/Services/Properties/AssemblyInfo.cs
+++ src/Services/Properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("7.6.2")]
-[assembly: AssemblyFileVersion("7.6.2")]
-[assembly: AssemblyInformationalVersion("7.6.2")]
+[assembly: AssemblyVersion("7.6.3")]
+[assembly: AssemblyFileVersion("7.6.3")]
+[assembly: AssemblyInformationalVersion("7.6.3")]
--- src/Tests/SenseNet.Packaging.Tests/Properties/AssemblyInfo.cs
+++ src/Tests/SenseNet.Packaging.Tests/Properties/AssemblyInfo.cs
@@ -11,9 +11,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("7.6.2")]
-[assembly: AssemblyFileVersion("7.6.2")]
-[assembly: AssemblyInformationalVersion("7.6.2")]
+[assembly: AssemblyVersion("7.6.3")]
+[assembly: AssemblyFileVersion("7.6.3")]
+[assembly: AssemblyInformationalVersion("7.6.3")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("224fe019-a978-4390-9f77-ebeb7a384743")]
