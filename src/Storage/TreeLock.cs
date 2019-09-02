309b7b9a72d03f39acd618856c0f2bc82e7fbdad
tusmester <tusmester@gmail.com>
2019-09-02 13:49:52 +0200

    Add async API to treelock.

--- src/Storage/TreeLock.cs
+++ src/Storage/TreeLock.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace SenseNet.ContentRepository.Storage
 {
@@ -44,6 +45,40 @@ namespace SenseNet.ContentRepository.Storage
             var logOp = SnTrace.ContentOperation.StartOperation("TreeLock: {0} for {1}", lockIds, paths);
             return new TreeLock(logOp, lockIds);
         }
+        /// <summary>
+        /// Locks one or more subtrees in the Content Repository. If a subtree is locked, no modifications (Save operations) can be made there.
+        /// Use this method with a using statement to make sure that the lock is released when not needed anymore.
+        /// </summary>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
+        /// <param name="paths">One or more Content Repository paths to be locked.</param>
+        /// <exception cref="LockedTreeException">Thrown when any of the requested paths (or any of the parent containers) are already locked.</exception>
+        /// <returns>A Task that represents the asynchronous operation and wraps the new tree lock
+        /// object containing the lock ids.</returns>
+        public static async Task<TreeLock> AcquireAsync(CancellationToken cancellationToken, params string[] paths)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            SnTrace.ContentOperation.Write("TreeLock: Acquiring lock for {0}", paths);
+
+            var lockTasks = paths.Select(p => DataStore.AcquireTreeLockAsync(p, cancellationToken));
+            var lockIds = await Task.WhenAll(lockTasks);
+
+            for (var i = 0; i < lockIds.Length; i++)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                if (lockIds[i] == 0)
+                {
+                    await DataStore.ReleaseTreeLockAsync(lockIds, cancellationToken);
+                    var msg = "Cannot acquire a tree lock for " + paths[i];
+                    SnTrace.ContentOperation.Write("TreeLock: " + msg);
+                    throw new LockedTreeException(msg);
+                }
+            }
+
+            var logOp = SnTrace.ContentOperation.StartOperation("TreeLock: {0} for {1}", lockIds, paths);
+            return new TreeLock(logOp, lockIds);
+        }
 
         /// <summary>
         /// Checks whether a subtree is locked. Used by save operations to make sure that it is OK to make modifications.
@@ -64,9 +99,34 @@ namespace SenseNet.ContentRepository.Storage
                 }
             }
         }
+        /// <summary>
+        /// Checks whether a subtree is locked. Used by save operations to make sure that it is OK to make modifications.
+        /// </summary>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
+        /// <param name="paths">One or more Content Repository paths to check for locked state.</param>
+        /// <exception cref="LockedTreeException">Thrown when any of the requested paths (or any of the parent containers) are already locked.</exception>
+        /// <returns>A Task that represents the asynchronous operation.</returns>
+        public static async Task AssertFreeAsync(CancellationToken cancellationToken, params string[] paths)
+        {
+            SnTrace.ContentOperation.Write("TreeLock: Checking {0}", string.Join(", ", paths));
+
+            foreach (var path in paths)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                if (await DataStore.IsTreeLockedAsync(path, cancellationToken))
+                {
+                    var msg = "Cannot perform the operation because another process is making changes on this path: " + path;
+                    SnTrace.ContentOperation.Write("TreeLock: Checking {0}", string.Join(", ", paths));
+                    throw new LockedTreeException(msg);
+                }
+            }
+        }
 
         public void Dispose()
         {
+            //TODO: find a better design instead of calling an asynchronous method
+            // synchronously inside a Dispose method.
             DataStore.ReleaseTreeLockAsync(_lockIds, CancellationToken.None).GetAwaiter().GetResult();
             if (_logOp != null)
             {
@@ -75,9 +135,21 @@ namespace SenseNet.ContentRepository.Storage
             }
         }
 
+        /// <summary>
+        /// Gets all existing locks from the database.
+        /// </summary>
         public static Dictionary<int, string> GetAllLocks()
         {
             return DataStore.LoadAllTreeLocksAsync(CancellationToken.None).GetAwaiter().GetResult();
+        }
+        /// <summary>
+        /// Gets all existing locks in the system.
+        /// </summary>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
+        /// <returns>A Task that represents the asynchronous operation and wraps a lock id, path dictionary.</returns>
+        public static Task<Dictionary<int, string>> GetAllLocksAsync(CancellationToken cancellationToken)
+        {
+            return DataStore.LoadAllTreeLocksAsync(cancellationToken);
         }
     }
 }
--- src/Tests/SenseNet.ContentRepository.Tests/TreeLockTests.cs
+++ src/Tests/SenseNet.ContentRepository.Tests/TreeLockTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Threading;
+using STT=System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SenseNet.ContentRepository.Storage;
 using SenseNet.Tests;
 
@@ -8,7 +10,7 @@ namespace SenseNet.ContentRepository.Tests
     public class TreeLockTests : TestBase
     {
         [TestMethod]
-        public void TreeLock_AcquieAndScope()
+        public void TreeLock_AcquireAndScope()
         {
             Test(() =>
             {
@@ -27,6 +29,31 @@ namespace SenseNet.ContentRepository.Tests
                     Assert.IsFalse(IsLocked("/Root/A/B/X"));
                     Assert.IsFalse(IsLocked("/Root/A/X"));
                     Assert.IsFalse(IsLocked("/Root/X"));
+                }
+            });
+        }
+        [TestMethod]
+        public async STT.Task TreeLock_AcquireAndScopeAsync()
+        {
+            var token = CancellationToken.None;
+
+            await Test(async () => 
+            {
+                using (await TreeLock.AcquireAsync(CancellationToken.None, "/Root/A/B/C"))
+                {
+                    var locks = await TreeLock.GetAllLocksAsync(CancellationToken.None);
+                    Assert.AreEqual(1, locks.Count);
+                    Assert.IsTrue(locks.ContainsValue("/Root/A/B/C"));
+
+                    Assert.IsTrue(await IsLockedAsync("/Root/A/B/C/D", token));
+                    Assert.IsTrue(await IsLockedAsync("/Root/A/B/C", token));
+                    Assert.IsTrue(await IsLockedAsync("/Root/A/B", token));
+                    Assert.IsTrue(await IsLockedAsync("/Root/A", token));
+                    Assert.IsTrue(await IsLockedAsync("/Root", token));
+
+                    Assert.IsFalse(await IsLockedAsync("/Root/A/B/X", token));
+                    Assert.IsFalse(await IsLockedAsync("/Root/A/X", token));
+                    Assert.IsFalse(await IsLockedAsync("/Root/X", token));
                 }
             });
         }
@@ -80,6 +107,58 @@ namespace SenseNet.ContentRepository.Tests
                 Assert.IsFalse(IsLocked("/Root"));
             });
         }
+        [TestMethod]
+        public async STT.Task TreeLock_ReleaseAsync()
+        {
+            var token = CancellationToken.None;
+
+            await Test(async () =>
+            {
+                var locks = await TreeLock.GetAllLocksAsync(token);
+                Assert.AreEqual(0, locks.Count);
+
+                using (await TreeLock.AcquireAsync(token, "/Root/A/B/C1"))
+                {
+                    using (await TreeLock.AcquireAsync(token, "/Root/A/B/C2"))
+                    {
+                        locks = await TreeLock.GetAllLocksAsync(token);
+                        Assert.AreEqual(2, locks.Count);
+                        Assert.IsTrue(locks.ContainsValue("/Root/A/B/C1"));
+                        Assert.IsTrue(locks.ContainsValue("/Root/A/B/C2"));
+
+                        Assert.IsTrue(IsLocked("/Root/A/B/C1/D"));
+                        Assert.IsTrue(IsLocked("/Root/A/B/C2/D"));
+                        Assert.IsTrue(IsLocked("/Root/A/B/C1"));
+                        Assert.IsTrue(IsLocked("/Root/A/B/C2"));
+                        Assert.IsTrue(IsLocked("/Root/A/B"));
+                        Assert.IsTrue(IsLocked("/Root/A"));
+                        Assert.IsTrue(IsLocked("/Root"));
+                    }
+                    locks = await TreeLock.GetAllLocksAsync(token);
+                    Assert.AreEqual(1, locks.Count);
+                    Assert.IsTrue(locks.ContainsValue("/Root/A/B/C1"));
+
+                    Assert.IsTrue(await IsLockedAsync("/Root/A/B/C1/D", token));
+                    Assert.IsTrue(await IsLockedAsync("/Root/A/B/C1", token));
+                    Assert.IsTrue(await IsLockedAsync("/Root/A/B", token));
+                    Assert.IsTrue(await IsLockedAsync("/Root/A", token));
+                    Assert.IsTrue(await IsLockedAsync("/Root", token));
+
+                    Assert.IsFalse(await IsLockedAsync("/Root/A/B/C2/D", token));
+                    Assert.IsFalse(await IsLockedAsync("/Root/A/B/C2", token));
+                }
+                locks = await TreeLock.GetAllLocksAsync(token);
+                Assert.AreEqual(0, locks.Count);
+
+                Assert.IsFalse(await IsLockedAsync("/Root/A/B/C1/D", token));
+                Assert.IsFalse(await IsLockedAsync("/Root/A/B/C2/D", token));
+                Assert.IsFalse(await IsLockedAsync("/Root/A/B/C1", token));
+                Assert.IsFalse(await IsLockedAsync("/Root/A/B/C2", token));
+                Assert.IsFalse(await IsLockedAsync("/Root/A/B", token));
+                Assert.IsFalse(await IsLockedAsync("/Root/A", token));
+                Assert.IsFalse(await IsLockedAsync("/Root", token));
+            });
+        }
 
         [TestMethod]
         public void TreeLock_CannotAcquire()
@@ -113,12 +192,58 @@ namespace SenseNet.ContentRepository.Tests
                 }
             });
         }
+        [TestMethod]
+        public async STT.Task TreeLock_CannotAcquireAsync()
+        {
+            var token = CancellationToken.None;
 
-        private bool IsLocked(string path)
+            await Test(async () =>
+            {
+                var locks = await TreeLock.GetAllLocksAsync(token);
+                Assert.AreEqual(0, locks.Count);
+
+                using (await TreeLock.AcquireAsync(token,"/Root/A/B/C"))
+                {
+                    locks = await TreeLock.GetAllLocksAsync(token);
+                    Assert.AreEqual(1, locks.Count);
+                    Assert.IsTrue(locks.ContainsValue("/Root/A/B/C"));
+
+                    foreach (var path in new[] { "/Root/A/B/C/D", "/Root/A/B/C", "/Root/A/B", "/Root/A", "/Root" })
+                    {
+                        try
+                        {
+                            await TreeLock.AcquireAsync(token,"/Root/A/B/C/D");
+                            Assert.Fail($"LockedTreeException was not thrown. Path: {path}");
+                        }
+                        catch (LockedTreeException)
+                        {
+                        }
+
+                        locks = await TreeLock.GetAllLocksAsync(token);
+                        Assert.AreEqual(1, locks.Count);
+                        Assert.IsTrue(locks.ContainsValue("/Root/A/B/C"));
+                    }
+                }
+            });
+        }
+
+        private static bool IsLocked(string path)
         {
             try
             {
                 TreeLock.AssertFree(path);
+                return false;
+            }
+            catch (LockedTreeException)
+            {
+                return true;
+            }
+        }
+        private static async STT.Task<bool> IsLockedAsync(string path, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await TreeLock.AssertFreeAsync(cancellationToken, path);
                 return false;
             }
             catch (LockedTreeException)
@@ -150,6 +275,41 @@ namespace SenseNet.ContentRepository.Tests
                     try
                     {
                         TreeLock.AssertFree("/Root/A/BxB");
+                    }
+                    catch (LockedTreeException)
+                    {
+                        thrown = true;
+                    }
+
+                    Assert.IsTrue(thrown, "#1");
+                }
+            });
+        }
+        [TestMethod]
+        public async STT.Task TreeLock_TSQL_UnderscoreAsync()
+        {
+            // This test makes sure that it does not cause a problem if a path
+            // contains an underscore. Previously this did not work correctly
+            // because the SQL LIKE operator treated it as a special character.
+
+            var token = CancellationToken.None;
+
+            await Test(async () =>
+            {
+                using (await TreeLock.AcquireAsync(token,"/Root/A/BxB/C"))
+                {
+                    await TreeLock.AssertFreeAsync(token,"/Root/A/B_B");
+
+                    using (await TreeLock.AcquireAsync(token,"/Root/A/B_B"))
+                    {
+                        await TreeLock.AssertFreeAsync(token, "/Root/A/B_");
+                        await TreeLock.AssertFreeAsync(token, "/Root/A/ByB");
+                    }
+
+                    var thrown = false;
+                    try
+                    {
+                        await TreeLock.AssertFreeAsync(token,"/Root/A/BxB");
                     }
                     catch (LockedTreeException)
                     {
