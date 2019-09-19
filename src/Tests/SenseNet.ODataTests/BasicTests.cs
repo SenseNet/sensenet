f7fe1286f1d959b0f9829a89746c71d912a0c84a
Zoltán Gyebrovszki <kavics@sensenet.com>
2019-09-20 01:59:36 +0200

    OD_Getting_ChildrenCollection

--- src/Tests/SenseNet.ODataTests/BasicTests.cs
+++ src/Tests/SenseNet.ODataTests/BasicTests.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.ContentRepository;
+using SenseNet.ContentRepository.Storage;
 using SenseNet.OData;
 
 namespace SenseNet.ODataTests
@@ -19,6 +23,22 @@ namespace SenseNet.ODataTests
                 var odataContent = (ODataContent)response.Value;
                 Assert.AreEqual(ODataResponseType.SingleContent, response.Type);
                 Assert.AreEqual("/Root/IMS", odataContent.Path);
+            });
+        }
+        [TestMethod]
+        public void OD_Getting_ChildrenCollection()
+        {
+            ODataTest(() =>
+            {
+                var response = ODataGET<ODataResponse>("/OData.svc/Root/IMS/BuiltIn/Portal", "");
+
+                var entities = (IEnumerable<ODataContent>) response.Value;
+                var origIds = Node.Load<Folder>("/Root/IMS/BuiltIn/Portal").Children.Select(f => f.Id).ToArray();
+                var ids = entities.Select(e => e.Id).ToArray();
+
+                Assert.AreEqual(ODataResponseType.ChildrenCollection, response.Type);
+                Assert.AreEqual(0, origIds.Except(ids).Count());
+                Assert.AreEqual(0, ids.Except(origIds).Count());
             });
         }
     }
