8cc3c558a8beba28ec7078ee3ba20dfe63aa5fd9
Zoltán Gyebrovszki <kavics@sensenet.com>
2019-09-20 13:24:55 +0200

    Accessors

---
+++ src/OData/Accessors/Accessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SenseNet.OData.Accessors
+{
+        public abstract class Accessor
+        {
+            public static ObjectAccessor Create(object target)
+            {
+                return new ObjectAccessor(target);
+            }
+            public static TypeAccessor Create(Type target)
+            {
+                return new TypeAccessor(target);
+            }
+        }
+}
