d64843da8fd85ba4ddba07a6bd378cb9795809cb
Zoltán Gyebrovszki <kavics@sensenet.com>
2019-10-10 17:15:53 +0200

    OData middleware test.

--- src/Tests/SenseNet.ODataTests/ODataGeneralTests.cs
+++ src/Tests/SenseNet.ODataTests/ODataGeneralTests.cs
@@ -25,6 +25,49 @@ namespace SenseNet.ODataTests
     [TestClass]
     public class ODataGeneralTests : ODataTestBase
     {
+        private class TestMiddleware
+        {
+            public TestMiddleware(RequestDelegate next)
+            {
+                _next = next;
+            }
+
+            private readonly RequestDelegate _next;
+            public async Task InvokeAsync(HttpContext context)
+            {
+                context.Response.Headers.Add("Header1", "HeaderValue1");
+                if (_next != null)
+                    await _next(context).ConfigureAwait(false);
+            }
+        }
+        [TestMethod]
+        public async Task OD_Middleware()
+        {
+            await ODataTestAsync(async () =>
+            {
+                // ARRANGE
+
+                // Create HttpContext 
+                var httpContext = new DefaultHttpContext();
+                var request = httpContext.Request;
+                request.Path = "/OData.svc";
+                request.QueryString = QueryString.Empty;
+                request.Method = "GET";
+                httpContext.Response.Body = new MemoryStream();
+
+                // ACTION: Simulate the aspnet framework
+                // instantiate the OData with the next chain member
+                var odata = new ODataMiddleware(new TestMiddleware(null).InvokeAsync);
+                // call the first of the chain
+                await odata.InvokeAsync(httpContext);
+
+                // ASSERT
+                var header = httpContext.Response.Headers.FirstOrDefault(h => h.Key == "Header1");
+                Assert.IsNotNull(header);
+                Assert.AreEqual("HeaderValue1", header.Value.ToString());
+            }).ConfigureAwait(false);
+        }
+
         [TestMethod]
         public async Task OD_GET_NoRequest()
         {
