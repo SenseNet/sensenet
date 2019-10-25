0b5520043db8af8810cbafe9716ad2a944519b5e
tusmester <tusmester@gmail.com>
2019-10-25 13:50:06 +0200

    Remove nonexistent namespace.

--- src/Tests/SenseNet.ODataTest.WebApp/Program.cs
+++ src/Tests/SenseNet.ODataTest.WebApp/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
-using Compatibility.SenseNet.ContentRepository.Security;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using SenseNet.Configuration;
