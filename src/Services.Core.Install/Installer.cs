1593887b4fe46631b5c284315c840a4d7f0b38ad
tusmester <tusmester@gmail.com>
2019-11-14 14:38:47 +0100

    Add docs to the installer methods.

--- src/Services.Core.Install/Installer.cs
+++ src/Services.Core.Install/Installer.cs
@@ -9,16 +9,29 @@ namespace SenseNet.Services.Core.Install
     {
         private const string InstallPackageName = "install-services-core.zip";
 
+        /// <summary>
+        /// Installs the sensenet Services main component.
+        /// </summary>
+        /// <param name="builder">Repository builder used by the install process to start the repository.
+        /// It should be preconfigured with all the necessary sensenet providers like data provider
+        /// and search engine.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
         public static async Task<IRepositoryBuilder> InstallSenseNetAsync(this IRepositoryBuilder builder, 
             CancellationToken cancellationToken)
         {
             await Packaging.Installer.InstallSenseNetAsync(typeof(Installer).Assembly,
                 InstallPackageName,
-                builder as RepositoryBuilder, cancellationToken);
+                builder as RepositoryBuilder, cancellationToken).ConfigureAwait(false);
 
             return builder;
         }
 
+        /// <summary>
+        /// Installs the sensenet Services main component.
+        /// </summary>
+        /// <param name="builder">Repository builder used by the install process to start the repository.
+        /// It should be preconfigured with all the necessary sensenet providers like data provider
+        /// and search engine.</param>
         public static IRepositoryBuilder InstallSenseNet(this IRepositoryBuilder builder)
         {
             Packaging.Installer.InstallSenseNetAsync(typeof(Installer).Assembly,
