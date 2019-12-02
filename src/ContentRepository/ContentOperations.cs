5fc5c88f318ab4ec72f36e4a2a6a3cecd5f8b433
Zoltán Gyebrovszki <kavics@sensenet.com>
2019-12-02 07:02:34 +0100

    RestoreVersion

--- src/ContentRepository/ContentOperations.cs
+++ src/ContentRepository/ContentOperations.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using SenseNet.ApplicationModel;
 
 // ReSharper disable StringLiteralTypo
@@ -116,5 +117,44 @@ namespace SenseNet.ContentRepository
             content.ForceUndoCheckOut();
             return content;
         }
+
+        [ODataAction]
+        [ContentType(N.GenericContent)]
+        [SnAuthorize(Role = N.Everyone, Permission = N.Save + "," + N.RecallOldVersion)]
+        public static Content RestoreVersion(Content content, string version)
+        {
+            // Perform checks
+            if (version == null)
+                throw new Exception("Can't restore old version because the version parameter is empty.");
+            if (!(content.ContentHandler is GenericContent))
+                throw new Exception($"Can't restore old version of content '{content.Path}' because " +
+                                    "its content handler is not a GenericContent. It needs to " +
+                                    "inherit from GenericContent for collaboration feature support.");
+
+            // Append 'V' if it is not there
+            var versionText = version.StartsWith("V")
+                ? version
+                : string.Concat("V", version);
+
+            // Find old version on the content
+
+            // Try exact match on version string - example input: 1.0.A or V1.0.A
+            var oldVersion = content.Versions.AsEnumerable().SingleOrDefault(n => n.Version.VersionString == versionText) as GenericContent;
+
+            // Try adding status code - example input: 1.0 or V1.0
+            if (oldVersion == null)
+                oldVersion = content.Versions.AsEnumerable().SingleOrDefault(n => n.Version.VersionString == string.Concat(versionText, ".", n.Version.Status.ToString()[0])) as GenericContent;
+
+            // Throw exception if still not found
+            if (oldVersion == null)
+                throw new Exception($"The requested version '{version}' does not exist on content '{content.Path}'.");
+
+            // Restore old version
+            oldVersion.Save();
+
+            // Return actual state of content
+            return Content.Load(content.Id);
+        }
+
     }
 }
