b7d5c382544979864a924ed745b8790fcd5610ab
tusmester <tusmester@gmail.com>
2019-12-02 11:39:21 +0100

    Feature/claimsprincipal (#841)
    * fix: PortalPrincipal is a ClaimsPrincipal making user identity compatible with AspNetCore HttpContext.User: https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.http.httpcontext.user
    * fix: removed override of the Identity property.
    * fix: keep backward compatibility - primary claims identity is an IUser instead of ClaimsIdentity - Identities.First().

--- src/ContentRepository/Security/PortalPrincipal.cs
+++ src/ContentRepository/Security/PortalPrincipal.cs
@@ -1,34 +1,43 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Security.Claims;
 using System.Security.Principal;
 using SenseNet.ContentRepository.Storage.Security;
 
 namespace SenseNet.ContentRepository.Security
 {
-    public class PortalPrincipal : IPrincipal
+    /// <summary>
+    /// An <see cref="IPrincipal" /> implementation that supports multiple claims-based identities.
+    /// </summary>
+    /// <seealso cref="ClaimsPrincipal" />
+    public class PortalPrincipal : ClaimsPrincipal
     {
-        private IUser _user;
+        private readonly IUser _user;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PortalPrincipal" /> class.
+        /// </summary>
+        /// <param name="user">The primary user identity associated with this claims principal.</param>
+        /// <exception cref="ArgumentNullException">user - The user parameter cannot be null. Use 'User.Visitor' instead.</exception>
         public PortalPrincipal(IUser user)
+            : base(user)
         {
-            if (user == null)
-                throw new ArgumentNullException("user", "The user parameter cannot be null. Use 'User.Visitor' instead.");
-            _user = user;
+            _user = user ?? throw new ArgumentNullException(nameof(user), "User cannot be null. Use 'User.Visitor' instead.");
         }
 
-        #region IPrincipal Members
+        /// <summary>
+        /// Gets the primary claims identity associated with this claims principal.
+        /// </summary>
+        public override IIdentity Identity => _user;
 
-        public IIdentity Identity
-        {
-            get { return _user; }
-        }
-
-        public bool IsInRole(string role)
-        {
-            throw new NotSupportedException("Role management is not supported.");
-        }
-
-        #endregion
+        /// <summary>
+        /// Returns a value that indicates whether the entity (user) represented by this claims principal is in the specified role.
+        /// </summary>
+        /// <param name="role">The role for which to check.</param>
+        /// <returns>
+        /// true if claims principal is in the specified role; otherwise, false.
+        /// </returns>
+        /// <exception cref="NotSupportedException">Role management is not supported.</exception>
+        public override bool IsInRole(string role)
+            => throw new NotSupportedException("Role management is not supported.");
     }
 }
