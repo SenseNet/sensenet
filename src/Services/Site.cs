449ef038ade346b36394d550ef5d18859353790f
tusmester <tusmester@gmail.com>
2020-01-06 16:21:06 +0100

    Add a test and refactor the url validator method.

--- src/Services/Site.cs
+++ src/Services/Site.cs
@@ -226,15 +226,15 @@ namespace SenseNet.Portal
             foreach (var url in UrlList.Keys.Where(url => PortalContext.Sites.Keys.Count(k => k == url && PortalContext.Sites[k].Id != this.Id) > 0))
                 throw new ApplicationException(SNSR.GetString(SNSR.Exceptions.Site.UrlAlreadyUsed_2, url, PortalContext.Sites[url].DisplayName));
         }
-        private bool IsValidSiteUrl(string url)
+        internal static bool IsValidSiteUrl(string url)
         {
-            var absUrl = ("http://" + url).ToLower();
+            var absUrl = "http://" + url;
             if (!Uri.IsWellFormedUriString(absUrl, UriKind.Absolute))
                 return false;
             try
             {
                 var uri = new Uri(absUrl);
-                if (uri.Authority != url)
+                if (string.Compare(uri.Authority, url, StringComparison.InvariantCultureIgnoreCase) != 0)
                     return false;
             }
             catch
