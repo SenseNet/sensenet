af444f0442e75e5f51df33724847bdd997fb6434
kavics <kavics@sensenet.com>
2020-01-17 12:11:31 +0100

    The ContentTypeEnumerableIndexHandler is the default indexhandler of the AllowedChildTypesField. (#866)

--- src/ContentRepository/DefaultDatabase.cs
+++ src/ContentRepository/DefaultDatabase.cs
@@ -2007,10 +2007,6 @@ VersionId: 411
     <Field name=""AllowedChildTypes"" type=""AllowedChildTypes"">
       <DisplayName>$Ctd-GenericContent,AllowedChildTypes-DisplayName</DisplayName>
       <Description>$Ctd-GenericContent,AllowedChildTypes-Description</Description>
-      <Indexing>
-        <IndexHandler>SenseNet.Search.Indexing.ContentTypeEnumerableIndexHandler</IndexHandler>
-        <Analyzer>Keyword</Analyzer>
-      </Indexing>
       <Configuration>
         <VisibleBrowse>Hide</VisibleBrowse>
         <VisibleEdit>Hide</VisibleEdit>
@@ -2020,10 +2016,6 @@ VersionId: 411
     <Field name=""EffectiveAllowedChildTypes"" type=""AllowedChildTypes"">
       <DisplayName>$Ctd-GenericContent,EffectiveAllowedChildTypes-DisplayName</DisplayName>
       <Description>$Ctd-GenericContent,EffectiveAllowedChildTypes-Description</Description>
-      <Indexing>
-        <IndexHandler>SenseNet.Search.Indexing.ContentTypeEnumerableIndexHandler</IndexHandler>
-        <Analyzer>Keyword</Analyzer>
-      </Indexing>
       <Configuration>
         <VisibleBrowse>Hide</VisibleBrowse>
         <VisibleEdit>Hide</VisibleEdit>
