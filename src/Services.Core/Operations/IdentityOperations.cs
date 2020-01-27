36d090ddb4e9c859bfa3e31629b7f51ad1161ee8
tusmester <tusmester@gmail.com>
2020-01-27 10:08:41 +0100

    Add odata action for validating user credentials by loading the user from the repo.

---
+++ src/Services.Core/Operations/IdentityOperations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using SenseNet.ApplicationModel;
+using SenseNet.ContentRepository;
+using SenseNet.ContentRepository.Storage.Security;
+
+namespace SenseNet.Services.Core.Operations
+{
+    internal static class IdentityOperations
+    {
+        [ODataAction]
+        [ContentTypes(N.CT.PortalRoot)]
+        [AllowedRoles(N.R.All)]
+        public static object ValidateCredentials(Content content, HttpContext context, string userName, string password)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (userName == null)
+                throw new ArgumentNullException(nameof(userName));
+            if (password == null)
+                throw new ArgumentNullException(nameof(password));
+
+            var user = SystemAccount.Execute(() => User.Load(userName));
+
+            if (user?.CheckPasswordMatch(password) ?? false)
+            {
+                return new
+                {
+                    id = user.Id,
+                    email = user.Email,
+                    username = user.Username,
+                    name = user.Name,
+                    loginName = user.LoginName
+                };
+            }
+
+            throw new SenseNetSecurityException("Invalid username or password.");
+        }
+    }
+}
