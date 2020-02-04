dc2bf01c987fa6247d1f566e632e3c6e370348d6
tusmester <tusmester@gmail.com>
2020-02-04 13:58:46 +0100

    Fix/user create permissions (#875)
    * Set new users' owner (themselves) in elevated mode.
    * Add test user, diagnostic controller and view to SPA test project.
    * Add a new test for user creation.

--- src/Tests/SenseNet.Tests.SPA/ClientApp/src/components/Home.js
+++ src/Tests/SenseNet.Tests.SPA/ClientApp/src/components/Home.js
@@ -6,7 +6,14 @@ export class Home extends Component {
   render () {
     return (
       <div>
-        <h1>Hello, world!</h1>
+            <h1>Hello, world!</h1>
+
+            <p>
+                <ul>
+                    <li><a href='/sensenet'>sensenet test page</a></li>
+                </ul>
+            </p>
+
         <p>Welcome to your new single-page application, built with:</p>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
