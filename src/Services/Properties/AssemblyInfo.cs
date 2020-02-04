aca2aa21c49f55e8c1d949fc2c6a606ab3152cf0
tusmester <tusmester@gmail.com>
2020-02-04 14:04:10 +0100

    Raise version to 7.7.7.

--- src/Services/Properties/AssemblyInfo.cs
+++ src/Services/Properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("7.7.6")]
-[assembly: AssemblyFileVersion("7.7.6")]
-[assembly: AssemblyInformationalVersion("7.7.6")]
+[assembly: AssemblyVersion("7.7.7")]
+[assembly: AssemblyFileVersion("7.7.7")]
+[assembly: AssemblyInformationalVersion("7.7.7")]
