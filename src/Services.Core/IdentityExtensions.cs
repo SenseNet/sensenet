c68cde7ed88a13ae04eccd3112e703756c9e3918
tusmester <tusmester@gmail.com>
2020-03-09 13:50:53 +0100

    Add jwt token as a cookie if required by the application. (#890)

--- src/Services.Core/IdentityExtensions.cs
+++ src/Services.Core/IdentityExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using SenseNet.ContentRepository;
 using SenseNet.ContentRepository.Storage;
 using SenseNet.ContentRepository.Storage.Security;
@@ -8,6 +9,35 @@ using SenseNet.Diagnostics;
 
 namespace SenseNet.Services.Core
 {
+    public class SenseNetAuthenticationOptions
+    {
+        /// <summary>
+        /// Add a cookie containing the JWT bearer token if it was sent in the
+        /// request header. If this cookie is sent by the client later and
+        /// there is no authorization header, the system will set the value
+        /// in the header.
+        /// Use this setting only if you need to authenticate requests (e.g file
+        /// download) where it is not possible to send the JWT token in the header.
+        /// Default is false.
+        /// </summary>
+        public bool AddJwtCookie { get; set; }
+    }
+
+    internal static class IdentityConstants
+    {
+        /// <summary>
+        /// Base value for cookie expiration
+        /// The NumericDate value is defined in the following document:
+        /// https://tools.ietf.org/html/rfc7519
+        /// </summary>
+        internal static readonly DateTime JwtCookieExpirationBaseDate =
+            DateTime.SpecifyKind(new DateTime(1970, 1, 1), DateTimeKind.Utc);
+
+        internal const string JwtCookieName = ".sn.auth";
+        internal const string HeaderAuthorization = "Authorization";
+        internal const string HeaderBearerPrefix = "Bearer ";
+    }
+
     public static class IdentityExtensions
     {
         /// <summary>
@@ -17,11 +47,34 @@ namespace SenseNet.Services.Core
         /// <returns>A reference to this instance after the operation has completed.</returns>
         public static IApplicationBuilder UseSenseNetAuthentication(this IApplicationBuilder app)
         {
+            return app.UseSenseNetAuthentication(null);
+        }
+
+        /// <summary>
+        /// Adds the Authentication middleware and a middleware for setting the sensenet current user.
+        /// </summary>
+        /// <param name="app">The Microsoft.AspNetCore.Builder.IApplicationBuilder to add the middleware to.</param>
+        /// <param name="configure">Configure sensenet authentication.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IApplicationBuilder UseSenseNetAuthentication(this IApplicationBuilder app, Action<SenseNetAuthenticationOptions> configure)
+        {
+            var options = new SenseNetAuthenticationOptions();
+            configure?.Invoke(options);
+
+            // add the optional cookie reader middleware - default is false
+            if (options.AddJwtCookie)
+                app.UseSenseNetJwtCookieReader();
+
             app.UseAuthentication();
             app.UseSenseNetUser();
 
+            // add the optional cookie writer middleware - default is false
+            if (options.AddJwtCookie)
+                app.UseSenseNetJwtCookieWriter();
+
             return app;
         }
+        
         /// <summary>
         /// Adds a middleware for setting the sensenet current user.
         /// </summary>
@@ -71,7 +124,91 @@ namespace SenseNet.Services.Core
 
                 User.Current = user ?? User.Visitor;
 
-                await next.Invoke();
+                if (next != null)
+                    await next();
+            });
+
+            return app;
+        }
+        
+        /// <summary>
+        /// Adds a middleware for reading the sensenet authentication cookie and setting
+        /// the value in the Authorization header. Use this in conjunction with the
+        /// <see cref="UseSenseNetJwtCookieWriter"/> configuration method.
+        /// </summary>
+        /// <remarks>This method must be called before UseAuthentication.</remarks>
+        /// <param name="app">The Microsoft.AspNetCore.Builder.IApplicationBuilder to add the middleware to.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IApplicationBuilder UseSenseNetJwtCookieReader(this IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                var authHeader = context.Request.Headers[IdentityConstants.HeaderAuthorization].FirstOrDefault();
+
+                // If the client sent a token in the header the regular way,
+                // this middleware will not modify that.
+
+                if (string.IsNullOrEmpty(authHeader) && 
+                    context.Request.Cookies.TryGetValue(IdentityConstants.JwtCookieName, out var authCookie))
+                {
+                    if (authCookie?.StartsWith(IdentityConstants.HeaderBearerPrefix) ?? false)
+                        context.Request.Headers[IdentityConstants.HeaderAuthorization] = authCookie;
+                }
+
+                if (next != null)
+                    await next();
+            });
+
+            return app;
+        }
+        /// <summary>
+        /// Adds a middleware for setting the sensenet authentication cookie if it was
+        /// provided by the client in the Authorization header. Use this in conjunction
+        /// with the <see cref="UseSenseNetJwtCookieReader"/> configuration method.
+        /// </summary>
+        /// <remarks>This method must be called after UseAuthentication.</remarks>
+        /// <param name="app">The Microsoft.AspNetCore.Builder.IApplicationBuilder to add the middleware to.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IApplicationBuilder UseSenseNetJwtCookieWriter(this IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                // If the user is authenticated and there is an authorization header,
+                // set a cookie containing the token. The expiration date should be
+                // the same value as in the token - available as the 'exp' claim.
+
+                if (context.User.Identity?.IsAuthenticated ?? false)
+                {
+                    var authHeader = context.Request.Headers[IdentityConstants.HeaderAuthorization].FirstOrDefault();
+                    if (!string.IsNullOrEmpty(authHeader))
+                    {
+                        // The 'exp' claim value is an integer that defines the number of
+                        // seconds that have passed since the base date (1970.01.01 UTC).
+                        // If the claim does not exist or cannot be parsed, set a default
+                        // value of 5 minutes.
+
+                        var expClaim = context.User.Claims.FirstOrDefault(cl => cl.Type == "exp")?.Value;
+                        var expDate = !string.IsNullOrEmpty(expClaim) && long.TryParse(expClaim, out var expSeconds)
+                            ? IdentityConstants.JwtCookieExpirationBaseDate.AddSeconds(expSeconds)
+                            : DateTime.UtcNow.AddMinutes(5);
+
+                        context.Response.Cookies.Append(IdentityConstants.JwtCookieName, authHeader,
+                            new CookieOptions
+                            {
+                                IsEssential = true,
+                                HttpOnly = true,
+                                SameSite = SameSiteMode.Strict,
+                                Expires = expDate
+                            });
+                    }
+                }
+                else
+                {
+                    context.Response.Cookies.Delete(IdentityConstants.JwtCookieName);
+                }
+
+                if (next != null)
+                    await next();
             });
 
             return app;
