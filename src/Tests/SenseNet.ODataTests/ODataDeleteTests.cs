6a44acb1a9771616387fe9a0349eba545bad70f4
tusmester <tusmester@gmail.com>
2020-03-27 16:00:21 +0100

    Port Delete method action and add tests. (#918)

--- src/Tests/SenseNet.ODataTests/ODataDeleteTests.cs
+++ src/Tests/SenseNet.ODataTests/ODataDeleteTests.cs
@@ -131,5 +131,61 @@ namespace SenseNet.ODataTests
                 Assert.IsFalse(Node.Exists(repoPath));
             });
         }
+
+        [TestMethod]
+        public async Task OD_DELETE_ByAction_DefaultParam()
+        {
+            await IsolatedODataTestAsync(async () =>
+            {
+                await DeleteByAction(string.Empty, false);
+            }).ConfigureAwait(false);
+        }
+        [TestMethod]
+        public async Task OD_DELETE_ByAction_Trash()
+        {
+            await IsolatedODataTestAsync(async () =>
+            {
+                await DeleteByAction("{ 'permanent': false }", false);
+            }).ConfigureAwait(false);
+        }
+        [TestMethod]
+        public async Task OD_DELETE_ByAction_Permanent()
+        {
+            await IsolatedODataTestAsync(async () =>
+            {
+                await DeleteByAction("{ 'permanent': true }", true);
+            }).ConfigureAwait(false);
+        }
+        private async Task DeleteByAction(string body, bool permanent)
+        {
+            // workaround: remove the old Delete application that is 
+            // not compatible with the .net core method
+            var deleteAppPath = "/Root/(apps)/GenericContent/Delete";
+            if (Node.Exists(deleteAppPath))
+                Node.ForceDelete(deleteAppPath);
+
+            // ARRANGE
+            var testRoot = CreateTestRoot("ODataTestRoot");
+            var name = "Content1";
+            var content = Content.CreateNew("Folder", testRoot, name);
+            content.Save();
+            var repoPath = $"{testRoot.Path}/{name}";
+            var resource = $"/OData.svc/{testRoot.Path}('{name}')/Delete";
+
+            // ACTION
+            var response = await ODataPostAsync(resource, "", body).ConfigureAwait(false);
+
+            // ASSERT
+            AssertNoError(response);
+            Assert.IsFalse(Node.Exists(repoPath));
+            
+            // reload
+            content = Content.Load(content.Id);
+
+            if (permanent)
+                Assert.IsNull(content);
+            else
+                Assert.IsTrue(TrashBin.IsInTrash(content.ContentHandler as GenericContent));
+        }
     }
 }
