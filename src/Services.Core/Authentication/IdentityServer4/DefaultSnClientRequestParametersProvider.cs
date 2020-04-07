74368b79aed84ccb7b23828854a42b5aa72af7a6
tusmester <tusmester@gmail.com>
2020-04-07 08:34:07 +0200

    Add the client request parameter provider feature for IdentityServer. (#930)

---
+++ src/Services.Core/Authentication/IdentityServer4/DefaultSnClientRequestParametersProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.AspNetCore.Http;
+
+namespace SenseNet.Services.Core.Authentication.IdentityServer4
+{
+    internal class DefaultSnClientRequestParametersProvider : ISnClientRequestParametersProvider
+    {
+        private readonly IDictionary<string, IDictionary<string, string>> _parameters =
+            new Dictionary<string, IDictionary<string, string>>();
+
+        public DefaultSnClientRequestParametersProvider(SnClientRequestOptions options)
+        {
+            foreach (var client in options.Clients)
+            {
+                _parameters.Add(client.ClientType, new ReadOnlyDictionary<string, string>(new Dictionary<string, string>
+                {
+                    { "authority", options.Authority },
+                    { "client_id", client.ClientId }
+                }));
+            }
+        }
+        public IDictionary<string, string> GetClientParameters(HttpContext context, string clientId)
+        {
+            if (string.IsNullOrEmpty(clientId))
+                return new Dictionary<string, string>();
+
+            if (_parameters.TryGetValue(clientId, out var clientParameters))
+                return clientParameters;
+
+            throw new InvalidOperationException($"Unknown client: {clientId}");
+        }
+    }
+}
---
+++ src/Services.Core/Authentication/IdentityServer4/IdentityServerExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SenseNet.Services.Core.Authentication.IdentityServer4
+{
+    public static class IdentityServerExtensions
+    {
+        /// <summary>
+        /// Adds custom IdentityServer client definitions for multi-repository clients.
+        /// If not sure, use the <see cref="AddDefaultSenseNetIdentityServerClients"/> method instead.        
+        /// </summary>
+        /// <remarks>
+        /// This method does not add the default client, developers need to add it
+        /// manually if necessary.
+        /// </remarks>
+        /// <param name="appBuilder">The <see cref="AuthenticationBuilder"/> instance.</param>
+        /// <param name="configureOptions">Configure method.</param>
+        public static AuthenticationBuilder AddSenseNetIdentityServerClients(this AuthenticationBuilder appBuilder, 
+            Action<SnClientRequestOptions> configureOptions)
+        {
+            var options = new SnClientRequestOptions();
+            configureOptions(options);
+
+            appBuilder.Services.AddSingleton(typeof(ISnClientRequestParametersProvider), 
+                new DefaultSnClientRequestParametersProvider(options));
+
+            return appBuilder;
+        }
+
+        /// <summary>
+        /// Adds the default admin UI client definition for multi-repository 
+        /// clients with a client id 'spa'.
+        /// </summary>
+        /// <remarks>
+        /// This feature is required only if a client application needs to connect
+        /// to multiple repositories. This module contains an OData action that
+        /// returns the configured clients to the caller.
+        /// </remarks>
+        /// <param name="appBuilder">The <see cref="AuthenticationBuilder"/> instance.</param>
+        /// <param name="authority">Url of the authority, usually an IdentityServer service.</param>
+        public static AuthenticationBuilder AddDefaultSenseNetIdentityServerClients(this AuthenticationBuilder appBuilder,
+            string authority)
+        {
+            if (string.IsNullOrEmpty(authority))
+                throw new ArgumentNullException(nameof(authority));
+
+            appBuilder.AddSenseNetIdentityServerClients(options =>
+            {
+                options.Authority = authority;
+                options.Clients.Add(new SnIdentityServerClient
+                {
+                    ClientType = "adminui",
+                    ClientId = "spa"
+                });
+            });
+
+            return appBuilder;
+        }
+    }
+}
