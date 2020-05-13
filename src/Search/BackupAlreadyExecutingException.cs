99f8ac19b138222410c795fb45647a9c8dfd9cb8
kavics <gyeby5@gmail.com>
2020-05-13 02:10:29 +0200

    Add BackupAlreadyExecutingException

---
+++ src/Search/BackupAlreadyExecutingException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SenseNet.Search
+{
+    [Serializable]
+    public class BackupAlreadyExecutingException : Exception
+    {
+        public BackupAlreadyExecutingException() { }
+
+        public BackupAlreadyExecutingException(string message) : base(message) { }
+
+        public BackupAlreadyExecutingException(string message, Exception inner) : base(message, inner) { }
+
+        protected BackupAlreadyExecutingException(SerializationInfo info,StreamingContext context)
+            : base(info, context) { }
+    }
+}
