f42a58446e0c690199eece3c526aff2d910fd270
kavics <gyeby5@gmail.com>
2020-06-04 06:49:46 +0200

    Add initial package to the in-mem-db

--- src/Tests/SenseNet.ODataTest.WebApp/Program.cs
+++ src/Tests/SenseNet.ODataTest.WebApp/Program.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Xml;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using SenseNet.ContentRepository;
-using SenseNet.ContentRepository.Fields;
 using SenseNet.ContentRepository.Security;
 using SenseNet.ContentRepository.InMemory;
 using SenseNet.ContentRepository.Storage;
+using SenseNet.ContentRepository.Storage.Data;
 using SenseNet.Diagnostics;
 
 namespace SenseNet.ODataTest.WebApp
@@ -27,6 +31,7 @@ namespace SenseNet.ODataTest.WebApp
             }))
             {
                 SnTrace.EnableAll();
+                InitializeDatabase();
                 host.Run();
             }
         }
@@ -34,5 +39,31 @@ namespace SenseNet.ODataTest.WebApp
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        private static void InitializeDatabase()
+        {
+            var path = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                @"..\..\..\..\..\nuget\snadmin\install-services-core\manifest.xml"));
+            string manifestXml;
+            using (var reader = new StreamReader(path))
+                manifestXml = reader.ReadToEnd();
+            var xml = new XmlDocument();
+            xml.LoadXml(manifestXml);
+            var root = xml.DocumentElement;
+
+            var package = new Package
+            {
+                ComponentId = root.SelectSingleNode("Id").InnerText,
+                Description = root.SelectSingleNode("Description").InnerText,
+                PackageType = Enum.Parse<PackageType>(root.Attributes["type"].Value),
+                ReleaseDate = DateTime.Parse(root.SelectSingleNode("ReleaseDate").InnerText),
+                ComponentVersion = Version.Parse(root.SelectSingleNode("Version").InnerText),
+                ExecutionDate = DateTime.UtcNow,
+                ExecutionResult = ExecutionResult.Successful,
+                Manifest = manifestXml
+            };
+            var provider = DataStore.GetDataProviderExtension<IPackagingDataProviderExtension>();
+            provider.SavePackageAsync(package, CancellationToken.None).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
     }
 }
