674af95890708434d70c0ce1b9fb2da95aa63bb0
tusmester <tusmester@gmail.com>
2020-06-12 21:10:10 +0200

    Feature/add templates (#992)
    * Add inmem test web app projects.
    * Move test web apps to a separate folder.
    * Update console installer references.
    * Add SQL test web apps with and without authentication.
    * Remove old test webapp.

---
+++ src/WebApps/SnWebApplication.Api.InMem.Admin/Startup.cs
@@ -0,0 +1,95 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SenseNet.ContentRepository;
+using SenseNet.OData;
+using SenseNet.Services.Core;
+using SenseNet.Services.Core.Authentication;
+using SenseNet.Services.Core.Cors;
+using SenseNet.Services.Core.Virtualization;
+using SenseNet.Services.Wopi;
+
+namespace SnWebApplication.Api.InMem.Admin
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            // [sensenet]: Authentication: switched off below
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddSenseNetRegistration(options =>
+                {
+                    // add newly registered users to this group
+                    options.Groups.Add("/Root/IMS/Public/Administrators");
+                });
+
+            // [sensenet]: add allowed client SPA urls
+            services.AddSenseNetCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            // [sensenet]: custom CORS policy
+            app.UseSenseNetCors();
+            // [sensenet]: use Authentication and set User.Current
+            app.UseSenseNetAuthentication(options =>
+            {
+                options.AddJwtCookie = true;
+            });
+
+            // [sensenet]: Authentication: in this test project everybody
+            // is an administrator!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            app.Use(async (context, next) =>
+            {
+                User.Current = User.Administrator;
+                if (next != null)
+                    await next();
+            });
+
+            app.UseAuthorization();
+
+            // [sensenet] Add the sensenet binary handler
+            app.UseSenseNetFiles();
+
+            // [sensenet]: OData middleware
+            app.UseSenseNetOdata();
+            // [sensenet]: WOPI middleware
+            app.UseSenseNetWopi();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("sensenet is listening. Visit https://sensenet.com for " +
+                                                      "more information on how to call the REST API.");
+                });
+            });
+        }
+    }
+}
