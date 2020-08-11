28bc9b74c9e84abb21141595da9321c7cbf6b45e
tusmester <tusmester@gmail.com>
2020-08-11 09:06:56 +0200

    Raise internal version of Storage, ContentRepo, Service, Services, Services.Core and Test projects.

--- src/Services/Properties/AssemblyInfo.cs
+++ src/Services/Properties/AssemblyInfo.cs
@@ -22,6 +22,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("7.7.10")]
-[assembly: AssemblyFileVersion("7.7.10")]
-[assembly: AssemblyInformationalVersion("7.7.10")]
+[assembly: AssemblyVersion("7.7.10.1")]
+[assembly: AssemblyFileVersion("7.7.10.1")]
+[assembly: AssemblyInformationalVersion("7.7.10.1")]
