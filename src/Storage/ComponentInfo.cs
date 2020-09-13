dd150f0294901413a5ff675d076b836793f49279
kavics <gyeby5@gmail.com>
2020-09-14 00:36:55 +0200

    ComponentInfo's code doc.

--- src/Storage/ComponentInfo.cs
+++ src/Storage/ComponentInfo.cs
@@ -2,11 +2,26 @@
 
 namespace SenseNet.ContentRepository.Storage
 {
+    /// <summary>
+    /// Represents a software element in the sensenet ecosystem that can be installed and patched automatically.
+    /// </summary>
     public class ComponentInfo
     {
+        /// <summary>
+        /// Gets or sets the unique identifier of the component.
+        /// </summary>
         public string ComponentId { get; set; }
+        /// <summary>
+        /// Gets or sets the last saved version. This value is independent of the success of the installation.
+        /// </summary>
         public Version Version { get; set; }
+        /// <summary>
+        /// Gets or sets the last successfully installed version.
+        /// </summary>
         public Version AcceptableVersion { get; set; }
+        /// <summary>
+        /// Gets or sets the description of the component.
+        /// </summary>
         public string Description { get; set; }
 
         public static readonly ComponentInfo Empty = new ComponentInfo
