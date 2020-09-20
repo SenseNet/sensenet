0123bd9dcc8f42116a1dc13d35ad86c8ddbe1fa5
kavics <gyeby5@gmail.com>
2020-09-20 20:30:56 +0200

    Fix prototype demo.

--- src/WebApps/SnWebApplication.Api.InMem.Admin/_CustomizationPrototype.cs
+++ src/WebApps/SnWebApplication.Api.InMem.Admin/_CustomizationPrototype.cs
@@ -19,87 +19,93 @@ namespace SenseNet.Extensions.DependencyInjection
         }
     }
 }
+
 namespace Gyebi.TheCustomizer
 {
     public class Feature1 : ISnComponent
     {
         public string ComponentId => typeof(Feature1).FullName;
         public Version SupportedVersion => null;
+
         public bool IsComponentAllowed(Version componentVersion)
         {
             return true;
         }
 
-        public ISnPatch[] Patches => new ISnPatch[]
+        public ISnPatch[] GetPatches()
         {
-            new ComponentInstaller
+            return new ISnPatch[]
             {
-                ComponentId = this.ComponentId,
-                Version = new Version(1, 0),
-                ReleaseDate = new DateTime(2020, 09, 18),
-                Description = "My feature Feature1 description",
-                Dependencies = new[]
+                new ComponentInstaller
                 {
-                    new Dependency
+                    ComponentId = this.ComponentId,
+                    Version = new Version(1, 0),
+                    ReleaseDate = new DateTime(2020, 09, 18),
+                    Description = "My feature Feature1 description",
+                    Dependencies = new[]
                     {
-                        Id = "SenseNet.Services",
-                        Boundary = new VersionBoundary {MinVersion = new Version(7, 7, 9)}
+                        new Dependency
+                        {
+                            Id = "SenseNet.Services",
+                            Boundary = new VersionBoundary {MinVersion = new Version(7, 7, 9)}
+                        }
+                    },
+                    Execute = (context) =>
+                    {
+                        using (new SystemAccount())
+                        {
+                            var folderName = "GyebiTesztel_v1.0";
+                            var content = Content.Load($"/Root/{folderName}");
+                            if (content == null)
+                            {
+                                var parent = Node.LoadNode("/Root");
+                                content = Content.CreateNew("SystemFolder", parent, folderName);
+                                content.Save();
+                            }
+                        }
                     }
                 },
-                Execute = (context) =>
+                new SnPatch
                 {
-                    using (new SystemAccount())
+                    ComponentId = this.ComponentId,
+                    Version = new Version(1, 1),
+                    ReleaseDate = new DateTime(2020, 09, 19),
+                    Description = "Feature1's PATCH to v1.1 description",
+                    Boundary = new VersionBoundary {MinVersion = new Version(1, 0)},
+                    Dependencies = new[]
                     {
-                        var folderName = "GyebiTesztel_v1.0";
-                        var content = Content.Load($"/Root/{folderName}");
-                        if (content == null)
+                        new Dependency
                         {
-                            var parent = Node.LoadNode("/Root");
-                            content = Content.CreateNew("SystemFolder", parent, folderName);
-                            content.Save();
+                            Id = "SenseNet.Services",
+                            Boundary = new VersionBoundary {MinVersion = new Version(7, 7, 9)}
+                        }
+                    },
+                    Execute = (context) =>
+                    {
+                        using (new SystemAccount())
+                        {
+                            var oldFolderName = "GyebiTesztel_v1.0";
+                            var newFolderName = "GyebiTesztel_v1.1";
+                            var newContent = Content.Load($"/Root/{newFolderName}");
+                            if (newContent != null)
+                                return;
+                            var oldContent = Content.Load($"/Root/{oldFolderName}");
+                            if (oldContent != null)
+                            {
+                                oldContent.ContentHandler.Name = newFolderName;
+                                oldContent.Save();
+                            }
+                            else
+                            {
+                                var parent = Node.LoadNode("/Root");
+                                newContent = Content.CreateNew("SystemFolder", parent, newFolderName);
+                                newContent.Save();
+                            }
                         }
                     }
                 }
-            },
-            new SnPatch
-            {
-                ComponentId = this.ComponentId,
-                Version = new Version(1, 1),
-                ReleaseDate = new DateTime(2020, 09, 19),
-                Description = "Feature1's PATCH to v1.1 description",
-                Boundary = new VersionBoundary{MinVersion = new Version(1,0)},
-                Dependencies = new[]
-                {
-                    new Dependency
-                    {
-                        Id = "SenseNet.Services",
-                        Boundary = new VersionBoundary {MinVersion = new Version(7, 7, 9)}
-                    }
-                },
-                Execute =  (context) =>
-                {
-                    using (new SystemAccount())
-                    {
-                        var oldFolderName = "GyebiTesztel_v1.0";
-                        var newFolderName = "GyebiTesztel_v1.1";
-                        var newContent = Content.Load($"/Root/{newFolderName}");
-                        if (newContent != null)
-                            return;
-                        var oldContent = Content.Load($"/Root/{oldFolderName}");
-                        if (oldContent != null)
-                        {
-                            oldContent.ContentHandler.Name = newFolderName;
-                            oldContent.Save();
-                        }
-                        else
-                        {
-                            var parent = Node.LoadNode("/Root");
-                            newContent = Content.CreateNew("SystemFolder", parent, newFolderName);
-                            newContent.Save();
-                        }
-                    }
-                }
-            }
-        };
+            };
+        }
+
     }
 }
