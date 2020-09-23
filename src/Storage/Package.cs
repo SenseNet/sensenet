e5299da03a5c324b0b653ec2a78bf14f8f1392f2
kavics <gyeby5@gmail.com>
2020-09-23 03:23:19 +0200

    Show enum name instead of value.

--- src/Storage/Package.cs
+++ src/Storage/Package.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace SenseNet.ContentRepository.Storage
 {
@@ -15,9 +16,11 @@ namespace SenseNet.ContentRepository.Storage
         public int Id { get; set; }                           // [Id] [int] IDENTITY(1,1) NOT NULL,
         public string Description { get; set; }               // [Description] [nvarchar](1000) NULL,
         public string ComponentId { get; set; }               // [ComponentId] [nvarchar](450) NULL,
+        [JsonConverter(typeof(StringEnumConverter))]
         public PackageType PackageType { get; set; }          // [PackageType] [varchar](50) NOT NULL,
         public DateTime ReleaseDate { get; set; }             // [ReleaseDate] [datetime] NOT NULL,
         public DateTime ExecutionDate { get; set; }           // [ExecutionDate] [datetime] NOT NULL,
+        [JsonConverter(typeof(StringEnumConverter))]
         public ExecutionResult ExecutionResult { get; set; }  // [ExecutionResult] [varchar](50) NOT NULL,
         public Version ComponentVersion { get; set; }         // [ComponentVersion] [varchar](50) NULL,
         public Exception ExecutionError { get; set; }         // [ExecutionError] [nvarchar](max) NULL,
