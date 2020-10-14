cbad0028651092d3a5d7d750c4fd2977852c02c2
kavics <gyeby5@gmail.com>
2020-10-14 18:07:52 +0200

    ExclusiveBlockConfiguration : SnConfig

--- src/Storage/ExclusiveLock/ExclusiveBlockConfiguration.cs
+++ src/Storage/ExclusiveLock/ExclusiveBlockConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using SenseNet.Configuration;
 using SenseNet.ContentRepository.Storage.Data;
 
 // ReSharper disable once CheckNamespace
@@ -8,21 +9,30 @@ namespace SenseNet.ContentRepository.Storage
     /// <summary>
     /// Defines a configuration of the exclusive block's execution.
     /// </summary>
-    public class ExclusiveBlockConfiguration
+    public class ExclusiveBlockConfiguration : SnConfig
     {
+        private const string SectionName = "sensenet/exclusiveLock";
+
+        internal static double LockTimeoutInSeconds { get; set; }
+            = GetDouble(SectionName, nameof(LockTimeoutInSeconds), 30.0, 5.0);
+        internal static double PollingTimeInSeconds { get; set; }
+            = GetDouble(SectionName, nameof(PollingTimeInSeconds), 5.0, 1.0);
+        internal static double WaitTimeoutInSeconds { get; set; }
+            = GetDouble(SectionName, nameof(WaitTimeoutInSeconds), 120.0, 20.0);
+
         /// <summary>
         /// Gets or sets the timeout of the obtained exclusive lock.
         /// If the time is out, the lock is automatically released.
         /// </summary>
-        public TimeSpan LockTimeout { get; set; } = TimeSpan.FromSeconds(/*1*/30); //UNDONE:X: configure the default LockTimeout
+        public TimeSpan LockTimeout { get; set; } = TimeSpan.FromSeconds(LockTimeoutInSeconds);
         /// <summary>
         /// Gets or sets the length of the lock monitoring period. Used in algorithms that wait for the releasing the lock.
         /// </summary>
-        public TimeSpan PollingTime { get; set; } = TimeSpan.FromSeconds(/*0.1*/5); //UNDONE:X: configure the default PollingTime
+        public TimeSpan PollingTime { get; set; } = TimeSpan.FromSeconds(PollingTimeInSeconds);
         /// <summary>
         /// Gets or sets the lock monitoring timeout. After this time a <see cref="TimeoutException"/> will be thrown.
         /// </summary>
-        public TimeSpan WaitTimeout { get; set; } = TimeSpan.FromMinutes(2); //UNDONE:X: configure the default WaitTimeout
+        public TimeSpan WaitTimeout { get; set; } = TimeSpan.FromSeconds(WaitTimeoutInSeconds);
         /// <summary>
         /// Gets the current implementation of the <see cref="IExclusiveLockDataProviderExtension"/>.
         /// </summary>
