7ed775927d37e78fb22a933195e297ca743edb7b
kavics <gyeby5@gmail.com>
2020-11-16 13:20:46 +0100

    Base classes.

---
+++ src/SenseNet.IntegrationTests/TestCases1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using SenseNet.ContentRepository;
+
+namespace SenseNet.IntegrationTests
+{
+    internal class RepositoryManager
+    {
+        public IDisposable StartRepository(TestCaseBase testCaseClass)
+        {
+            RepositoryBuilder builder = testCaseClass.GetRepositoryBuilder();
+            return Repository.Start(builder);
+        }
+    }
+
+    /* =========================================================================== TEST CASES */
+
+    public abstract class TestCaseBase
+    {
+        public void IntegrationTest(Action callback)
+        {
+            using (new RepositoryManager().StartRepository(this))
+            {
+                callback();
+            }
+        }
+
+        public abstract RepositoryBuilder GetRepositoryBuilder();
+    }
+    public class TestCases1 : TestCaseBase
+    {
+        public override RepositoryBuilder GetRepositoryBuilder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestCase_Experimental1()
+        {
+            IntegrationTest(() =>
+            {
+                // ASSIGN
+
+                // ACTION
+
+                //ASSERT
+            });
+        }
+    }
+
+    /* =========================================================================== TEST IMPLEMENTATIONS */
+
+    public abstract class IntegrationTestBase
+    {
+        public abstract TestCaseBase TestCases { get; }
+
+        public abstract RepositoryBuilder GetRepositoryBuilder();
+    }
+}
