49735f1c59b0a92532aa7e6b64187e69a03df704
kavics <gyeby5@gmail.com>
2020-11-17 16:27:31 +0100

    Use extended BuildInMemoryRepository method

--- src/Tests/SenseNet.IntegrationTests/InMemIntegrationTestBase.cs
+++ src/Tests/SenseNet.IntegrationTests/InMemIntegrationTestBase.cs
@@ -28,54 +28,12 @@ namespace SenseNet.IntegrationTests
 
         public override RepositoryBuilder GetRepositoryBuilder()
         {
-            var dataProvider = new InMemoryDataProvider();
-
             return new RepositoryBuilder()
-                .UseAccessProvider(new DesktopAccessProvider())
-                .UseDataProvider(dataProvider)
-                .UseInitialData(GetInitialData())
-                .UseSharedLockDataProviderExtension(new InMemorySharedLockDataProvider())
-                .UseBlobMetaDataProvider(new InMemoryBlobStorageMetaDataProvider(dataProvider))
-                .UseBlobProviderSelector(new InMemoryBlobProviderSelector())
-                .UseAccessTokenDataProviderExtension(new InMemoryAccessTokenDataProvider())
-                .UsePackagingDataProviderExtension(new InMemoryPackageStorageProvider())
-                .UseSearchEngine(new InMemorySearchEngine(GetInitialIndex()))
-                .UseSecurityDataProvider(GetSecurityDataProvider(dataProvider))
+                .BuildInMemoryRepository(GetInitialData(), GetInitialIndex())
                 .UseTestingDataProviderExtension(new InMemoryTestingDataProvider())
-                .UseElevatedModificationVisibilityRuleProvider(new ElevatedModificationVisibilityRule())
-                .StartWorkflowEngine(false)
                 .DisableNodeObservers()
                 .EnableNodeObservers(typeof(SettingsCache))
                 .UseTraceCategories("Test", "Event", "Custom") as RepositoryBuilder;
-        }
-        protected static ISecurityDataProvider GetSecurityDataProvider(InMemoryDataProvider repo)
-        {
-            return new MemoryDataProvider(new DatabaseStorage
-            {
-                Aces = new List<StoredAce>
-                {
-                    new StoredAce {EntityId = 2, IdentityId = 1, LocalOnly = false, AllowBits = 0x0EF, DenyBits = 0x000}
-                },
-                Entities = repo.LoadEntityTreeAsync(CancellationToken.None).GetAwaiter().GetResult()
-                    .ToDictionary(x => x.Id, x => new StoredSecurityEntity
-                    {
-                        Id = x.Id,
-                        OwnerId = x.OwnerId,
-                        ParentId = x.ParentId,
-                        IsInherited = true,
-                        HasExplicitEntry = x.Id == 2
-                    }),
-                Memberships = new List<Membership>
-                {
-                    new Membership
-                    {
-                        GroupId = Identifiers.AdministratorsGroupId,
-                        MemberId = Identifiers.AdministratorUserId,
-                        IsUser = true
-                    }
-                },
-                Messages = new List<Tuple<int, DateTime, byte[]>>()
-            });
         }
 
         private static InitialData _initialData;
