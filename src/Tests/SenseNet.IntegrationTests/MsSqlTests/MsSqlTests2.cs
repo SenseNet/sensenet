e8e85edacc36e7e8221e0ba9a3bd59ea93b5153a
kavics <gyeby5@gmail.com>
2020-12-18 03:06:55 +0100

    New frames: user, async.

--- src/Tests/SenseNet.IntegrationTests/MsSqlTests/MsSqlTests2.cs
+++ src/Tests/SenseNet.IntegrationTests/MsSqlTests/MsSqlTests2.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SenseNet.IntegrationTests.Infrastructure;
 using SenseNet.IntegrationTests.Platforms;
 using SenseNet.IntegrationTests.TestCases;
@@ -9,11 +10,11 @@ namespace SenseNet.IntegrationTests.MsSqlTests
     public class MsSqlTests2 : IntegrationTest<MsSqlPlatform, TestCase2>
     {
         [TestMethod] public void Integration_MsSql_2_1() { TestCase.TestCase_2_1(); }
-        [TestMethod] public void Integration_MsSql_2_2() { TestCase.TestCase_2_2(); }
+        [TestMethod] public void Integration_MsSql_2_2u() { TestCase.TestCase_2_2u(); }
         [TestMethod] public void Integration_MsSql_2_3() { TestCase.TestCase_2_3(); }
         [TestMethod] public void Integration_MsSql_2_4i() { TestCase.TestCase_2_4i(); }
         [TestMethod] public void Integration_MsSql_2_5() { TestCase.TestCase_2_5(); }
-        [TestMethod] public void Integration_MsSql_2_6() { TestCase.TestCase_2_6(); }
+        [TestMethod] public async Task Integration_MsSql_2_6a() { await TestCase.TestCase_2_6a(); }
         [TestMethod] public void Integration_MsSql_2_7() { TestCase.TestCase_2_7(); }
         [TestMethod] public void Integration_MsSql_2_8() { TestCase.TestCase_2_8(); }
     }
--- src/Tests/SenseNet.IntegrationTests/TestCases/TestCase2.cs
+++ src/Tests/SenseNet.IntegrationTests/TestCases/TestCase2.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SenseNet.ContentRepository;
 using SenseNet.ContentRepository.Storage;
+using Task = System.Threading.Tasks.Task;
 
 namespace SenseNet.IntegrationTests.TestCases
 {
@@ -24,22 +27,25 @@ namespace SenseNet.IntegrationTests.TestCases
                 Assert.AreEqual(42, loaded.Index);
             });
         }
-        public void TestCase_2_2()
+        public void TestCase_2_2u()
         {
             IntegrationTest((sandbox) =>
             {
-                // ASSIGN
-                var created = new SystemFolder(sandbox);
-                created.Save();
+                using (CurrentUserBlock(User.Administrator))
+                {
+                    // ASSIGN
+                    var created = new SystemFolder(sandbox);
+                    created.Save();
 
-                // ACTION
-                var edited = Node.LoadNode(created.Id);
-                edited.Index = 42;
-                edited.Save();
+                    // ACTION
+                    var edited = Node.LoadNode(created.Id);
+                    edited.Index = 42;
+                    edited.Save();
 
-                // ASSERT
-                var loaded = Node.LoadNode(created.Id);
-                Assert.AreEqual(42, loaded.Index);
+                    // ASSERT
+                    var loaded = Node.LoadNode(created.Id);
+                    Assert.AreEqual(42, loaded.Index);
+                }
             });
         }
         public void TestCase_2_3()
@@ -96,23 +102,23 @@ namespace SenseNet.IntegrationTests.TestCases
                 Assert.AreEqual(42, loaded.Index);
             });
         }
-        public void TestCase_2_6()
+        public async Task TestCase_2_6a()
         {
-            IntegrationTest((sandbox) =>
+            await IntegrationTestAsync(async (sandbox) =>
             {
                 // ASSIGN
                 var created = new SystemFolder(sandbox);
                 created.Save();
 
                 // ACTION
-                var edited = Node.LoadNode(created.Id);
+                var edited = await Node.LoadNodeAsync(created.Id, CancellationToken.None);
                 edited.Index = 42;
                 edited.Save();
 
                 // ASSERT
-                var loaded = Node.LoadNode(created.Id);
+                var loaded = await Node.LoadNodeAsync(created.Id, CancellationToken.None);
                 Assert.AreEqual(42, loaded.Index);
-            });
+            }).ConfigureAwait(false);
         }
         public void TestCase_2_7()
         {
