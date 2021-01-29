ffa2eae76e30b5a9caaa6c642fe3a3401a865c9e
kavics <gyeby5@gmail.com>
2021-01-29 04:00:29 +0100

    FIX: Linux problem of GetFiles method (case sensitive search pattern).

---
+++ src/Storage/FileSystemWrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using IO = System.IO;
+
+namespace SenseNet.Storage
+{
+    public static class FileSystemWrapper
+    {
+        public static class Directory
+        {
+            public static string[] GetFilesByExtension(string path, string ext)
+            {
+                return GetFilesByExtension(IO.Directory.GetFiles(path), ext);
+            }
+
+            private static string[] GetFilesByExtension(string[] paths, string ext)
+            {
+                if (!ext.StartsWith("."))
+                    ext = "." + ext;
+                return paths
+                    .Where(x => string.Compare(IO.Path.GetExtension(x), ext, StringComparison.OrdinalIgnoreCase) == 0)
+                    .ToArray();
+            }
+        }
+    }
+}
