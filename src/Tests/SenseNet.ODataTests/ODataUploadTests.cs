f7954a672a6914b051b208744db6f942dea1b253
kavics <gyeby5@gmail.com>
2021-02-01 06:42:20 +0100

    Modified test

--- src/Tests/SenseNet.ODataTests/ODataUploadTests.cs
+++ src/Tests/SenseNet.ODataTests/ODataUploadTests.cs
@@ -11,6 +11,210 @@ namespace SenseNet.ODataTests
     [TestClass]
     public class ODataUploadTests : ODataTestBase
     {
+        private static readonly string WebContent = @"<ContentType name='WebContent' parentType='ListItem' handler='SenseNet.ContentRepository.GenericContent' xmlns='http://schemas.sensenet.com/SenseNet/ContentRepository/ContentTypeDefinition'>
+  <DisplayName>$Ctd-WebContent,DisplayName</DisplayName>
+  <Description>$Ctd-WebContent,Description</Description>
+  <Icon>WebContent</Icon>
+  <Fields>
+    <Field name='ReviewDate' type='DateTime'>
+      <DisplayName>$Ctd-WebContent,ReviewDate-DisplayName</DisplayName>
+      <Description>$Ctd-WebContent,ReviewDate-Description</Description>
+      <Configuration>
+        <VisibleBrowse>Hide</VisibleBrowse>
+        <VisibleEdit>Hide</VisibleEdit>
+        <VisibleNew>Hide</VisibleNew>
+        <DateTimeMode>DateAndTime</DateTimeMode>
+      </Configuration>
+    </Field>
+    <Field name='ArchiveDate' type='DateTime'>
+      <DisplayName>$Ctd-WebContent,ArchiveDate-DisplayName</DisplayName>
+      <Description>$Ctd-WebContent,ArchiveDate-Description</Description>
+      <Configuration>
+        <VisibleBrowse>Hide</VisibleBrowse>
+        <VisibleEdit>Hide</VisibleEdit>
+        <VisibleNew>Hide</VisibleNew>
+        <DateTimeMode>DateAndTime</DateTimeMode>
+      </Configuration>
+    </Field>
+    <Field name='EnableLifespan' type='Boolean'>
+      <Configuration>
+        <VisibleBrowse>Show</VisibleBrowse>
+        <VisibleEdit>Show</VisibleEdit>
+        <VisibleNew>Show</VisibleNew>
+      </Configuration>
+    </Field>
+    <Field name='ValidFrom' type='DateTime'>
+      <Configuration>
+        <VisibleBrowse>Show</VisibleBrowse>
+        <VisibleEdit>Show</VisibleEdit>
+        <VisibleNew>Show</VisibleNew>
+      </Configuration>
+    </Field>
+    <Field name='ValidTill' type='DateTime'>
+      <Configuration>
+        <VisibleBrowse>Show</VisibleBrowse>
+        <VisibleEdit>Show</VisibleEdit>
+        <VisibleNew>Show</VisibleNew>
+      </Configuration>
+    </Field>
+    <Field name='IsTaggable' type='Boolean'>
+      <DisplayName>$Ctd-GenericContent,IsTaggable-DisplayName</DisplayName>
+      <Description>$Ctd-GenericContent,IsTaggable-Description</Description>
+      <Configuration>
+        <VisibleBrowse>Show</VisibleBrowse>
+        <VisibleEdit>Show</VisibleEdit>
+        <VisibleNew>Show</VisibleNew>
+        <DefaultValue>false</DefaultValue>
+      </Configuration>
+    </Field>
+    <Field name='Tags' type='LongText'>
+      <DisplayName>$Ctd-GenericContent,Tags-DisplayName</DisplayName>
+      <Description>$Ctd-GenericContent,Tags-Description</Description>
+      <Indexing>
+        <IndexHandler>SenseNet.Search.Indexing.TagIndexHandler</IndexHandler>
+      </Indexing>
+      <Configuration>
+        <ReadOnly>false</ReadOnly>
+        <VisibleBrowse>Show</VisibleBrowse>
+        <VisibleEdit>Show</VisibleEdit>
+        <VisibleNew>Show</VisibleNew>
+        <ControlHint>sn:TagList</ControlHint>
+      </Configuration>
+    </Field>
+  </Fields>
+</ContentType>";
+        private static readonly string ArticleCTD = @"<ContentType name='Article' parentType='WebContent' handler='SenseNet.ContentRepository.GenericContent' xmlns='http://schemas.sensenet.com/SenseNet/ContentRepository/ContentTypeDefinition'>
+  <DisplayName>$Ctd-Article,DisplayName</DisplayName>
+  <Description>$Ctd-Article,Description</Description>
+  <Icon>WebContent</Icon>
+  <Fields>
+    <Field name='DisplayName' type='ShortText'>
+      <DisplayName>$Ctd-Article,DisplayName-DisplayName</DisplayName>
+      <Description>$Ctd-Article,DisplayName-Description</Description>
+      <Configuration>
+        <FieldIndex>0</FieldIndex>
+      </Configuration>
+    </Field>
+    <Field name='Name' type='ShortText'>
+      <DisplayName>$Ctd-GenericContent,Name-DisplayName</DisplayName>
+      <Description>$Ctd-GenericContent,Name-Description</Description>
+      <Configuration>
+        <FieldIndex>1</FieldIndex>
+      </Configuration>
+    </Field>
+    <Field name='Subtitle' type='ShortText'>
+      <DisplayName>$Ctd-Article,Subtitle-DisplayName</DisplayName>
+      <Description>$Ctd-Article,Subtitle-Description</Description>
+      <Configuration>
+        <FieldIndex>2</FieldIndex>
+      </Configuration>
+    </Field>
+    <Field name='Author' type='ShortText'>
+      <DisplayName>$Ctd-Article,Author-DisplayName</DisplayName>
+      <Description>$Ctd-Article,Author-Description</Description>
+      <Configuration>
+        <FieldIndex>3</FieldIndex>
+      </Configuration>
+    </Field>
+    <Field name='Lead' type='LongText'>
+      <DisplayName>$Ctd-Article,Lead-DisplayName</DisplayName>
+      <Description>$Ctd-Article,Lead-Description</Description>
+      <Indexing>
+        <Analyzer>Standard</Analyzer>
+      </Indexing>
+      <Configuration>
+        <ControlHint>sn:RichText</ControlHint>
+        <FieldIndex>4</FieldIndex>
+      </Configuration>
+    </Field>
+    <Field name='Body' type='LongText'>
+      <DisplayName>$Ctd-Article,Body-DisplayName</DisplayName>
+      <Description>$Ctd-Article,Body-Description</Description>
+      <Indexing>
+        <Analyzer>Standard</Analyzer>
+      </Indexing>
+      <Configuration>
+        <ControlHint>sn:RichText</ControlHint>
+        <FieldIndex>5</FieldIndex>
+      </Configuration>
+    </Field>
+    <Field name='Pinned' type='Boolean'>
+      <DisplayName>$Ctd-Article,Pinned-DisplayName</DisplayName>
+      <Description>$Ctd-Article,Pinned-Description</Description>
+      <Configuration>
+        <FieldIndex>6</FieldIndex>
+      </Configuration>
+    </Field>
+    <Field name='Keywords' type='LongText'>
+      <DisplayName>$Ctd-Article,Keywords-DisplayName</DisplayName>
+      <Description>$Ctd-Article,Keywords-DisplayName</Description>
+      <Indexing>
+        <Analyzer>Whitespace</Analyzer>
+      </Indexing>
+      <Configuration>
+        <ControlHint>sn:Textarea</ControlHint>
+        <FieldIndex>7</FieldIndex>
+      </Configuration>
+    </Field>
+    <Field name='ImageRef' type='Reference'>
+      <DisplayName>$Ctd-Article,ImageRef-DisplayName</DisplayName>
+      <Configuration>
+        <VisibleBrowse>Hide</VisibleBrowse>
+        <VisibleEdit>Hide</VisibleEdit>
+        <VisibleNew>Hide</VisibleNew>
+        <AllowMultiple>false</AllowMultiple>
+      </Configuration>
+    </Field>
+    <Field name='ImageData' type='Binary'>
+      <DisplayName>$Ctd-Article,ImageData-DisplayName</DisplayName>
+      <Configuration>
+        <VisibleBrowse>Hide</VisibleBrowse>
+        <VisibleEdit>Hide</VisibleEdit>
+        <VisibleNew>Hide</VisibleNew>
+      </Configuration>
+    </Field>
+    <Field name='Image' type='Image'>
+      <DisplayName>$Ctd-Article,Image-DisplayName</DisplayName>
+      <Bind property='ImageRef' />
+      <Bind property='ImageData' />
+      <Configuration>
+        <VisibleBrowse>Show</VisibleBrowse>
+        <VisibleEdit>Show</VisibleEdit>
+        <VisibleNew>Show</VisibleNew>
+        <ControlHint>sn:Image</ControlHint>
+        <FieldIndex>8</FieldIndex>
+      </Configuration>
+    </Field>
+    <Field name='Description' type='LongText'>
+      <Configuration>
+        <VisibleBrowse>Hide</VisibleBrowse>
+        <VisibleEdit>Hide</VisibleEdit>
+        <VisibleNew>Hide</VisibleNew>
+      </Configuration>
+    </Field>
+    <Field name='Version' type='Version'>
+      <Configuration>
+        <VisibleBrowse>Hide</VisibleBrowse>
+        <VisibleEdit>Hide</VisibleEdit>
+        <VisibleNew>Hide</VisibleNew>
+      </Configuration>
+    </Field>
+    <Field name='IsTaggable' type='Boolean'>
+      <Configuration>
+        <VisibleBrowse>Hide</VisibleBrowse>
+        <VisibleEdit>Hide</VisibleEdit>
+        <VisibleNew>Hide</VisibleNew>
+      </Configuration>
+    </Field>
+    <Field name='Tags' type='LongText'>
+      <Configuration>
+        <VisibleBrowse>Hide</VisibleBrowse>
+        <VisibleEdit>Hide</VisibleEdit>
+        <VisibleNew>Hide</VisibleNew>
+      </Configuration>
+    </Field>
+  </Fields>
+</ContentType>";
         private static readonly string PageComponentCTD = @"<ContentType name='PageComponent' parentType='Folder' handler='SenseNet.ContentRepository.Folder' xmlns='http://schemas.sensenet.com/SenseNet/ContentRepository/ContentTypeDefinition'>
   <DisplayName>PageComponent</DisplayName>
   <Description></Description>
@@ -185,7 +389,7 @@ namespace SenseNet.ODataTests
             IsolatedODataTest(() =>
             {
                 // ARRANGE
-                ContentTypeInstaller.InstallContentType(PageComponentCTD, BlogPostCTD);
+                ContentTypeInstaller.InstallContentType(WebContent, ArticleCTD, PageComponentCTD);
                 var contentTypesFolder = Content.Load(Repository.ContentTypesFolderPath);
                 var httpContext = new DefaultHttpContext();
 
@@ -193,17 +397,29 @@ namespace SenseNet.ODataTests
                 var contentFolderGc = (GenericContent)contentFolder.ContentHandler;
                 contentFolderGc.AllowChildType("BlogPost", save: true);
 
+                // ACTION-1
+                var response1 = UploadActions.Upload(
+                        Content.Load("/Root/System/Schema/ContentTypes/GenericContent/Folder/PageComponent"),
+                        httpContext,
+                        ContentType: "ContentType",
+                        FileLength: 2230,
+                        FileName: "BlogPost",
+                        PropertyName: "Binary",
+                        Overwrite: true,
+                        ChunkToken: "0*0*False*False",
+                        FileText: BlogPostCTD)
+                    .ConfigureAwait(false).GetAwaiter().GetResult();
+
+
+
+                // ACTION-2
                 var blogPost = Content.CreateNew("BlogPost", contentFolderGc, "BlogPost1");
                 blogPost.Save();
 
+                // ASSERT-2
                 Assert.IsFalse(blogPost.Fields.ContainsKey("ImageAlt"));
 
-                // ACTION
-                //var response = await ODataPostAsync(
-                //        "/OData.svc/Root/IMS/BuiltIn/Portal('Administrators')/ODataError",
-                //        "?$format=table",
-                //        $@"{{""errorType"":""NodeAlreadyExistsException""}}")
-                //    .ConfigureAwait(false);
+                // ACTION-3
                 var response2 = UploadActions.Upload(
                         Content.Load("/Root/System/Schema/ContentTypes/GenericContent/Folder/PageComponent"),
                         httpContext,
@@ -216,7 +432,7 @@ namespace SenseNet.ODataTests
                         FileText: BlogPostCTD_updated)
                     .ConfigureAwait(false).GetAwaiter().GetResult();
 
-                // ASSERT
+                // ASSERT-3
                 //AssertNoError(response);
                 var loaded = Content.Load(blogPost.Id);
                 Assert.IsTrue(loaded.Fields.ContainsKey("ImageAlt"));
