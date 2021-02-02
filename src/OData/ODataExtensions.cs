aec4a3ea20259f92e9ab857de770aca8686f0fcf
tusmester <tusmester@gmail.com>
2021-02-02 19:51:12 +0100

    Consolidate middleware registration and add before-after builder methods. (#1301)

--- src/OData/ODataExtensions.cs
+++ src/OData/ODataExtensions.cs
@@ -14,13 +14,17 @@ namespace SenseNet.Extensions.DependencyInjection
         /// if the request contains the odata.svc prefix.
         /// </summary>
         /// <param name="builder">IApplicationBuilder instance.</param>
-        /// <param name="buildAppBranch">Optional builder method. Use this when you want to add
+        /// <param name="buildAppBranchBefore">Optional builder method. Use this when you want to add
+        /// additional middleware in the pipeline before the sensenet OData middleware.</param>
+        /// <param name="buildAppBranchAfter">Optional builder method. Use this when you want to add
         /// additional middleware in the pipeline after the sensenet OData middleware.</param>
         public static IApplicationBuilder UseSenseNetOdata(this IApplicationBuilder builder, 
-            Action<IApplicationBuilder> buildAppBranch = null)
+            Action<IApplicationBuilder> buildAppBranchBefore = null,
+            Action<IApplicationBuilder> buildAppBranchAfter = null)
         {
             // add OData middleware only if the request contains the appropriate prefix
-            builder.MapMiddlewareWhen<ODataMiddleware>("/odata.svc", buildAppBranch);
+            builder.MapMiddlewareWhen<ODataMiddleware>("/odata.svc", buildAppBranchBefore, 
+                buildAppBranchAfter, true);
 
             builder.UseOperationMethodExecutionPolicy(new VersioningOperationMethodPolicy());
 
