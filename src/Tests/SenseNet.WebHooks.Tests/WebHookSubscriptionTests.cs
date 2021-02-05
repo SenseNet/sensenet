035696ec0d91ef67aecefe868d03749b1a1710b3
tusmester <tusmester@gmail.com>
2021-02-05 18:57:35 +0100

    Add a simple webhook subscription test.

---
+++ src/Tests/SenseNet.WebHooks.Tests/WebHookSubscriptionTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.ContentRepository;
+using SenseNet.ContentRepository.Storage;
+using SenseNet.Events;
+using SenseNet.Tests.Core;
+using Task = System.Threading.Tasks.Task;
+
+namespace SenseNet.WebHooks.Tests
+{
+    [TestClass]
+    public class WebHookSubscriptionTests : TestBase
+    {
+        [TestMethod]
+        public async Task WebHookSubscription_OneType()
+        {
+            await Test(async () =>
+            {
+                var wh = await CreateWebHookSubscriptionAsync(@"{
+    ""Path"": ""/Root/Content"",
+    ""ContentTypes"": [ 
+        {
+            ""Name"": ""Folder"", 
+            ""Events"": [ ""Create"" ] 
+        }
+    ] 
+}");
+                Assert.AreEqual("/Root/Content", wh.FilterData.Path);
+                Assert.AreEqual(1, wh.FilterData.ContentTypes.Length);
+                Assert.AreEqual("Folder", wh.FilterData.ContentTypes[0].Name);
+                Assert.AreEqual(1, wh.FilterData.ContentTypes[0].Events.Length);
+                Assert.AreEqual(WebHookEventType.Create, wh.FilterData.ContentTypes[0].Events[0]);
+
+                Assert.AreEqual("+InTree:'/Root/Content' +Type:(Folder)", wh.FilterQuery);
+            });
+        }
+
+        private Task<WebHookSubscription> CreateWebHookSubscriptionAsync(string filter, string headers = null)
+        {
+            var container = RepositoryTools.CreateStructure("/Root/System/WebHooks", "SystemFolder") ??
+                Content.Load("/Root/System/WebHooks");
+
+            var wh = new WebHookSubscription(container.ContentHandler)
+            {
+                Url = "https://localhost:44393/webhooks/test",
+                Filter = filter,
+                //Filter = "{ \"Path\": \"/Root/Content\", \"ContentTypes\": [ { \"Name\": \"Folder\", \"Events\": [ \"Create\", \"Publish\" ] } ] }",
+                Headers = headers
+                //Headers = "{ \"h1-custom\": \"value1\", \"h2-custom\": \"value2\" }"
+            };
+            wh.Save();
+
+            return Node.LoadAsync<WebHookSubscription>(wh.Id, CancellationToken.None);
+        }
+    }
+}
