e9d01890b747f76aa8883a9106497ff91c6a693a
kavics <gyeby5@gmail.com>
2021-03-29 19:08:52 +0200

    Improved tracing for test start-end-outcome

--- src/Tests/SenseNet.IntegrationTests/Infrastructure/IntegrationTest.cs
+++ src/Tests/SenseNet.IntegrationTests/Infrastructure/IntegrationTest.cs
@@ -1,8 +1,54 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.Diagnostics;
+using SenseNet.Testing;
 
 namespace SenseNet.IntegrationTests.Infrastructure
 {
+    public static class MsTestTestContextExtensions
+    {
+        public static void StartTest(this TestContext testContext, bool traceToFile = false)
+        {
+            if (traceToFile)
+            {
+                var tracers =  SnTrace.SnTracers.ToArray();
+                testContext.Properties["SnTrace.Operation.Writers"] = tracers;
+                if(!tracers.Any(x => x is SnFileSystemTracer))
+                    SnTrace.SnTracers.Add(new SnFileSystemTracer());
+            }
+            StartTestPrivate(testContext);
+        }
+        private static void StartTestPrivate(TestContext testContext)
+        {
+            using (new Swindler<bool>(true, () => SnTrace.Test.Enabled, x => SnTrace.Test.Enabled = x))
+                testContext.Properties["SnTrace.Operation"] =
+                    SnTrace.Test.StartOperation("TESTMETHOD: " + testContext.TestName);
+        }
+        public static void FinishTestTest(this TestContext testContext)
+        {
+            using (new Swindler<bool>(true, () => SnTrace.Test.Enabled, x => SnTrace.Test.Enabled = x))
+            {
+                var op = (SnTrace.Operation)testContext.Properties["SnTrace.Operation"];
+                SnTrace.Test.Write("TESTMETHOD: {0}: {1}", testContext.TestName, testContext.CurrentTestOutcome);
+                if (op != null)
+                {
+                    op.Successful = true;
+                    op.Dispose();
+                }
+                SnTrace.Flush();
+            }
+            var originalTracers = (ISnTracer[])testContext.Properties["SnTrace.Operation.Writers"];
+            if (originalTracers != null)
+            {
+                SnTrace.SnTracers.Clear();
+                SnTrace.SnTracers.AddRange(originalTracers);
+            }
+        }
+    }
+
     public abstract class IntegrationTest<TPlatform, TTestCase>
         where TPlatform : IPlatform, new()
         where TTestCase : TestCaseBase, new()
@@ -19,19 +65,15 @@ namespace SenseNet.IntegrationTests.Infrastructure
             TestCase = new TTestCase { Platform = Platform };
         }
 
-        private DateTime _startTime;
-
         [TestInitialize]
         public void _initializeTest()
         {
-            _startTime = DateTime.Now;
-            Logger.Log($"    {TestContext.TestName} START");
+            TestContext.StartTest();
         }
         [TestCleanup]
         public void _cleanupTest()
         {
-            var duration = DateTime.Now - _startTime;
-            Logger.Log($"    {TestContext.TestName} {TestContext.CurrentTestOutcome} {duration:g}");
+            TestContext.FinishTestTest();
         }
 
         [ClassInitialize]
@@ -43,7 +85,7 @@ namespace SenseNet.IntegrationTests.Infrastructure
         public void _cleanupClass()
         {
             Logger.Log($"CleanupClass {this.GetType().Name}");
-                TestCaseBase.CleanupClass();
+            TestCaseBase.CleanupClass();
         }
     }
 }
