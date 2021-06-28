b92467c8e6a6acf825980e42ac66d2b90e16d252
kavics <gyeby5@gmail.com>
2021-06-28 03:18:26 +0200

    ExclusiveLock integration tests.

---
+++ src/Tests/SenseNet.IntegrationTests/InMemTests/InMemExclusiveLockTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.IntegrationTests.Infrastructure;
+using SenseNet.IntegrationTests.Platforms;
+using SenseNet.IntegrationTests.TestCases;
+
+namespace SenseNet.IntegrationTests.InMemTests
+{
+    [TestClass]
+    public class InMemExclusiveLockTests : IntegrationTest<InMemPlatform, ExclusiveLockTestCases>
+    {
+        [TestMethod]
+        public void IntT_InMem_ExclusiveLock_SkipIfLocked() { TestCase.ExclusiveLock_SkipIfLocked(); }
+        [TestMethod]
+        public void IntT_InMem_ExclusiveLock_WaitForReleased() { TestCase.ExclusiveLock_WaitForReleased(); }
+        [TestMethod]
+        public void IntT_InMem_ExclusiveLock_WaitAndAcquire() { TestCase.ExclusiveLock_WaitAndAcquire(); }
+    }
+}
---
+++ src/Tests/SenseNet.IntegrationTests/MsSqlTests/MsSqlExclusiveLockTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.IntegrationTests.Infrastructure;
+using SenseNet.IntegrationTests.Platforms;
+using SenseNet.IntegrationTests.TestCases;
+
+namespace SenseNet.IntegrationTests.MsSqlTests
+{
+    [TestClass]
+    public class MsSqlExclusiveLockTests : IntegrationTest<MsSqlPlatform, ExclusiveLockTestCases>
+    {
+        [TestMethod]
+        public void IntT_MsSql_ExclusiveLock_SkipIfLocked() { TestCase.ExclusiveLock_SkipIfLocked(); }
+        [TestMethod]
+        public void IntT_MsSql_ExclusiveLock_WaitForReleased() { TestCase.ExclusiveLock_WaitForReleased(); }
+        [TestMethod]
+        public void IntT_MsSql_ExclusiveLock_WaitAndAcquire() { TestCase.ExclusiveLock_WaitAndAcquire(); }
+    }
+}
