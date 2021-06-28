29588242f79913baccfd7911cbb80c78315ff90e
kavics <gyeby5@gmail.com>
2021-06-28 03:25:00 +0200

    Fix MsSql tests.

--- src/Tests/SenseNet.IntegrationTests/MsSqlTests/MsSqlExclusiveLockTests.cs
+++ src/Tests/SenseNet.IntegrationTests/MsSqlTests/MsSqlExclusiveLockTests.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.Configuration;
+using SenseNet.ContentRepository;
+using SenseNet.ContentRepository.Storage.Data.MsSqlClient;
 using SenseNet.IntegrationTests.Infrastructure;
 using SenseNet.IntegrationTests.Platforms;
 using SenseNet.IntegrationTests.TestCases;
@@ -8,6 +12,23 @@ namespace SenseNet.IntegrationTests.MsSqlTests
     [TestClass]
     public class MsSqlExclusiveLockTests : IntegrationTest<MsSqlPlatform, ExclusiveLockTestCases>
     {
+        #region MsSql specific infrastructure
+        private void ResetDatabase(RepositoryBuilder builder)
+        {
+            using (var ctx = new MsSqlDataContext(ConnectionStrings.ConnectionString, DataOptions.GetLegacyConfiguration(), CancellationToken.None))
+            {
+                ctx.ExecuteNonQueryAsync(MsSqlExclusiveLockDataProvider.DropScript).GetAwaiter().GetResult();
+                ctx.ExecuteNonQueryAsync(MsSqlExclusiveLockDataProvider.CreationScript).GetAwaiter().GetResult();
+            }
+        }
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            TestCase.TestInitializer = ResetDatabase;
+        }
+        #endregion
+
         [TestMethod]
         public void IntT_MsSql_ExclusiveLock_SkipIfLocked() { TestCase.ExclusiveLock_SkipIfLocked(); }
         [TestMethod]
