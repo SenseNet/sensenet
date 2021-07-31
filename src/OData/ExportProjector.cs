667555571e57696e344ea8ef3d70ac3457edb8f8
kavics <gyeby5@gmail.com>
2021-07-31 03:03:30 +0200

    refactor.

--- src/OData/ExportProjector.cs
+++ src/OData/ExportProjector.cs
@@ -13,9 +13,6 @@ namespace SenseNet.OData
     // No metadata. $export and $select are irrelevant
     internal class ExportProjector : Projector
     {
-        ////TODO: PERFORMANCE , INVALIDATION ???
-        //private readonly Dictionary<string, IEnumerable<string>> _fieldNamesForPaths = new Dictionary<string, IEnumerable<string>>();
-
         internal override void Initialize(Content container)
         {
             // do nothing
@@ -25,55 +22,21 @@ namespace SenseNet.OData
             var fields = new ODataEntity();
             var selfurl = GetSelfUrl(content);
 
-            IEnumerable<string> fieldNames;
-            //if (IsCollectionItem)
-            //{
-            //    if (_fieldNamesForPaths.ContainsKey(content.ContentHandler.ParentPath))
-            //        fieldNames = _fieldNamesForPaths[content.ContentHandler.ParentPath];
-            //    else
-            //        _fieldNamesForPaths[content.ContentHandler.ParentPath] = fieldNames = content.GetFieldNamesInParentTable();
-
-            //    if (content.AspectFields != null && content.AspectFields.Count > 0)
-            //        fieldNames = fieldNames.Concat(content.AspectFields.Keys);
-            //}
-            //else
-            //{
-                fieldNames = content.Fields.Keys;
-            //}
-
-            //if (!(content.ContentHandler is Content.RuntimeContentHandler))
-            //    fieldNames = fieldNames.Concat(new[] { ISFILEPROPERTY });
+            var fieldNames = content.Fields.Keys;
 
             foreach (var fieldName in fieldNames)
             {
-                if (fields.ContainsKey(fieldName))
-                    continue;
-
                 if (ODataMiddleware.DisabledFieldNames.Contains(fieldName))
-                {
-                    //fields.Add(fieldName, null);
                     continue;
-                }
 
                 if (IsAllowedField(content, fieldName))
                 {
                     if (content.Fields.TryGetValue(fieldName, out var field))
                         fields.Add(fieldName, GetJsonObject(field, selfurl, Request));
-                    else if (fieldName == ACTIONSPROPERTY)
-                        continue;
-                    else if (fieldName == ISFILEPROPERTY)
-                        //fields.Add(ISFILEPROPERTY, content.Fields.ContainsKey(ODataMiddleware.BinaryPropertyName));
-                        continue;
                     else if (fieldName == ICONPROPERTY)
                         fields.Add(fieldName, content.Icon ?? content.ContentType.Icon);
-                    else if (fieldName == ODataMiddleware.ChildrenPropertyName)
-                        continue;
                     else
                         fields.Add(fieldName, null);
-                }
-                else
-                {
-                    //fields.Add(fieldName, null);
                 }
             }
             return fields;
