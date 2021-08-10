628d970b378000ae34d6a8e6d728de9c134d399c
tusmester <tusmester@gmail.com>
2021-08-10 13:32:58 +0200

    Fix/linux ps install (#1542)
    * Add log to copy operation in compress script.
    
    * Change file system path separators and relative path handling.

--- src/Services.Core.Install/scripts/CompressInstall.ps1
+++ src/Services.Core.Install/scripts/CompressInstall.ps1
@@ -1,15 +1,15 @@
-﻿$srcPath = [System.IO.Path]::GetFullPath(($PSScriptRoot + '\..\..'))
-$installSourceSnAdminPath = "$srcPath\nuget\snadmin"
-$installPackagePath = "$srcPath\Services.Core.Install\install-services-core.zip"
-$source = "$installSourceSnAdminPath\install-services\import"
-$destination = "$installSourceSnAdminPath\install-services-core\import"
+﻿$srcPath = [System.IO.Path]::GetFullPath(($PSScriptRoot + '/../..'))
+$installSourceSnAdminPath = [System.IO.Path]::GetFullPath("$srcPath/nuget/snadmin")
+$installPackagePath = [System.IO.Path]::GetFullPath("$srcPath/Services.Core.Install/install-services-core.zip")
+$source = "$installSourceSnAdminPath/install-services/import"
+$destination = "$installSourceSnAdminPath/install-services-core/import"
 
 # Create the destination folder for the import structure.
 
-New-Item $installSourceSnAdminPath\install-services-core\import -ItemType directory -Force
+New-Item "$installSourceSnAdminPath/install-services-core/import" -ItemType directory -Force
 
 # Copy import structure from its original place excluding applications.
-Write-Host "Copying common source items..."
+Write-Host "Copying common source items from $source"
 
 $exclude = @("(apps).Content", "WebRoot.Content", "ErrorMessages.Content")
 $excludeMatch = @("(apps)", "WebRoot", "ErrorMessages")
@@ -18,8 +18,10 @@ Get-ChildItem -Path $source -Recurse -Exclude $exclude |
  where { $excludeMatch -eq $null -or $_.FullName.Replace($source, "") -notmatch $excludeMatchRegEx} |
  Copy-Item -Destination {
   if ($_.PSIsContainer) {
+   Write-Host $_.Parent.FullName
    Join-Path $destination $_.Parent.FullName.Substring($source.length)
   } else {
+   Write-Host $_.FullName
    Join-Path $destination $_.FullName.Substring($source.length)
   }
  } -Force -Exclude $exclude
@@ -29,9 +31,9 @@ Get-ChildItem -Path $source -Recurse -Exclude $exclude |
  # git what to commit and what to ignore.
  Write-Host "Copying netcore source items..."
 
- Copy-Item $installSourceSnAdminPath\install-services\importNetCore\** $destination -Container -Recurse -Force
+ Copy-Item "$installSourceSnAdminPath/install-services/importNetCore/**" $destination -Container -Recurse -Force
 
 # Create the install package.
 Write-Host "Compressing package..."
 
-Compress-Archive -Path "$installSourceSnAdminPath\install-services-core\*" -Force -CompressionLevel Optimal -DestinationPath $installPackagePath
+Compress-Archive -Path "$installSourceSnAdminPath/install-services-core/*" -Force -CompressionLevel Optimal -DestinationPath $installPackagePath
