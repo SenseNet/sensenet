e2bc8379f4214b80b2dfca0182d3541d704fba2d
tusmester <tusmester@gmail.com>
2021-08-19 15:29:46 +0200

    Add dashboard data provider feature and an action with an empty result. (#1554)

---
+++ src/Services.Core/Operations/DashboardOperations.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using SenseNet.ApplicationModel;
+using SenseNet.ContentRepository;
+using Task = System.Threading.Tasks.Task;
+
+namespace SenseNet.Services.Core.Operations
+{
+    /// <summary>
+    /// Defines methods for providing dashboard data to admin clients.
+    /// </summary>
+    public interface IDashboardDataProvider
+    {
+        /// <summary>
+        /// Gets dashboard data.
+        /// </summary>
+        /// <returns>An object containing dashboard data properties. This will be serialized
+        /// as the response of the GetDashboardData action.</returns>
+        Task<object> GetDashboardDataAsync();
+    }
+
+    public static class DashboardOperations
+    {
+        [ODataFunction]
+        [ContentTypes(N.CT.PortalRoot)]
+        [AllowedRoles(N.R.Everyone)]
+        public static Task<object> GetDashboardData(Content content, HttpContext httpContext)
+        {
+            var ddp = httpContext.RequestServices.GetService<IDashboardDataProvider>();
+            
+            return ddp == null ? Task.FromResult(default(object)) : ddp.GetDashboardDataAsync();
+        }
+    }
+}
