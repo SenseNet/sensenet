e88e277860137925d256eb93d79112a69d103afd
tusmester <tusmester@gmail.com>
2021-11-18 11:14:48 +0100

    Remove .net framework projects from main solution.

--- tools/scripts/CreateNuGetPackages.ps1
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -5,9 +5,9 @@ $scriptsSourcePath = "$srcPath\Storage\Data\MsSqlClient\Scripts"
 # delete existing packages
 Remove-Item $PSScriptRoot\*.nupkg
 
-nuget pack $srcPath\Services\SenseNet.Services.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
-nuget pack $srcPath\Tests\SenseNet.Tests\SenseNet.Tests.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
-nuget pack $srcPath\Tests\SenseNet.Tests.Hosting\SenseNet.Tests.Hosting.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
+# nuget pack $srcPath\Services\SenseNet.Services.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
+# nuget pack $srcPath\Tests\SenseNet.Tests\SenseNet.Tests.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
+# nuget pack $srcPath\Tests\SenseNet.Tests.Hosting\SenseNet.Tests.Hosting.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
 
 New-Item $srcPath\nuget\snadmin\install-services\scripts -ItemType directory -Force
 
@@ -18,8 +18,8 @@ Copy-Item $scriptsSourcePath\MsSqlInstall_Schema.sql $srcPath\nuget\snadmin\inst
 
 Compress-Archive -Path "$srcPath\nuget\snadmin\install-services\*" -Force -CompressionLevel Optimal -DestinationPath $installPackagePath
 
-nuget pack $srcPath\Services\SenseNet.Services.Install.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
+# nuget pack $srcPath\Services\SenseNet.Services.Install.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
 
 # assemble legacy packages
-nuget pack $srcPath\Scripting.JScript\SenseNet.Scripting.JScript.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
-nuget pack $srcPath\TextExtractors.Pdf\SenseNet.TextExtractors.Pdf.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
+# nuget pack $srcPath\Scripting.JScript\SenseNet.Scripting.JScript.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
+# nuget pack $srcPath\TextExtractors.Pdf\SenseNet.TextExtractors.Pdf.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
