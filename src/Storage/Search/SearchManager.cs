5e5818e20504b423af0664d6da7d1a4412e1c43b
Zoltán Gyebrovszki <kavics@sensenet.com>
2022-02-06 15:31:00 +0100

    Refactor.

--- src/Storage/Search/SearchManager.cs
+++ src/Storage/Search/SearchManager.cs
@@ -126,10 +126,6 @@ namespace SenseNet.ContentRepository.Search
     /// </summary>
     public class SearchManager
     {
-//private static IDataStore DataStore => Providers.Instance.DataStore;
-//private static SearchManager Instance = new SearchManager();
-//private SearchManager() { }
-
         /// <summary>
         /// Contains all values that mean "true". These are: "1", "true", "y" and "yes"
         /// </summary>
@@ -139,126 +135,72 @@ namespace SenseNet.ContentRepository.Search
         /// </summary>
         public static readonly List<string> NoList = new List<string>(new[] { "0", "false", "n", IndexValue.No });
 
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Gets a value that is true if the content query can run in the configured outer query engine.
-/// </summary>
-public static bool ContentQueryIsAllowed => Providers.Instance.SearchManager.ContentQueryIsAllowed;
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static bool ContentQueryIsAllowed => Providers.Instance.SearchManager.ContentQueryIsAllowed;
 
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Gets the implementation instance of the current <see cref="ISearchEngine"/>.
-/// The value depends on the value of the Configuration.Indexing.IsOuterSearchEngineEnabled setting.
-/// If this value is true, returns Providers.Instance.SearchEngine, otherwise the InternalSearchEngine.Instance.
-/// </summary>
-public static ISearchEngine SearchEngine => Providers.Instance.SearchManager.SearchEngine;
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static ISearchEngine SearchEngine => Providers.Instance.SearchManager.SearchEngine;
 
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Returns with the <see cref="QueryResult"/> of the given CQL query.
-/// </summary>
-/// <param name="text">CQL query text.</param>
-/// <param name="settings"><see cref="QuerySettings"/> that extends the query.</param>
-/// <param name="parameters">Values to substitute the parameters of the CQL query text.</param>
-public static QueryResult ExecuteContentQuery(string text, QuerySettings settings, params object[] parameters)
-{
-    return Providers.Instance.SearchManager.ExecuteContentQuery(text, settings, parameters);
-}
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Returns an <see cref="IIndexPopulator"/> implementation instance.
-/// </summary>
-public static IIndexPopulator GetIndexPopulator()
-{
-    return Providers.Instance.SearchManager.GetIndexPopulator();
-}
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Gets indexing metadata descriptor instance by fieldName
-/// </summary>
-public static IPerFieldIndexingInfo GetPerFieldIndexingInfo(string fieldName)
-{
-    return Providers.Instance.SearchManager.GetPerFieldIndexingInfo(fieldName);
-}
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-public static IndexDocument CompleteIndexDocument(IndexDocumentData indexDocumentData)
-{
-    return Providers.Instance.SearchManager.CompleteIndexDocument(indexDocumentData);
-}
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static QueryResult ExecuteContentQuery(string text, QuerySettings settings, params object[] parameters)
+        {
+            return Providers.Instance.SearchManager.ExecuteContentQuery(text, settings, parameters);
+        }
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static IIndexPopulator GetIndexPopulator()
+        {
+            return Providers.Instance.SearchManager.GetIndexPopulator();
+        }
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static IPerFieldIndexingInfo GetPerFieldIndexingInfo(string fieldName)
+        {
+            return Providers.Instance.SearchManager.GetPerFieldIndexingInfo(fieldName);
+        }
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static IndexDocument CompleteIndexDocument(IndexDocumentData indexDocumentData)
+        {
+            return Providers.Instance.SearchManager.CompleteIndexDocument(indexDocumentData);
+        }
 
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Gets a value that is true if the outer search engine is enabled.
-/// </summary>
-public static bool IsOuterEngineEnabled => Providers.Instance.SearchManager.IsOuterEngineEnabled;
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static bool IsOuterEngineEnabled => Providers.Instance.SearchManager.IsOuterEngineEnabled;
 
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Gets the path of the local index in the file system in case of local indexing engines.
-/// The value can be configured in the Indexing configuration class or set directly
-/// using the SetIndexDirectoryPath method.
-/// </summary>
-public static string IndexDirectoryPath => Providers.Instance.SearchManager.IndexDirectoryPath;
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static string IndexDirectoryPath => Providers.Instance.SearchManager.IndexDirectoryPath;
 
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Sets the path of the local index in the file system in case of local indexing engines.
-/// </summary>
-public static void SetIndexDirectoryPath(string path)
-{
-    Providers.Instance.SearchManager.IndexDirectoryPath = path;
-}
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static void SetIndexDirectoryPath(string path)
+        {
+            Providers.Instance.SearchManager.IndexDirectoryPath = path;
+        }
 
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Returns with the <see cref="IndexDocumentData"/> of the version identified by the given versionId.
-/// </summary>
-public static IndexDocumentData LoadIndexDocumentByVersionId(int versionId)
-{
-    return Providers.Instance.SearchManager.LoadIndexDocumentByVersionId(versionId);
-}
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Returns with the <see cref="IEnumerable&lt;IndexDocumentData&gt;"/> of the versions identified by the given versionIds.
-/// </summary>
-public static IEnumerable<IndexDocumentData> LoadIndexDocumentByVersionId(IEnumerable<int> versionId)
-{
-    return Providers.Instance.SearchManager.LoadIndexDocumentByVersionId(versionId);
-}
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Returns with the <see cref="IEnumerable&lt;IndexDocumentData&gt;"/> of all version of the node identified by the given path.
-/// </summary>
-public static IEnumerable<IndexDocumentData> LoadIndexDocumentsByPath(string path, int[] excludedNodeTypes)
-{
-    return Providers.Instance.SearchManager.LoadIndexDocumentsByPath(path, excludedNodeTypes);
-}
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static IndexDocumentData LoadIndexDocumentByVersionId(int versionId)
+        {
+            return Providers.Instance.SearchManager.LoadIndexDocumentByVersionId(versionId);
+        }
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static IEnumerable<IndexDocumentData> LoadIndexDocumentByVersionId(IEnumerable<int> versionId)
+        {
+            return Providers.Instance.SearchManager.LoadIndexDocumentByVersionId(versionId);
+        }
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static IEnumerable<IndexDocumentData> LoadIndexDocumentsByPath(string path, int[] excludedNodeTypes)
+        {
+            return Providers.Instance.SearchManager.LoadIndexDocumentsByPath(path, excludedNodeTypes);
+        }
 
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Constant value of the default auto filter status. The value is FilterStatus.Enabled.
-/// </summary>
-public static FilterStatus EnableAutofiltersDefaultValue => SnQuery.EnableAutofiltersDefaultValue;
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Constant value of the default lifespan filter status. The value is FilterStatus.Disabled.
-/// </summary>
-public static FilterStatus EnableLifespanFilterDefaultValue => SnQuery.EnableLifespanFilterDefaultValue;
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static FilterStatus EnableAutofiltersDefaultValue => SnQuery.EnableAutofiltersDefaultValue;
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static FilterStatus EnableLifespanFilterDefaultValue => SnQuery.EnableLifespanFilterDefaultValue;
 
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Returns with true id the value is "Enabled".
-/// Takes into account the EnableAutofiltersDefaultValue actual value.
-/// </summary>
-public static bool IsAutofilterEnabled(FilterStatus value) =>
-    Providers.Instance.SearchManager.IsAutofilterEnabled(value);
-//UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
-/// <summary>
-/// Returns with true id the value is "Enabled".
-/// Takes into account the EnableLifespanFilterDefaultValue actual value.
-/// </summary>
-public static bool IsLifespanFilterEnabled(FilterStatus value) =>
-    Providers.Instance.SearchManager.IsLifespanFilterEnabled(value);
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static bool IsAutofilterEnabled(FilterStatus value) =>
+            Providers.Instance.SearchManager.IsAutofilterEnabled(value);
+        //UNDONE:<?xxx: Delete if all references rewritten in the ecosystem
+        public static bool IsLifespanFilterEnabled(FilterStatus value) =>
+            Providers.Instance.SearchManager.IsLifespanFilterEnabled(value);
 
     }
 }
