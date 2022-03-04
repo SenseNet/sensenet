0d0b2e7c04a16cc386c39002ab0b926bfb4442bf
Zoltán Gyebrovszki <kavics@sensenet.com>
2022-03-04 03:09:48 +0100

    Integration tests for copying system nodes.

--- src/Tests/SenseNet.IntegrationTests/MsSqlTests/MsSqlNodeCopyMoveTests.cs
+++ src/Tests/SenseNet.IntegrationTests/MsSqlTests/MsSqlNodeCopyMoveTests.cs
@@ -137,6 +137,29 @@ namespace SenseNet.IntegrationTests.MsSqlTests
             TestCase.NodeCopy_CrossBinding_DataType_Binary();
         }
 
+        /* ================================================================================ COPY SYSTEM NODES */
+
+        [TestMethod]
+        public void IntT_MsSql_NodeCopy_NonSystem_to_NonSystem()
+        {
+            TestCase.NodeCopy_NonSystem_to_NonSystem();
+        }
+        [TestMethod]
+        public void IntT_MsSql_NodeCopy_NonSystem_to_System()
+        {
+            TestCase.NodeCopy_NonSystem_to_System();
+        }
+        [TestMethod]
+        public void IntT_MsSql_NodeCopy_System_to_NonSystem()
+        {
+            TestCase.NodeCopy_System_to_NonSystem();
+        }
+        [TestMethod]
+        public void IntT_MsSql_NodeCopy_System_to_System()
+        {
+            TestCase.NodeCopy_System_to_System();
+        }
+
         /* ==================================================================================== MOVE */
 
         [TestMethod]
