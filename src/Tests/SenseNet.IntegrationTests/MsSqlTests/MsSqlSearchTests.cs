037cfeb44064bf85eebf889d3f09b8269d348532
kavics <kavics@sensenet.com>
2022-03-04 07:30:51 +0100

    Fix/reference search (#1625)
    * Add red test to reproduce the reference search issue.
    
    * InMem reference search test.
    
    Co-authored-by: tusmester <tusmester@gmail.com>

---
+++ src/Tests/SenseNet.IntegrationTests/MsSqlTests/MsSqlSearchTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.IntegrationTests.Infrastructure;
+using SenseNet.IntegrationTests.Platforms;
+using SenseNet.IntegrationTests.TestCases;
+
+namespace SenseNet.IntegrationTests.MsSqlTests
+{
+    [TestClass]
+    public class MsSqlSearchTests : IntegrationTest<MsSqlPlatform, SearchTestCases>
+    {
+        [TestMethod]
+        public void IntT_MsSql_Search_ReferenceField()
+        {
+            TestCase.Search_ReferenceField();
+        }
+    }
+}
