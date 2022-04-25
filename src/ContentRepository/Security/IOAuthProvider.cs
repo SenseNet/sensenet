9a431ce21c64b6dfa82d949023bab7bfeb2e6d4a
tusmester <tusmester@gmail.com>
2022-04-25 10:13:46 +0200

    Remove unused system start and warmup config and make old OAuth api obsolete.

--- src/ContentRepository/Security/IOAuthProvider.cs
+++ src/ContentRepository/Security/IOAuthProvider.cs
@@ -1,9 +1,12 @@
-﻿namespace SenseNet.ContentRepository.Security
+﻿using System;
+
+namespace SenseNet.ContentRepository.Security
 {
     /// <summary>
     /// Defines members for an OAuth provider. Developers should not implement this
     /// interface directly: use the OAuthProvider base class instead.
     /// </summary>
+    [Obsolete("This feature is obsolete. Use newer authentication methods.")]
     public interface IOAuthProvider
     {
         string ProviderName { get; }
