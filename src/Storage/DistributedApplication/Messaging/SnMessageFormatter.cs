3993f2c3e8cb2e22144d7ca97641f4829fcbc6ac
Zoltán Gyebrovszki <kavics@sensenet.com>
2022-09-27 04:06:10 +0200

    SnMessageFormatter

---
+++ src/Storage/DistributedApplication/Messaging/SnMessageFormatter.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SenseNet.Communication.Messaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SenseNet.Storage.DistributedApplication.Messaging
+{
+    public class ClusterMessageTypes : IEnumerable<Type>
+    {
+        public IEnumerable<Type> Types { get; set; }
+        public IEnumerator<Type> GetEnumerator() => Types.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class SnMessageFormatter : IClusterMessageFormatter
+    {
+        private class Envelope
+        {
+            public string Type { get; set; }
+            public ClusterMessage Msg { get; set; }
+        }
+
+        private readonly Dictionary<string, Type> _knownMessageTypes;
+
+        public SnMessageFormatter(ClusterMessageTypes knownMessageTypes)
+        {
+            _knownMessageTypes = knownMessageTypes.ToDictionary(x => x.FullName, x => x);
+        }
+
+        public ClusterMessage Deserialize(Stream data)
+        {
+            using var reader = new StreamReader(data);
+            var text = reader.ReadToEnd();
+            var envelope = JsonConvert.DeserializeObject(text) as JObject;
+            if (envelope == null)
+                throw new InvalidDataException("Deserialization error.");
+
+            var typeName = envelope["Type"]?.ToString();
+            if(typeName == null)
+                throw new InvalidDataException("TypeName not found.");
+
+            if(!_knownMessageTypes.TryGetValue(typeName, out var type))
+                throw new InvalidDataException("Type not found: " + typeName);
+
+            var msg = envelope["Msg"];
+            if (msg == null)
+                throw new InvalidDataException("Message not found");
+
+            var message = msg.ToObject(type);
+            if (message == null)
+                throw new InvalidDataException($"Conversion to {typeName} is failed.");
+
+            var result = message as ClusterMessage;
+            if(result == null)
+                throw new InvalidDataException("Conversion to ClusterMessage is failed.");
+
+            return result;
+        }
+
+        public Stream Serialize(ClusterMessage message)
+        {
+            var envelope = new Envelope {Type = message.GetType().FullName, Msg = message};
+            var text = JsonConvert.SerializeObject(envelope);
+            var stream = GetStreamFromString(text);
+            return stream;
+        }
+        private static Stream GetStreamFromString(string textData)
+        {
+            var stream = new MemoryStream();
+
+            // Write to the stream only if the text is not empty, because writing an empty
+            // string in UTF-8 format would result in a 3 bytes length stream.
+            if (!string.IsNullOrEmpty(textData))
+            {
+                var writer = new StreamWriter(stream, Encoding.UTF8);
+                writer.Write(textData);
+                writer.Flush();
+
+                stream.Position = 0;
+            }
+
+            return stream;
+        }
+
+    }
+}
---
+++ src/Tests/SenseNet.ContentRepository.Tests/ClusterMessageTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SenseNet.Communication.Messaging;
+using SenseNet.Storage.DistributedApplication.Messaging;
+using SenseNet.Testing;
+using SenseNet.Tests.Core;
+using SenseNet.Tools;
+using static SenseNet.ContentRepository.Schema.ContentTypeManager;
+using STT = System.Threading.Tasks;
+
+namespace SenseNet.ContentRepository.Tests
+{
+    [TestClass]
+    public class ClusterMessageTests : TestBase
+    {
+        #region private class TestClusterChannel
+        private class TestClusterChannel : ClusterChannel
+        {
+            public ClusterMessage ReceivedMessage { get; private set; }
+
+            public TestClusterChannel(IClusterMessageFormatter formatter, ClusterMemberInfo clusterMemberInfo)
+                : base(formatter, clusterMemberInfo) { }
+
+            protected override STT.Task InternalSendAsync(Stream messageBody, bool isDebugMessage, CancellationToken cancellationToken)
+            {
+                this.OnMessageReceived(messageBody);
+                return STT.Task.CompletedTask;
+            }
+
+            protected internal override void OnMessageReceived(Stream messageBody)
+            {
+                base.OnMessageReceived(messageBody);
+
+                var baseAcc = new TypeAccessor(typeof(ClusterChannel));
+                var incomingMessages = (List<ClusterMessage>)baseAcc.GetStaticField("_incomingMessages");
+                ReceivedMessage = incomingMessages.FirstOrDefault();
+                incomingMessages.Clear();
+            }
+
+            public override bool RestartingAllChannels => false;
+            public override STT.Task RestartAllChannelsAsync(CancellationToken cancellationToken) => STT.Task.CompletedTask;
+        }
+        #endregion
+
+
+        [TestMethod]
+        public async STT.Task Messaging_Serialization_()
+        {
+            var services = new ServiceCollection()
+                .AddSingleton(ClusterMemberInfo.Current)
+                //.AddSingleton<IClusterMessageFormatter, BinaryMessageFormatter>()
+                .AddSingleton(new ClusterMessageTypes { Types = TypeResolver.GetTypesByBaseType(typeof(ClusterMessage)) })
+                .AddSingleton<IClusterMessageFormatter, SnMessageFormatter>()
+                .AddSingleton<IClusterChannel, TestClusterChannel>()
+                .BuildServiceProvider();
+
+            var message = new ContentTypeManagerResetDistributedAction();
+            var channel = (TestClusterChannel)services.GetRequiredService<IClusterChannel>();
+
+            // ACTION
+            await channel.SendAsync(message, CancellationToken.None).ConfigureAwait(false);
+
+            // ASSERT (serialized and deserialized but the type remains the same)
+            var received = channel.ReceivedMessage;
+            Assert.IsNotNull(received);
+            Assert.AreNotSame(message, received);
+            Assert.AreEqual(message.GetType(), received.GetType());
+        }
+    }
+}
