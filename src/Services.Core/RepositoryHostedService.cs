45654a35fc8eec4986e51225e8e1edd71c357ba1
Zoltán Gyebrovszki <kavics@sensenet.com>
2023-02-13 14:20:55 +0100

    Turn on the “Messages” and “Web” tracing categories by default.

--- src/Services.Core/RepositoryHostedService.cs
+++ src/Services.Core/RepositoryHostedService.cs
@@ -49,7 +49,7 @@ namespace SenseNet.Services.Core
                 .UseEventDistributor(new EventDistributor())
                 .AddAsyncEventProcessors(eventProcessors)
                 .UseTraceCategories("Event", "Custom", "System", "Security", "ContentOperation", "Database", "Index",
-                    "Repository") as RepositoryBuilder;
+                    "Repository", "Web", "Messaging") as RepositoryBuilder;
 
             // hook for developers to modify the repository builder before start
             BuildRepository?.Invoke(repositoryBuilder, Services);
