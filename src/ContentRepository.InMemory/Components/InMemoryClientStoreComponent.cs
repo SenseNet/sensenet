854761b44965101bf87add436ce2520fa02d129e
tusmester <tusmester@gmail.com>
2023-02-16 18:30:35 +0100

    Add inmemory clientstore component to generate default clients.

---
+++ src/ContentRepository.InMemory/Components/InMemoryClientStoreComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using SenseNet.ContentRepository.Security.Clients;
+using SenseNet.Packaging;
+
+namespace SenseNet.ContentRepository.Components
+{
+    public class InMemoryClientStoreComponent : SnComponent
+    {
+        public override string ComponentId { get; } = "SenseNet.ClientStore.InMemory";
+
+        public override void AddPatches(PatchBuilder builder)
+        {
+            builder.Install("1.0.0", "2023-02-16", "In memory implementation of Client store.")
+                .DependsOn("SenseNet.Services", "7.7.23")
+                .ActionOnBefore(context =>
+                {
+                    // no need to install anything
+                })
+                .Action(context =>
+                {
+                    // generate default clients and secrets
+                    var clientStore = context.GetService<ClientStore>();
+                    var clientOptions = context.GetService<IOptions<ClientStoreOptions>>().Value;
+
+                    clientStore.EnsureClientsAsync(clientOptions.Authority,
+                            clientOptions.RepositoryUrl.RemoveUrlSchema())
+                        .GetAwaiter().GetResult();
+                });
+        }
+    }
+}
