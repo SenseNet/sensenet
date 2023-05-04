b6fb4b0291f827a8ed6ef0f45464848d4c66e9c7
tusmester <tusmester@gmail.com>
2023-05-04 15:38:59 +0200

    Move apikey manager interface to the storage layer. (#1912)
    * Move apikey manager interface to the storage layer.
    
    * Use IUser instead of int.
    
    * Hide unnecessary extension class.

---
+++ src/Storage/Security/ApiKeys/ApiKey.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SenseNet.ContentRepository.Security.ApiKeys
+{
+    public class ApiKey
+    {
+        public string Value { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
