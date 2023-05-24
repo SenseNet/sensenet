bc8077de78096ba81b9a01dcb41c02632a8aaa1c
Zoltán Gyebrovszki <kavics@sensenet.com>
2023-05-24 14:46:38 +0200

    refactor

--- src/ContentRepository/ImageResizer.cs
+++ src/ContentRepository/ImageResizer.cs
@@ -36,13 +36,6 @@ namespace SenseNet.ContentRepository
             {
                 using var bitmap = SKBitmap.Decode(originalStream);
 
-                // if the size of the original image is the same as the specified resizing size then we just return the original stream
-                if (bitmap.Width == System.Convert.ToInt32(x) && bitmap.Height == System.Convert.ToInt32(y))
-                {
-                    originalStream.Position = 0;
-                    return originalStream;
-                }
-
                 double iw = bitmap.Width;
                 double ih = bitmap.Height;
                 double w = 0;
@@ -64,13 +57,6 @@ namespace SenseNet.ContentRepository
                 }
                 else
                 {
-                    // simpler resizing
-                    //var width = Convert.ToInt32(w);
-                    //var height = Convert.ToInt32(h);
-                    //var info = new SKImageInfo(width, height);
-                    //var quality = SKFilterQuality.High;
-                    //targetBitmap = bitmap.Resize(info, quality);
-
                     var resizeFactorX = Convert.ToSingle(w / iw);
                     var resizeFactorY = Convert.ToSingle(h / ih);
 
@@ -103,131 +89,30 @@ namespace SenseNet.ContentRepository
             return stream;
         }
 
-        //public static Stream CreateCropedImageFile(Stream originalStream, double x, double y, double q, ImageFormat outputFormat, SmoothingMode smoothingMode, InterpolationMode interpolationMode, PixelOffsetMode pixelOffsetMode, double verticalDiff, double horizontalDiff)
-        //{
-
-
-        //    if (originalStream == null)
-        //        return new MemoryStream();
-
-        //    Stream newMemoryStream;
-
-        //    using (var originalImage = System.Drawing.Image.FromStream(originalStream))
-        //    {
-        //        using (var bmp = new Bitmap((int)x, (int)y))
-        //        {
-        //            double verticalOffset = verticalDiff;
-        //            double horizontalOffset = horizontalDiff;
-        //            if(horizontalDiff == double.MaxValue)
-        //            {
-        //                horizontalOffset = originalImage.Width - x;
-        //            }else if(horizontalDiff < 0)
-        //            {
-        //                horizontalOffset = (originalImage.Width - x)/2;
-        //            }
-
-        //            if(horizontalOffset<0)
-        //                horizontalOffset = 0;
-
-        //            if (verticalDiff == double.MaxValue)
-        //            {
-        //                verticalOffset = originalImage.Height - y;
-        //            }else if(verticalDiff < 0)
-        //            {
-        //                verticalOffset = (originalImage.Height - y)/2;
-        //            }
-
-        //            if(verticalOffset<0)
-        //                verticalOffset = 0;
-
-        //            bmp.SetResolution(originalImage.HorizontalResolution, originalImage.VerticalResolution);
-        //            using (var graphic = Graphics.FromImage(bmp))
-        //            {
-        //                graphic.SmoothingMode = smoothingMode;
-        //                graphic.InterpolationMode = interpolationMode;
-        //                graphic.PixelOffsetMode = pixelOffsetMode;
-        //                graphic.DrawImage(originalImage, new Rectangle(0, 0, (int)x, (int)y), (int)horizontalOffset, (int)verticalOffset, (int)x, (int)y, GraphicsUnit.Pixel);
-        //                newMemoryStream = new MemoryStream();
-        //                bmp.Save(newMemoryStream, originalImage.RawFormat);
-                        
-        //                if(bmp != null)
-        //                    bmp.Dispose();
-        //            }
-        //        }
-        //    }
-        //    newMemoryStream.Position = 0;
-        //    return newMemoryStream;
-        //}
-        public static Stream CreateCropedImageFile(Stream originalStream, double x, double y, double q, SKEncodedImageFormat outputFormat, bool antiAlias, SKFilterQuality filterQuality, double verticalDiff, double horizontalDiff)
+        public static Stream CreateCroppedImageFile(Stream originalStream, double x, double y,
+            double offsetX, double offsetY, SKEncodedImageFormat outputFormat)
         {
             if (originalStream == null)
                 return new MemoryStream();
 
-            Stream newMemoryStream;
+            var left = Convert.ToInt32(offsetX);
+            var top = Convert.ToInt32(offsetY);
+            var right = left + Convert.ToInt32(x);
+            var bottom = top + Convert.ToInt32(y);
+            var cropRect = new SKRectI(left, top, right, bottom);
 
-            using (var originalBitmap = SKBitmap.Decode(originalStream))
-            {
-var left = Convert.ToInt32(horizontalDiff);
-var top = Convert.ToInt32(verticalDiff);
-var right = left + Convert.ToInt32(x);
-var bottom = top + Convert.ToInt32(y);
+            using var originalBitmap = SKBitmap.Decode(originalStream);
+            using var croppedBitmap = new SKBitmap(cropRect.Width, cropRect.Height);
+            var okay = originalBitmap.ExtractSubset(croppedBitmap, cropRect);
 
-                var cropRect = new SKRectI(left, top, right, bottom);
-                var croppedBitmap = new SKBitmap(cropRect.Width, cropRect.Height);
-                var okay = originalBitmap.ExtractSubset(croppedBitmap, cropRect);
+            using var image = SKImage.FromBitmap(croppedBitmap);
+            using var data = image.Encode(outputFormat, 90);
 
-                var image = SKImage.FromBitmap(croppedBitmap);
-                var data = image.Encode(outputFormat, 90);
-                newMemoryStream = new MemoryStream();
-                data.SaveTo(newMemoryStream);
+            var resultStream = new MemoryStream();
+            data.SaveTo(resultStream);
 
-                /*
-                using (var bmp = new Bitmap((int)x, (int)y))
-                {
-                    double verticalOffset = verticalDiff;
-                    double horizontalOffset = horizontalDiff;
-                    if (horizontalDiff == double.MaxValue)
-                    {
-                        horizontalOffset = originalImage.Width - x;
-                    }
-                    else if (horizontalDiff < 0)
-                    {
-                        horizontalOffset = (originalImage.Width - x) / 2;
-                    }
-
-                    if (horizontalOffset < 0)
-                        horizontalOffset = 0;
-
-                    if (verticalDiff == double.MaxValue)
-                    {
-                        verticalOffset = originalImage.Height - y;
-                    }
-                    else if (verticalDiff < 0)
-                    {
-                        verticalOffset = (originalImage.Height - y) / 2;
-                    }
-
-                    if (verticalOffset < 0)
-                        verticalOffset = 0;
-
-                    bmp.SetResolution(originalImage.HorizontalResolution, originalImage.VerticalResolution);
-                    using (var graphic = Graphics.FromImage(bmp))
-                    {
-                        graphic.SmoothingMode = smoothingMode;
-                        graphic.InterpolationMode = interpolationMode;
-                        graphic.PixelOffsetMode = pixelOffsetMode;
-                        graphic.DrawImage(originalImage, new Rectangle(0, 0, (int)x, (int)y), (int)horizontalOffset, (int)verticalOffset, (int)x, (int)y, GraphicsUnit.Pixel);
-                        newMemoryStream = new MemoryStream();
-                        bmp.Save(newMemoryStream, originalImage.RawFormat);
-
-                        if (bmp != null)
-                            bmp.Dispose();
-                    }
-                }
-                */
-            }
-            newMemoryStream.Position = 0;
-            return newMemoryStream;
+            resultStream.Position = 0;
+            return resultStream;
         }
 
         private static void GetRealXY(double imgX, double imgY, double targetX, double targetY, out double realX, out double realY)
