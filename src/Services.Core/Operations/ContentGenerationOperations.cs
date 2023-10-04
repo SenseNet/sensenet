5981c86dd5dce8a2979c551b5f68822ea01a8d75
Zoltán Gyebrovszki <kavics@sensenet.com>
2023-10-04 02:37:27 +0200

    A gift from me.

--- src/Services.Core/Operations/ContentGenerationOperations.cs
+++ src/Services.Core/Operations/ContentGenerationOperations.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Microsoft.Extensions.DependencyInjection;
 using SenseNet.ApplicationModel;
@@ -7,6 +9,8 @@ using SenseNet.ContentRepository;
 using SenseNet.ContentRepository.Storage.Data;
 using SenseNet.ContentRepository.Storage.Data.Replication;
 using STT = System.Threading.Tasks;
+using Newtonsoft.Json;
+using SenseNet.ContentRepository.Storage.Schema;
 
 namespace SenseNet.Services.Core.Operations;
 
@@ -30,5 +34,41 @@ public static class ContentGenerationOperations
 #pragma warning disable CS4014 // This call is not awaited, execution of the current method continues before the call is completed.
         replicationService.ReplicateNodeAsync(content.ContentHandler, targetContent.ContentHandler, options, CancellationToken.None);
 #pragma warning restore CS4014
+    }
+
+    [ODataFunction]
+    [AllowedRoles(N.R.Administrators, N.R.Developers)]
+    [ContentTypes(N.CT.GenericContent)]
+    public static object GetReplicationTemplate(Content content)
+    {
+        var readOnlyFields = content.Fields.Where(x => x.Value.ReadOnly)
+            .Select(x=>x.Key)
+            .ToArray();
+        var fields1 = ReplicationDescriptor.WellKnownProperties
+            .Select(x=>new KeyValuePair<string,string>(x.Key, $"____DataType.{x.Value}____"));
+        var fields2 = content.ContentHandler.NodeType.PropertyTypes
+            .Where(x => x.DataType == DataType.String || x.DataType == DataType.Int || x.DataType == DataType.DateTime || x.DataType == DataType.Text)
+            .Select(x => new KeyValuePair<string, string>(x.Name, $"____DataType.{x.DataType}____"))
+            //.OrderBy(x=>x.Key)
+            ;
+        var fields = fields1.Union(fields2)
+            .Where(x => !readOnlyFields.Contains(x.Key))
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        var result = new
+        {
+            target = "__Id_or_Path_of_the_existing_empty_container_content__",
+            options = new ReplicationDescriptor
+            {
+                MaxCount = 10,
+                MaxItemsPerFolder = 100,
+                MaxFoldersPerFolder = 100,
+                FirstFolderIndex = 0,
+                Fields = fields
+            }
+        };
+        var json = JsonConvert.SerializeObject(result, Formatting.Indented);
+
+        return json;
     }
 }
