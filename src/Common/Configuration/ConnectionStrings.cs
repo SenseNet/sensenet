146263852b6477b084fa3fffa046434d1a7527af
Zoltán Gyebrovszki <kavics@sensenet.com>
2023-10-17 09:31:04 +0200

    Use OptionsClassAttribute part 1.

--- src/Common/Configuration/ConnectionStrings.cs
+++ src/Common/Configuration/ConnectionStrings.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using SenseNet.Tools.Configuration;
 
 // ReSharper disable once CheckNamespace
 namespace SenseNet.Configuration
 {
+    /// <summary>
+    /// Contains connection strings of the repository and all used component.
+    /// </summary>
+    [OptionsClass(sectionName: "ConnectionStrings")]
     public class ConnectionStringOptions
     {
+        [JsonProperty("SnCrMsSql")]
         public string Repository { get; set; }
 
         private string _security;
+        [JsonProperty("SecurityStorage")]
         public string Security
         {
             get => _security ?? Repository;
@@ -18,6 +27,7 @@ namespace SenseNet.Configuration
         }
 
         private string _signalR;
+        [JsonProperty("SignalRDatabase")]
         public string SignalR
         {
             get => _signalR ?? Repository;
@@ -25,7 +35,6 @@ namespace SenseNet.Configuration
         }
 
         public IDictionary<string, string> AllConnectionStrings { get; set; }
-
     }
 
     public static class ConnectionOptionExtensions
