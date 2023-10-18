ea5a11543862dc1dd4c2137c6f8ca68189ef72fc
Zoltán Gyebrovszki <kavics@sensenet.com>
2023-10-18 04:21:46 +0200

    Undo ConnectionStrings

--- src/Common/Configuration/ConnectionStrings.cs
+++ src/Common/Configuration/ConnectionStrings.cs
@@ -1,25 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
-using SenseNet.Tools.Configuration;
 
 // ReSharper disable once CheckNamespace
 namespace SenseNet.Configuration
 {
-    /// <summary>
-    /// Contains connection strings of the repository and all used component.
-    /// </summary>
-    [OptionsClass(sectionName: "ConnectionStrings")]
     public class ConnectionStringOptions
     {
-        [JsonProperty("SnCrMsSql")]
         public string Repository { get; set; }
 
         private string _security;
-        [JsonProperty("SecurityStorage")]
         public string Security
         {
             get => _security ?? Repository;
@@ -27,7 +18,6 @@ namespace SenseNet.Configuration
         }
 
         private string _signalR;
-        [JsonProperty("SignalRDatabase")]
         public string SignalR
         {
             get => _signalR ?? Repository;
@@ -35,6 +25,7 @@ namespace SenseNet.Configuration
         }
 
         public IDictionary<string, string> AllConnectionStrings { get; set; }
+
     }
 
     public static class ConnectionOptionExtensions
