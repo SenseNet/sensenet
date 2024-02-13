56292999fde27f6f4364400bf67e250a9896f1b5
Zoltán Gyebrovszki <kavics@sensenet.com>
2024-02-13 13:34:42 +0100

    Fix AllRoles field

--- src/ContentRepository/Fields/AllRolesField.cs
+++ src/ContentRepository/Fields/AllRolesField.cs
@@ -1,9 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Xml;
+using AngleSharp.Dom;
 using SenseNet.Configuration;
 using SenseNet.ContentRepository.Schema;
 using SenseNet.ContentRepository.Storage;
 using SenseNet.ContentRepository.Storage.Security;
+using Node = SenseNet.ContentRepository.Storage.Node;
 
 namespace SenseNet.ContentRepository.Fields
 {
@@ -20,9 +23,21 @@ namespace SenseNet.ContentRepository.Fields
 
         public override object GetData()
         {
-            // this method will load only containers that the current user has permissions for
-            return Node.LoadNodes(Providers.Instance.SecurityHandler.SecurityContext
+            var thisPath = this.Content.Path;
+            var orgUnitNt = Providers.Instance.StorageSchema.NodeTypes[nameof(OrganizationalUnit)];
+
+            // loads only containers that the current user has permissions for
+            var allParents = Node.LoadNodes(Providers.Instance.SecurityHandler.SecurityContext
                 .GetParentGroups(this.Content.Id, false)).ToArray();
+
+            // valid item is all ancestors or any node that is not an OrganizationalUnit
+            var filtered = allParents
+                .Where(n => !n.NodeType.IsInstaceOfOrDerivedFrom(orgUnitNt) ||
+                            thisPath.StartsWith(n.Path, StringComparison.OrdinalIgnoreCase))
+                .ToArray();
+
+            return filtered;
+
         }
     }
 }
