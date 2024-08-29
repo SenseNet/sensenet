59f3a9034bb831ea3fbbe141fd77218fa2704ccb
kavics <kavics@sensenet.com>
2024-08-29 09:26:37 +0200

    Write codedoc for some service registration methods. (#2165)
    * Refactor for doc-generator of the service registrations.
    
    * Write codedoc for some service registration methods.

--- src/Storage/Diagnostics/AuditEventWriterExtensions.cs
+++ src/Storage/Diagnostics/AuditEventWriterExtensions.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.Extensions.DependencyInjection;
-using SenseNet.Configuration;
-using SenseNet.ContentRepository.Storage.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
 using SenseNet.Diagnostics;
 using SenseNet.Storage.Diagnostics;
-using SenseNet.Tools;
 using SenseNet.Tools.Diagnostics;
 
 // ReSharper disable once CheckNamespace
@@ -14,14 +8,23 @@ namespace SenseNet.Extensions.DependencyInjection
 {
     public static class AuditEventWriterExtensions
     {
+        /// <summary>
+        /// Adds the InactiveAuditEventWriter as an IAuditEventWriter implementation type to the service collection.
+        /// </summary>
         public static IServiceCollection AddInactiveAuditEventWriter(this IServiceCollection services)
         {
-            return AddAuditEventWriter<InactiveAuditEventWriter>(services);
+            return services.AddAuditEventWriter<InactiveAuditEventWriter>();
         }
+        /// <summary>
+        /// Adds the DatabaseAuditEventWriter as an IAuditEventWriter implementation type to the service collection.
+        /// </summary>
         public static IServiceCollection AddDatabaseAuditEventWriter(this IServiceCollection services)
         {
-            return AddAuditEventWriter<DatabaseAuditEventWriter>(services);
+            return services.AddAuditEventWriter<DatabaseAuditEventWriter>();
         }
+        /// <summary>
+        /// Adds an IAuditEventWriter implementation type to the service collection.
+        /// </summary>
         public static IServiceCollection AddAuditEventWriter<T>(this IServiceCollection services) where T : class, IAuditEventWriter
         {
             return services.AddSingleton<IAuditEventWriter, T>();
