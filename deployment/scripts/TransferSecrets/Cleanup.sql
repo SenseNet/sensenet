d528a80bb98d261e6af64700d485701a4e19c852
VargaJoe <17780970+VargaJoe@users.noreply.github.com>
2024-09-11 15:25:42 +0200

    add transfer secrets sql scripts (#2064)

---
+++ deployment/scripts/TransferSecrets/Cleanup.sql
@@ -0,0 +1,6 @@
+USE [ReinstallationTransfer]
+
+DELETE FROM SavedAccessTokens
+DELETE FROM SavedClientApps
+DELETE FROM SavedClientSecrets
+DELETE FROM SavedVersions
---
+++ deployment/scripts/TransferSecrets/_dropTables.sql
@@ -0,0 +1,6 @@
+USE [ReinstallationTransfer]
+
+DROP TABLE IF EXISTS [dbo].[SavedAccessTokens]
+DROP TABLE IF EXISTS [dbo].[SavedClientApps]
+DROP TABLE IF EXISTS [dbo].[SavedClientSecrets]
+DROP TABLE IF EXISTS [dbo].[SavedVersions]
