6bef129cb504858883d9fe7cd2f84edded262a52
Zoltán Gyebrovszki <kavics@sensenet.com>
2025-01-14 04:06:12 +0100

    Upgrade to net8

--- src/Storage/DistributedApplication/Messaging/BinaryMessageFormatter.cs
+++ src/Storage/DistributedApplication/Messaging/BinaryMessageFormatter.cs
@@ -1,42 +1,42 @@
-﻿using System.Runtime.Serialization.Formatters.Binary;
-using System.IO;
-using System.Runtime.Serialization;
-using SenseNet.Diagnostics;
+﻿//using System.Runtime.Serialization.Formatters.Binary;
+//using System.IO;
+//using System.Runtime.Serialization;
+//using SenseNet.Diagnostics;
 
-namespace SenseNet.Communication.Messaging
-{
-    public class BinaryMessageFormatter : IClusterMessageFormatter
-    {
-        #region IMessageFormatter Members
+//namespace SenseNet.Communication.Messaging
+//{
+//    public class BinaryMessageFormatter : IClusterMessageFormatter
+//    {
+//        #region IMessageFormatter Members
 
-        public ClusterMessage Deserialize(Stream data)
-        {
-            var bf = new BinaryFormatter();
-            ClusterMessage message;
+//        public ClusterMessage Deserialize(Stream data)
+//        {
+//            var bf = new BinaryFormatter();
+//            ClusterMessage message;
 
-            try
-            {
-                message = (ClusterMessage)bf.Deserialize(data);
-            }
-            catch (SerializationException e) // logged
-            {
-                SnLog.WriteException(e);
-                message = new UnknownMessageType(data);
-            }
+//            try
+//            {
+//                message = (ClusterMessage)bf.Deserialize(data);
+//            }
+//            catch (SerializationException e) // logged
+//            {
+//                SnLog.WriteException(e);
+//                message = new UnknownMessageType(data);
+//            }
 
-            return message;
-        }
+//            return message;
+//        }
 
-        public Stream Serialize(ClusterMessage message)
-        {
-            var ms = new MemoryStream();
-            var bf = new BinaryFormatter();
+//        public Stream Serialize(ClusterMessage message)
+//        {
+//            var ms = new MemoryStream();
+//            var bf = new BinaryFormatter();
 
-            bf.Serialize(ms, message); ms.Flush(); ms.Position = 0;
+//            bf.Serialize(ms, message); ms.Flush(); ms.Position = 0;
 
-            return ms;
-        }
+//            return ms;
+//        }
 
-        #endregion
-    }
-}
+//        #endregion
+//    }
+//}
